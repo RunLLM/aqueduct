a50c02704174553a33a573ac4e9cec4d007e3429
Vikram Sreekanti <vsreekanti@gmail.com>
2022-05-27 21:40:35 +0000

    Makes lint settings uniform across ui projects

--- src/ui/app/pages/data/index.tsx
+++ src/ui/app/pages/data/index.tsx
@@ -1,23 +1,23 @@
-import useUser from "@aqueducthq/common/src/components/hooks/useUser";
-import DataPage from "@aqueducthq/common/src/components/pages/data";
-import { useRouter } from "next/router";
-import React from "react";
+import useUser from '@aqueducthq/common/src/components/hooks/useUser';
+import DataPage from '@aqueducthq/common/src/components/pages/data';
+import { useRouter } from 'next/router';
+import React from 'react';
 
-export { getServerSideProps } from "@aqueducthq/common/src/components/pages/getServerSideProps";
+export { getServerSideProps } from '@aqueducthq/common/src/components/pages/getServerSideProps';
 
 const Data: React.FC = () => {
-  const { user, loading, success } = useUser();
-  if (loading) {
-    return null;
-  }
+    const { user, loading, success } = useUser();
+    if (loading) {
+        return null;
+    }
 
-  const router = useRouter();
-  if (!success) {
-    router.push("/login");
-    return null;
-  }
+    const router = useRouter();
+    if (!success) {
+        router.push('/login');
+        return null;
+    }
 
-  return <DataPage user={user} />;
+    return <DataPage user={user} />;
 };
 
 export default Data;
--- src/ui/app/reducers/dataPreview.ts
+++ src/ui/app/reducers/dataPreview.ts
@@ -1,82 +1,70 @@
-import { useAqueductConsts } from "@aqueducthq/common/src/components/hooks/useAqueductConsts";
-import { DataPreview } from "@aqueducthq/common/src/utils/data";
-import {
-  LoadingStatus,
-  LoadingStatusEnum,
-} from "@aqueducthq/common/src/utils/shared";
-import { createAsyncThunk, createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { useAqueductConsts } from '@aqueducthq/common/src/components/hooks/useAqueductConsts';
+import { DataPreview } from '@aqueducthq/common/src/utils/data';
+import { LoadingStatus, LoadingStatusEnum } from '@aqueducthq/common/src/utils/shared';
+import { createAsyncThunk, createSlice, PayloadAction } from '@reduxjs/toolkit';
 
 const { apiAddress, httpProtocol } = useAqueductConsts();
 
 export interface DataPreviewState {
-  loadingStatus: LoadingStatus;
-  data: DataPreview;
+    loadingStatus: LoadingStatus;
+    data: DataPreview;
 }
 
 const initialPreviewState: DataPreviewState = {
-  loadingStatus: { loading: LoadingStatusEnum.Initial, err: "" },
-  data: {
-    historical_versions: {},
-    latest_versions: {},
-  },
+    loadingStatus: { loading: LoadingStatusEnum.Initial, err: '' },
+    data: {
+        historical_versions: {},
+        latest_versions: {},
+    },
 };
 
-export const getDataArtifactPreview = createAsyncThunk<
-  DataPreview,
-  { apiKey: string }
->(
-  "dataPreviewReducer/fetch",
-  async (
-    args: {
-      apiKey: string;
-    },
-    thunkAPI
-  ) => {
-    const { apiKey } = args;
-    const response = await fetch(
-      `${httpProtocol}://${apiAddress}/artifact_versions`,
-      {
-        method: "GET",
-        headers: {
-          "api-key": apiKey,
+export const getDataArtifactPreview = createAsyncThunk<DataPreview, { apiKey: string }>(
+    'dataPreviewReducer/fetch',
+    async (
+        args: {
+            apiKey: string;
         },
-      }
-    );
+        thunkAPI,
+    ) => {
+        const { apiKey } = args;
+        const response = await fetch(`${httpProtocol}://${apiAddress}/artifact_versions`, {
+            method: 'GET',
+            headers: {
+                'api-key': apiKey,
+            },
+        });
 
-    const body = await response.json();
+        const body = await response.json();
 
-    if (!response.ok) {
-      return thunkAPI.rejectWithValue(body.error);
-    }
+        if (!response.ok) {
+            return thunkAPI.rejectWithValue(body.error);
+        }
 
-    return body as DataPreview;
-  }
+        return body as DataPreview;
+    },
 );
 
 export const dataPreviewSlice = createSlice({
-  name: "dataPreviewReducer",
-  initialState: initialPreviewState,
-  reducers: {},
-  extraReducers: (builder) => {
-    builder.addCase(getDataArtifactPreview.pending, (state) => {
-      state.loadingStatus = { loading: LoadingStatusEnum.Loading, err: "" };
-    });
+    name: 'dataPreviewReducer',
+    initialState: initialPreviewState,
+    reducers: {},
+    extraReducers: (builder) => {
+        builder.addCase(getDataArtifactPreview.pending, (state) => {
+            state.loadingStatus = { loading: LoadingStatusEnum.Loading, err: '' };
+        });
 
-    builder.addCase(
-      getDataArtifactPreview.fulfilled,
-      (state, { payload }: PayloadAction<DataPreview>) => {
-        state.loadingStatus = { loading: LoadingStatusEnum.Succeeded, err: "" };
-        state.data = payload;
-      }
-    );
+        builder.addCase(getDataArtifactPreview.fulfilled, (state, { payload }: PayloadAction<DataPreview>) => {
+            state.loadingStatus = { loading: LoadingStatusEnum.Succeeded, err: '' };
+            state.data = payload;
+        });
 
-    builder.addCase(getDataArtifactPreview.rejected, (state, { payload }) => {
-      state.loadingStatus = {
-        loading: LoadingStatusEnum.Failed,
-        err: payload as string,
-      };
-    });
-  },
+        builder.addCase(getDataArtifactPreview.rejected, (state, { payload }) => {
+            state.loadingStatus = {
+                loading: LoadingStatusEnum.Failed,
+                err: payload as string,
+            };
+        });
+    },
 });
 
 export default dataPreviewSlice.reducer;
