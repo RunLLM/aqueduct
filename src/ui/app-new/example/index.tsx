f8905f73399137f17822a5bf23b5f423ad1c0596
Andre Giron <andre@spiralai.co>
2022-06-07 12:59:39 -0700

    Adds react-router, pushing latest.

--- src/ui/app-new/example/index.tsx
+++ src/ui/app-new/example/index.tsx
@@ -1,12 +1,21 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+import { BrowserRouter } from "react-router-dom";
+import { Routes, Route, Link } from "react-router-dom";
+import HomePage from './pages/home';
 import { Thing } from '../.';
+import AboutPage from "./pages/about";
 
 const App = () => {
   return (
-    <div>
-      <Thing />
-    </div>
+      <React.StrictMode>
+        <BrowserRouter>
+          <Routes>
+              <Route path="/" component={HomePage} />
+              <Route path="/about" component={AboutPage} />
+          </Routes>
+        </BrowserRouter>
+      </React.StrictMode>
   );
 };
 
