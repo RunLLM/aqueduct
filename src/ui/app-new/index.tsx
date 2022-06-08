86bccc54972244680dd3ae45521ca2e17d1b6f64
Vikram Sreekanti <vsreekanti@gmail.com>
2022-06-08 18:38:28 +0000

    Fixes routes

--- src/ui/app-new/index.tsx
+++ src/ui/app-new/index.tsx
@@ -1,9 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Routes, Route, Link, useLocation } from "react-router-dom";
-// import HomePage from './pages/home';
+import { BrowserRouter, Routes, Route, Link, Navigate, useLocation } from "react-router-dom";
 import { Thing } from '../.';
-import AboutPage from "./pages/about";
 import { HomePage, DataPage, IntegrationsPage, WorkflowPage, WorkflowsPage, LoginPage } from '@aqueducthq/common';
 import { store } from './stores/store';
 import { Provider } from 'react-redux';
@@ -19,23 +17,35 @@ const App = () => {
     return null;
   }
 
+  let routesContent: React.ReactElement;
   if (!success) {
-    return <LoginPage />
+    routesContent = (
+      <Routes>
+        <Route path="/" element={<Navigate to="/login" />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/data" element={<Navigate to="/login" />} />
+        <Route path="/integrations" element={<Navigate to="/login" />} />
+        <Route path="/workflows" element={<Navigate to="/login" />} />
+        <Route path="/workflow/:id" element={<Navigate to="/login" />} />
+      </Routes>
+    );
+  } else {
+    routesContent = (
+      <Routes>
+        <Route path="/" element={<HomePage user={user} />} />
+        <Route path="/data" element={<DataPage user={user} />} />
+        <Route path="/integrations" element={<IntegrationsPage user={user} />} />
+        <Route path="/workflows" element={<WorkflowsPage user={user} />} />
+        <Route path="/login" element={<Navigate to="/" />} />
+        <Route path="/workflow/:id" element={<WorkflowPage user={user} />} />
+      </Routes>
+    );
   }
 
   const muiTheme = createTheme(theme);
   return (
       <ThemeProvider theme={muiTheme}>
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<HomePage user={user} />} />
-            <Route path="/data" element={<DataPage user={user} />} />
-            <Route path="/integrations" element={<IntegrationsPage user={user} />} />
-            <Route path="/workflows" element={<WorkflowsPage user={user} />} />
-            <Route path="/login" element={<LoginPage />} />
-            <Route path="/workflow/:id" element={<WorkflowPage user={user} />} />
-          </Routes>
-        </BrowserRouter>
+        <BrowserRouter>{routesContent}</BrowserRouter>
       </ThemeProvider>
   );
 };
