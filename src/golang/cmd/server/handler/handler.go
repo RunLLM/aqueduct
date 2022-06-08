02255aa8ccba9a2ec1b7e149db2da06fa955f15c
Wei Chen <cwinddavid@gmail.com>
2022-06-08 14:00:42 -0700

    [4/4] Reorg handler codes (#19)

--- src/golang/cmd/server/handler/handler.go
+++ src/golang/cmd/server/handler/handler.go
@@ -1,4 +1,4 @@
-package server
+package handler
 
 import (
 	"context"
@@ -6,7 +6,6 @@ import (
 
 	"github.com/aqueducthq/aqueduct/cmd/server/response"
 	"github.com/dropbox/godropbox/errors"
-	log "github.com/sirupsen/logrus"
 )
 
 type RequestMethod string
@@ -24,44 +23,6 @@ const (
 
 var ErrUnsupportedAuthMethod = errors.New("Auth method is not supported.")
 
-// Logs the full internal error message and sends the external error message back to the client.
-func HandleError(
-	ctx context.Context,
-	server Server,
-	handlerName string,
-	w http.ResponseWriter,
-	r *http.Request,
-	statusCode int,
-	err error,
-) {
-	if err == nil {
-		log.Fatal("Cannot pass no error into handleError()!")
-	}
-	server.Log(ctx, handlerName, r, statusCode, err)
-
-	var externalMsg string
-	dbxErr, ok := err.(errors.DropboxError)
-	if ok {
-		// Only return the top-level error message to clients.
-		externalMsg = dbxErr.GetMessage()
-	} else {
-		externalMsg = err.Error()
-	}
-	response.SendErrorResponse(w, externalMsg, statusCode)
-}
-
-func HandleSuccess(
-	ctx context.Context,
-	server Server,
-	handler Handler,
-	w http.ResponseWriter,
-	r *http.Request,
-	resp interface{},
-) {
-	server.Log(ctx, handler.Name(), r, http.StatusOK, nil)
-	handler.SendResponse(w, resp)
-}
-
 type Handler interface {
 	Name() string
 	// Extra headers additional to those required by auth.
@@ -78,23 +39,6 @@ type Handler interface {
 	Perform(ctx context.Context, interfaceArgs interface{}) (interface{}, int, error)
 	// Send response back.
 	SendResponse(w http.ResponseWriter, resp interface{})
-}
-
-func ExecuteHandler(server Server, handler Handler) func(w http.ResponseWriter, r *http.Request) {
-	return func(w http.ResponseWriter, r *http.Request) {
-		args, statusCode, err := handler.Prepare(r)
-		ctx := r.Context()
-		if err != nil {
-			HandleError(ctx, server, handler.Name(), w, r, statusCode, err)
-			return
-		}
-		resp, statusCode, err := handler.Perform(ctx, args)
-		if err != nil {
-			HandleError(ctx, server, handler.Name(), w, r, statusCode, err)
-			return
-		}
-		HandleSuccess(ctx, server, handler, w, r, resp)
-	}
 }
 
 type GetHandler struct{}
