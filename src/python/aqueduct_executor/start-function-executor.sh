eeacc7a617f6714bf724625ea670284cb940a961
likawind <cwinddavid@gmail.com>
2022-06-15 23:26:09 +0000

    fix exit code

--- src/python/aqueduct_executor/start-function-executor.sh
+++ src/python/aqueduct_executor/start-function-executor.sh
@@ -5,7 +5,7 @@ python3 -m aqueduct_executor.operators.function_executor.extract_function --spec
 
 if test -f "$OP_PATH/requirements.txt"; then pip3 install -r "$OP_PATH/requirements.txt" --no-cache-dir; fi
 
-python3 -m aqueduct_executor.operators.function_executor.execute_function --spec "$JOB_SPEC"
+EXIT_CODE=$(python3 -m aqueduct_executor.operators.function_executor.execute_function --spec "$JOB_SPEC")
 
 # Remove the /op suffix.
 FUNCTION_EXTRACT_PATH=${OP_PATH::-3}
@@ -14,3 +14,5 @@ if [ ! -z "$FUNCTION_EXTRACT_PATH" -a "$FUNCTION_EXTRACT_PATH" != *"*"* ]
 then
       rm -rf $FUNCTION_EXTRACT_PATH
 fi
+
+exit $(($EXIT_CODE))
