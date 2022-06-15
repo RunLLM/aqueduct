92e0a98fdcdccd23ae8c37a8f4a4330795835e03
Kenneth Xu <kenxu95@gmail.com>
2022-06-15 11:28:27 -0700

    ready

--- src/golang/cmd/server/request/parameters.go
+++ src/golang/cmd/server/request/parameters.go
@@ -3,8 +3,6 @@ package request
 import (
 	"encoding/json"
 	"net/http"
-
-	"github.com/aqueducthq/aqueduct/cmd/server/routes"
 )
 
 const (
@@ -15,23 +13,15 @@ const (
 // Extracts the parameters dictionary from the request, which maps
 // parameter name to its json-serialized string value.
 func ExtractParamsfromRequest(r *http.Request) (map[string]string, error) {
-	paramsInBytes, err := ExtractHttpPayload(
-		r.Header.Get(routes.ContentTypeHeader),
-		parametersKey,
-		false,
-		r,
-	)
-	if err != nil {
-		return nil, err
-	}
+	serializedParams := []byte(r.FormValue(parametersKey))
 
 	// No-op if there aren't any parameters set.
-	if len(paramsInBytes) == 0 {
+	if len(serializedParams) == 0 {
 		return nil, nil
 	}
 
 	var params map[string]string
-	err = json.Unmarshal(paramsInBytes, &params)
+	err := json.Unmarshal(serializedParams, &params)
 	if err != nil {
 		return nil, err
 	}
