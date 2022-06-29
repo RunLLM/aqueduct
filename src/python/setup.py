5cfa058a0c7908903e01c1bb2c74fb960cb42146
Eunice Chan <ekchan@berkeley.edu>
2022-06-29 22:00:34 +0000

    Move download to pip

--- src/python/setup.py
+++ src/python/setup.py
@@ -1,10 +1,63 @@
 from setuptools import find_packages, setup
 from setuptools.command.develop import develop
 from setuptools.command.install import install
+from setuptools.command.egg_info import egg_info
 from pathlib import Path
 import os
+import string
+import random
+import subprocess
+import sys
+import platform
 
+base_directory = os.path.join(os.environ["HOME"], ".aqueduct")
+server_directory = os.path.join(os.environ["HOME"], ".aqueduct", "server")
+ui_directory = os.path.join(os.environ["HOME"], ".aqueduct", "ui")
 
+package_version = "0.0.3"
+
+def update_config_yaml(file):
+    s=string.ascii_uppercase+string.digits
+    encryption_key = ''.join(random.sample(s,32))
+    api_key = ''.join(random.sample(s,32))
+
+    with open(file, "r") as sources:
+        lines = sources.readlines()
+    with open(file, "w") as sources:
+        for line in lines:
+            if "<BASE_PATH>" in line:
+                sources.write(line.replace("<BASE_PATH>", server_directory))
+            elif "<ENCRYPTION_KEY>" in line:
+                sources.write(line.replace("<ENCRYPTION_KEY>", encryption_key))
+            elif "<API_KEY>" in line:
+                sources.write(line.replace("<API_KEY>", api_key))
+            else:
+                sources.write(line)
+    print("Updated configurations.")
+
+def execute_command(args, cwd=None):
+    with subprocess.Popen(args, stdout=sys.stdout, stderr=sys.stderr, cwd=cwd) as proc:
+        proc.communicate()
+        if proc.returncode != 0:
+            raise Exception("Error executing command: %s" % args)
+
+def generate_version_file(file_path):
+    with open(file_path, 'w') as f:
+        f.write(package_version)
+    print("Wrote version to file.")
+
+# Returns a bool indicating whether we need to perform a version upgrade.
+def require_update(file_path):
+    if not os.path.isfile(file_path):
+        return True
+    with open(file_path, 'r') as f:
+        current_version = f.read()
+        if package_version < current_version:
+            raise Exception("Attempting to install an older version %s but found existing newer version %s" % (package_version, current_version))
+        elif package_version == current_version:
+            return False
+        else:
+            return True
 
 def update_executable_permissions():
     execute_command(["chmod", "755", os.path.join(server_directory, "bin", "server")])
@@ -12,6 +65,7 @@ def update_executable_permissions():
     execute_command(["chmod", "755", os.path.join(server_directory, "bin", "migrator")])
 
 def download_server_binaries():
+    print("Downloading server binaries.")
     s3_prefix = "https://aqueduct-ai.s3.us-east-2.amazonaws.com/assets/%s/server" % package_version
     execute_command(["rm", "-rf", os.path.join(server_directory, "bin")])
     os.mkdir(os.path.join(server_directory, "bin"))
@@ -65,21 +119,68 @@ def update_server_version():
 
     execute_command([os.path.join(server_directory, "bin", "migrator"), "--type", "sqlite", "goto", "9"])
 
+def updates():
+    if not os.path.isdir(base_directory):
+        os.mkdir(base_directory)
 
-class PostDevelopCommand(develop):
+    if not os.path.isdir(ui_directory) or require_update(os.path.join(ui_directory, "__version__")):
+        update_ui_version()
+    
+    if not os.path.isdir(server_directory):
+        try:
+            directories = [
+                server_directory,
+                os.path.join(server_directory, "db"),
+                os.path.join(server_directory, "storage"),
+                os.path.join(server_directory, "storage", "operators"),
+                os.path.join(server_directory, "vault"),
+                os.path.join(server_directory, "bin"),
+                os.path.join(server_directory, "config"),
+                os.path.join(server_directory, "logs"),
+            ]
+
+            for directory in directories:
+                os.mkdir(directory)
+
+            update_server_version()
+
+            s3_prefix = "https://aqueduct-ai.s3.us-east-2.amazonaws.com/assets/%s/server" % package_version
+            execute_command(["curl", os.path.join(s3_prefix, "config/config.yml"), "--output", os.path.join(server_directory, "config/config.yml")])
+            update_config_yaml(os.path.join(server_directory, "config", "config.yml"))
+            execute_command(["curl", os.path.join(s3_prefix, "db/demo.db"), "--output", os.path.join(server_directory, "db/demo.db")])
+
+            print("Finished initializing Aqueduct base directory.")
+        except Exception as e:
+            print(e)
+            execute_command(["rm", "-rf", server_directory])
+            exit(1)
+
+    if require_update(os.path.join(server_directory, "__version__")):
+        try:
+            update_server_version()
+        except Exception as e:
+            print(e)
+            if os.path.isfile(os.path.join(server_directory, "__version__")):
+                execute_command(["rm", os.path.join(server_directory, "__version__")])
+            exit(1)
+
+class DevelopCommand(develop):
     """Post-installation for development mode."""
     def run(self):
         develop.run(self)
-        print("UI>>>")
-        update_ui_version()
-        update_server_version()
+        updates()
 
-class PostInstallCommand(install):
+class InstallCommand(install):
     """Post-installation for installation mode."""
     def run(self):
         install.run(self)
-        update_ui_version()
-        update_server_version()
+        updates()
+
+class EggInfoCommand(egg_info):
+    """Post-installation for egg info mode."""
+    def run(self):
+        egg_info.run(self)
+        updates()
 
 install_requires = open("requirements.txt").read().strip().split("\n")
 
@@ -90,7 +191,7 @@ setup(
     name="aqueduct-ml",
     version="0.0.3",
     install_requires=install_requires,
-    scripts=["bin/aqueduct.py"],
+    scripts=["bin/aqueduct"],
     packages=find_packages(),
     description="Prediction Infrastructure for Data Scientists",
     long_description=long_description,
@@ -103,8 +204,9 @@ setup(
         "Programming Language :: Python :: 3",
     ],
     cmdclass={
-        'develop': PostDevelopCommand,
-        'install': PostInstallCommand,
+        'develop': DevelopCommand,
+        'install': InstallCommand,
+        'egg_info': EggInfoCommand,
     },
     python_requires=">=3.7",
 )
