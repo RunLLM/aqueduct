49087a0aa62708745b7702b0d3b87d83ec3c8460
Ubuntu <ubuntu@ip-172-31-44-121.us-east-2.compute.internal>
2022-06-29 16:45:03 +0000

    fix

--- integration_tests/sdk/requirement_test.py
+++ integration_tests/sdk/requirement_test.py
@@ -19,12 +19,12 @@ def check_if_transformers_exist():
 
 
 @op(reqs_path=INVALID_REQUIREMENTS_PATH)
-def invalid_valid_path_operator(table: pd.DataFrame) -> pd.DataFrame:
+def sentiment_prediction_with_invalid_reqs_path(table: pd.DataFrame) -> pd.DataFrame:
     return table
 
 
 @op(reqs_path=VALID_REQUIREMENTS_PATH)
-def valid_sentiment_prediction(reviews: pd.DataFrame) -> pd.DataFrame:
+def sentiment_prediction_with_valid_reqs_path(reviews: pd.DataFrame) -> pd.DataFrame:
     import transformers
 
     model = transformers.pipeline("sentiment-analysis")
@@ -32,7 +32,7 @@ def valid_sentiment_prediction(reviews: pd.DataFrame) -> pd.DataFrame:
 
 
 @op()
-def default_sentiment_prediction(reviews: pd.DataFrame) -> pd.DataFrame:
+def sentiment_prediction_without_reqs_path(reviews: pd.DataFrame) -> pd.DataFrame:
     import transformers
 
     model = transformers.pipeline("sentiment-analysis")
@@ -43,24 +43,20 @@ def test_invalid_path_operator(client):
     db = client.integration(name=get_integration_name())
     table = db.sql(query=SENTIMENT_SQL_QUERY)
     with pytest.raises(FileNotFoundError):
-        invalid_path_table = invalid_valid_path_operator(table)
+        invalid_path_table = sentiment_prediction_with_invalid_reqs_path(table)
 
 
 @pytest.mark.skipif(
     condition=check_if_transformers_exist(),
     reason="the transformers package already exists so the error can't be triggered.",
 )
-def test_default_path_operator(client):
+def test_path_operator(client):
     db = client.integration(name=get_integration_name())
     table = db.sql(query=SENTIMENT_SQL_QUERY)
-    default_path_table = default_sentiment_prediction(table)
+    # test if operator with default path will error out because of missing requirement package
+    default_path_table = sentiment_prediction_without_reqs_path(table)
     with pytest.raises(AqueductError):
         default_path_table.get()
-
-
-@pytest.mark.last
-def test_valid_path_operator_with_requirement(client):
-    db = client.integration(name=get_integration_name())
-    table = db.sql(query=SENTIMENT_SQL_QUERY)
-    valid_path_table = valid_sentiment_prediction(table)
+    # test if operator with valid requirement path will install the package and return the correct dataframe
+    valid_path_table = sentiment_prediction_with_valid_reqs_path(table)
     assert valid_path_table.get().shape[0] == 100
