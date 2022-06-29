b7b6f1a14b8b9d3f72bd0dfd1a76ac00bf1fb493
Kenneth Xu <kenxu95@gmail.com>
2022-06-28 17:43:39 -0700

    revert some

--- src/python/aqueduct_executor/operators/connectors/tabular/main.py
+++ src/python/aqueduct_executor/operators/connectors/tabular/main.py
@@ -12,10 +12,10 @@ from aqueduct_executor.operators.utils.storage.parse import parse_storage
 from aqueduct_executor.operators.utils.storage.storage import Storage
 
 try:
-    pass
+    from typing import Literal
 except ImportError:
     # Python 3.7 does not support typing.Literal
-    pass
+    from typing_extensions import Literal
 
 from aqueduct_executor.operators.connectors.tabular import common, config
 from aqueduct_executor.operators.utils import enums
