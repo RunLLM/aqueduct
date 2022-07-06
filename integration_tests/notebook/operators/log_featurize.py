473131658b772be2c122be4e7c68f1acb2863526
cw75 <cgwu0530@gmail.com>
2022-07-06 19:18:48 +0000

    lint

--- integration_tests/notebook/operators/log_featurize.py
+++ integration_tests/notebook/operators/log_featurize.py
@@ -5,11 +5,11 @@ import pandas as pd
 
 
 @op(file_dependencies=["./data"])
-def log_featurize(cust: pd.DataFrame) -> pd.DataFrame: 
+def log_featurize(cust: pd.DataFrame) -> pd.DataFrame:
     if not os.path.exists("data"):
         raise Exception("Data does not exist!")
     features = cust.copy()
-    skip_cols = ['cust_id', 'using_deep_learning', 'using_dbt']
+    skip_cols = ["cust_id", "using_deep_learning", "using_dbt"]
     for col in features.columns.difference(skip_cols):
-        features["log_"+col] = np.log(features[col] + 1.0)
+        features["log_" + col] = np.log(features[col] + 1.0)
     return features.drop(columns="cust_id")
