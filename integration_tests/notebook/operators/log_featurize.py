9f16347fc0e8f972fc66de97493af35ca44ef995
cw75 <cgwu0530@gmail.com>
2022-07-06 19:11:32 +0000

    add integration test notebook

---
+++ integration_tests/notebook/operators/log_featurize.py
@@ -0,0 +1,15 @@
+from aqueduct import op
+import numpy as np
+import os
+import pandas as pd
+
+
+@op(file_dependencies=["./data"])
+def log_featurize(cust: pd.DataFrame) -> pd.DataFrame: 
+    if not os.path.exists("data"):
+        raise Exception("Data does not exist!")
+    features = cust.copy()
+    skip_cols = ['cust_id', 'using_deep_learning', 'using_dbt']
+    for col in features.columns.difference(skip_cols):
+        features["log_"+col] = np.log(features[col] + 1.0)
+    return features.drop(columns="cust_id")
