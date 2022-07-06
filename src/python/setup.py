3abfa02ecaf346110dbdd4811d0539c09ae22674
cw75 <cgwu0530@gmail.com>
2022-07-06 22:23:13 +0000

    update version number to 0.0.4

--- src/python/setup.py
+++ src/python/setup.py
@@ -245,7 +245,7 @@ long_description = open(readme_path).read()
 
 setup(
     name="aqueduct-ml",
-    version="0.0.4",
+    version=package_version,
     install_requires=install_requires,
     scripts=["bin/aqueduct"],
     packages=find_packages(),
