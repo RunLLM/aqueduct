fb1fc7074d8c24ac978b4cf5aa7cb5e6cd14b056
cw75 <cgwu0530@gmail.com>
2022-07-07 04:20:16 +0000

    add header stripping

---
+++ src/golang/cmd/server/middleware/header_stripping/header_stripping.go
@@ -0,0 +1,36 @@
+package header_stripping
+
+import (
+	"net/http"
+
+	"github.com/aqueducthq/aqueduct/cmd/server/routes"
+)
+
+func StripHeader() func(http.Handler) http.Handler {
+	return func(h http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			var usefulHeaders = map[string]bool{
+				routes.ContentTypeHeader:        true,
+				routes.ApiKeyHeader:             true,
+				routes.SdkClientVersionHeader:   true,
+				routes.IntegrationNameHeader:    true,
+				routes.IntegrationServiceHeader: true,
+				routes.IntegrationConfigHeader:  true,
+				routes.TableNameHeader:          true,
+			}
+			toRemove := []string{}
+			// Loop over header names
+			for name := range r.Header {
+				if _, ok := usefulHeaders[name]; !ok {
+					toRemove = append(toRemove, name)
+				}
+			}
+
+			for _, header := range toRemove {
+				r.Header.Del(header)
+			}
+
+			h.ServeHTTP(w, r)
+		})
+	}
+}
