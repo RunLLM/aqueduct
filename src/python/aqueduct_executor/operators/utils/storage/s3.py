a5a7bd19e4cd552598a1fda60eaa64ad52c69e6a
Saurav Chhatrapati <sauravc@berkeley.edu>
2022-07-08 16:02:09 -0400

    Refactors Python s3 access to use s3 path

--- src/python/aqueduct_executor/operators/utils/storage/s3.py
+++ src/python/aqueduct_executor/operators/utils/storage/s3.py
@@ -1,10 +1,11 @@
 import os
-from typing import Any
+from typing import Any, Tuple
 
 import boto3
+from botocore.config import Config as BotoConfig
+
 from aqueduct_executor.operators.utils.storage.config import S3StorageConfig
 from aqueduct_executor.operators.utils.storage.storage import Storage
-from botocore.config import Config as BotoConfig
 
 
 class S3Storage(Storage):
@@ -19,10 +20,20 @@ class S3Storage(Storage):
         self._client = boto3.client("s3", config=BotoConfig(region_name=config.region))
         self._config = config
 
+        bucket, key_prefix = parse_s3_path(self._config.bucket)
+        self._bucket = bucket
+        self._key_prefix = key_prefix
+
     def put(self, key: str, value: bytes) -> None:
         print(f"writing to s3: {key}")
-        self._client.put_object(Bucket=self._config.bucket, Key=key, Body=value)
+        self._client.put_object(Bucket=self._bucket, Key="/".join(self._key_prefix, key), Body=value)
 
     def get(self, key: str) -> bytes:
         print(f"reading from s3: {key}")
-        return self._client.get_object(Bucket=self._config.bucket, Key=key)["Body"].read()  # type: ignore
+        return self._client.get_object(Bucket=self._bucket, Key="/".join(self._key_prefix, key))["Body"].read()
+    
+def parse_s3_path(s3_path: str) -> Tuple[str, str]:
+    path_parts=s3_path.replace("s3://","").split("/")
+    bucket=path_parts.pop(0)
+    key="/".join(path_parts)
+    return bucket, key
