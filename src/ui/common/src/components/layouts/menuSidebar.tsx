e12fb34e512b8997402158bf9e661eb5da739263
Andre Giron <andre@spiralai.co>
2022-07-13 16:25:39 -0700

    Adds logo to the menu sidebar

--- src/ui/common/src/components/layouts/menuSidebar.tsx
+++ src/ui/common/src/components/layouts/menuSidebar.tsx
@@ -179,9 +179,7 @@ const MenuSidebar: React.FC<{ user: UserProfile }> = ({ user }) => {
           underline="none"
           component={RouterLink as any}
         >
-          <Typography variant="h3" sx={{ color: 'white' }}>
-            Aqueduct
-          </Typography>
+          <img style={{ maxWidth: '130px', width: '130px' }} src="https://aqueduct-public-assets-bucket.s3.us-east-2.amazonaws.com/webapp/logos/aqueduct_logo_horizontal.png" />
         </Link>
 
         {/* popover target */}
