4433e912cd8153843ed060b58fcfe4e533c49717
Eunice Chan <ekchan@berkeley.edu>
2022-07-14 15:46:08 -0700

    Remove default conf vals

--- integration_tests/backend/conftest.py
+++ integration_tests/backend/conftest.py
@@ -11,8 +11,8 @@ ADAPTER_ENV_NAME = "ADAPTER"
 
 def pytest_configure(config):
     pytest.api_key = os.getenv(API_KEY_ENV_NAME)
-    pytest.server_address = os.getenv(SERVER_ADDR_ENV_NAME) or "localhost:8080"
-    pytest.adapter = os.getenv(ADAPTER_ENV_NAME) or "http://"
+    pytest.server_address = os.getenv(SERVER_ADDR_ENV_NAME)
+    pytest.adapter = os.getenv(ADAPTER_ENV_NAME)
 
     if pytest.api_key is None or pytest.server_address is None or pytest.adapter is None:
         raise Exception(
