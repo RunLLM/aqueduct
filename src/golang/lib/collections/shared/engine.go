960a94fbb8203170d380ac6a520334fdea447592
Saurav Chhatrapati <sauravc@berkeley.edu>
2022-07-18 16:12:41 -0400

    Adds engine_config to workflow_dag object

--- src/golang/lib/collections/shared/engine.go
+++ src/golang/lib/collections/shared/engine.go
@@ -4,20 +4,32 @@ import (
 	"database/sql/driver"
 
 	"github.com/aqueducthq/aqueduct/lib/collections/utils"
+	"github.com/google/uuid"
 )
 
 type EngineType string
 
 const (
 	AqueductEngineType EngineType = "aqueduct"
+	AirflowEngineType  EngineType = "airflow"
 )
 
 type EngineConfig struct {
 	Type           EngineType      `yaml:"type" json:"type"`
 	AqueductConfig *AqueductConfig `yaml:"aqueductConfig" json:"aqueduct_config,omitempty"`
+	AirflowConfig  *AirflowConfig  `yaml:"airflowConfig" json:"airflow_config,omitempty"`
 }
 
 type AqueductConfig struct{}
+
+type AirflowConfig struct {
+	IntegrationId              uuid.UUID
+	DagId                      string
+	OperatorToTask             map[uuid.UUID]string
+	OperatorMetadataPathPrefix map[uuid.UUID]string
+	ArtifactContentPathPrefix  map[uuid.UUID]string
+	ArtifactMetadataPathPrefix map[uuid.UUID]string
+}
 
 func (e *EngineConfig) Scan(value interface{}) error {
 	return utils.ScanJsonB(value, e)
