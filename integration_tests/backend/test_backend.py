3945ee98657e442ddf7904fa6fd7a8e066cc8297
Eunice Chan <ekchan@berkeley.edu>
2022-07-19 12:23:33 -0700

    Update

--- integration_tests/backend/test_backend.py
+++ integration_tests/backend/test_backend.py
@@ -66,7 +66,8 @@ class TestBackend:
             ('table_2', 'append'),
         ])
         assert set([(item['table_name'], item['update_mode'])for item in data]) == data_set
-        
+
         # Check all in same integration
         assert len(set([item['integration_id'] for item in data])) == 1
         assert len(set([item['service'] for item in data])) == 1
+        
