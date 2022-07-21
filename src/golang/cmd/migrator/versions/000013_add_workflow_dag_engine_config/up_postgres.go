9dc176a41a67d1fb46894cea5bcce2c998087d22
Saurav Chhatrapati <sauravc@berkeley.edu>
2022-07-21 13:47:15 -0400

    Fixes json tags

--- src/golang/cmd/migrator/versions/000013_add_workflow_dag_engine_config/up_postgres.go
+++ src/golang/cmd/migrator/versions/000013_add_workflow_dag_engine_config/up_postgres.go
@@ -3,4 +3,4 @@ package _000013_add_workflow_dag_engine_config
 const upPostgresScript = `
 ALTER TABLE workflow_dag
 ADD COLUMN engine_config JSONB NOT NULL
-DEFAULT '{"type":"aqueduct", "aqueductConfig":{}}'::jsonb;`
+DEFAULT '{"type":"aqueduct", "aqueduct_config":{}}'::jsonb;`
--- src/golang/cmd/migrator/versions/000013_add_workflow_dag_engine_config/up_sqlite.go
+++ src/golang/cmd/migrator/versions/000013_add_workflow_dag_engine_config/up_sqlite.go
@@ -2,7 +2,6 @@ package _000013_add_workflow_dag_engine_config
 
 const sqliteScript = `
 ALTER TABLE workflow_dag
-ADD COLUMN engine_config BLOB
-DEFAULT '{"type":"aqueduct", "aqueductConfig":{}}'
-NOT NULL;
+ADD COLUMN engine_config BLOB NOT NULL
+DEFAULT '{"type":"aqueduct", "aqueduct_config":{}}';
 `
