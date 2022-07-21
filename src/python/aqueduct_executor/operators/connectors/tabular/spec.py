333c5f64a0f2def74ffe4ad6c6f9691011a75d50
Saurav Chhatrapati <sauravc@berkeley.edu>
2022-07-21 13:37:52 -0400

    Linting

--- src/python/aqueduct_executor/operators/connectors/tabular/spec.py
+++ src/python/aqueduct_executor/operators/connectors/tabular/spec.py
@@ -52,7 +52,7 @@ def unwrap_connector_config(cls, connector_config, values):  # type: ignore
     if "conf" not in connector_config:
         # There is no inner `conf` dictionary to unwrap
         # This occurs when the spec is serialized in Python to run operators
-        # on other engines. 
+        # on other engines.
         return connector_config
 
     # This is a static config
