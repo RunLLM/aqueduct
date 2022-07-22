c5d361c8e55b3a3724cf6e1de3403fcc7fee5f63
Saurav Chhatrapati <sauravc@berkeley.edu>
2022-07-22 17:15:53 -0400

    Changes column add to use golang instead of SQL

--- src/golang/cmd/migrator/versions/000013_add_workflow_dag_engine_config/main.go
+++ src/golang/cmd/migrator/versions/000013_add_workflow_dag_engine_config/main.go
@@ -15,5 +15,20 @@ func DownPostgres(ctx context.Context, db database.Database) error {
 }
 
 func UpSqlite(ctx context.Context, db database.Database) error {
-	return db.Execute(ctx, sqliteScript)
+	// SQLite doesn't easily allow for inserting JSON data as a raw query,
+	// so we must split up the column add into 3 steps.
+
+	// Step 1: Add the column and allow it to take on NULL values
+	addColumnStmt := "ALTER TABLE workflow_dag ADD COLUMN engine_config BLOB;"
+	if err := db.Execute(ctx, addColumnStmt); err != nil {
+		return err
+	}
+
+	// Step 2: For each row with NULL `engine_config`, set it to the default value
+	if err := setDefaultEngineConfig(ctx, db); err != nil {
+		return err
+	}
+
+	// Step 3: Change `engine_config` to be NOT NULL
+	return makeEngineConfigNotNull(ctx, db)
 }
--- src/golang/cmd/migrator/versions/000013_add_workflow_dag_engine_config/up_sqlite.go
+++ src/golang/cmd/migrator/versions/000013_add_workflow_dag_engine_config/up_sqlite.go
@@ -1,7 +1,49 @@
 package _000013_add_workflow_dag_engine_config
 
-const sqliteScript = `
-ALTER TABLE workflow_dag
-ADD COLUMN engine_config BLOB NOT NULL
-DEFAULT '{"type":"aqueduct", "aqueduct_config":{}}';
-`
+import (
+	"context"
+
+	"github.com/aqueducthq/aqueduct/lib/database"
+)
+
+// setDefaultEngineConfig sets the `engine_config` column for the `workflow_dag` table
+// to the default Aqueduct engine for all rows with a NULL `engine_config`.
+func setDefaultEngineConfig(ctx context.Context, db database.Database) error {
+	query := "UPDATE workflow_dag SET engine_config = $1 WHERE engine_config = NULL;"
+
+	defaultEngineConfig := &EngineConfig{
+		Type:           AqueductEngineType,
+		AqueductConfig: &AqueductConfig{},
+	}
+	args := []interface{}{defaultEngineConfig}
+
+	return db.Execute(ctx, query, args...)
+}
+
+// makeEngineConfigNotNull changes the `engine_config` column to be NOT NULL
+func makeEngineConfigNotNull(ctx context.Context, db database.Database) error {
+	query := `
+	BEGIN TRANSACTION;
+
+	ALTER TABLE workflow_dag RENAME TO tmp_workflow_dag;
+
+	CREATE TABLE workflow_dag (
+		id BLOB NOT NULL PRIMARY KEY,
+		workflow_id BLOB NOT NULL REFERENCES workflow (id),
+		s3_config BLOB NOT NULL,
+		created_at DATETIME NOT NULL,
+		storage_config BLOB NOT NULL,
+		engine_config BLOB NOT NULL
+	);
+
+	INSERT INTO workflow_dag(id, workflow_id, s3_config, created_at, storage_config, engine_config)
+	SELECT id, workflow_id, s3_config, created_at, storage_config, engine_config
+	FROM tmp_workflow_dag;
+
+	DROP TABLE tmp_workflow_dag;
+
+	COMMIT;
+	`
+
+	return db.Execute(ctx, query)
+}
