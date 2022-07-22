a74a7ddfa2f60bd50c6ecf5703f435d4050b2074
Saurav Chhatrapati <sauravc@berkeley.edu>
2022-07-22 17:32:48 -0400

    Fixes column add

--- src/golang/cmd/migrator/versions/000013_add_workflow_dag_engine_config/up_sqlite.go
+++ src/golang/cmd/migrator/versions/000013_add_workflow_dag_engine_config/up_sqlite.go
@@ -9,7 +9,7 @@ import (
 // setDefaultEngineConfig sets the `engine_config` column for the `workflow_dag` table
 // to the default Aqueduct engine for all rows with a NULL `engine_config`.
 func setDefaultEngineConfig(ctx context.Context, db database.Database) error {
-	query := "UPDATE workflow_dag SET engine_config = $1 WHERE engine_config = NULL;"
+	query := "UPDATE workflow_dag SET engine_config = $1 WHERE engine_config IS NULL;"
 
 	defaultEngineConfig := &EngineConfig{
 		Type:           AqueductEngineType,
@@ -23,26 +23,21 @@ func setDefaultEngineConfig(ctx context.Context, db database.Database) error {
 // makeEngineConfigNotNull changes the `engine_config` column to be NOT NULL
 func makeEngineConfigNotNull(ctx context.Context, db database.Database) error {
 	query := `
-	BEGIN TRANSACTION;
-
 	ALTER TABLE workflow_dag RENAME TO tmp_workflow_dag;
 
 	CREATE TABLE workflow_dag (
 		id BLOB NOT NULL PRIMARY KEY,
 		workflow_id BLOB NOT NULL REFERENCES workflow (id),
-		s3_config BLOB NOT NULL,
 		created_at DATETIME NOT NULL,
 		storage_config BLOB NOT NULL,
 		engine_config BLOB NOT NULL
 	);
 
-	INSERT INTO workflow_dag(id, workflow_id, s3_config, created_at, storage_config, engine_config)
-	SELECT id, workflow_id, s3_config, created_at, storage_config, engine_config
+	INSERT INTO workflow_dag(id, workflow_id, created_at, storage_config, engine_config)
+	SELECT id, workflow_id, created_at, storage_config, engine_config
 	FROM tmp_workflow_dag;
 
 	DROP TABLE tmp_workflow_dag;
-
-	COMMIT;
 	`
 
 	return db.Execute(ctx, query)
