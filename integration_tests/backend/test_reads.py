8498bad96464f70fcb5597b88744c76fa7c76d8d
Eunice Chan <ekchan@berkeley.edu>
2022-07-21 20:22:00 -0700

    tests

--- integration_tests/backend/test_reads.py
+++ integration_tests/backend/test_reads.py
@@ -61,7 +61,7 @@ class TestReads:
         endpoint = self.GET_WORKFLOW_TABLES_TEMPLATE % self.flows["changing_saves.py"]
         data = self.get_response_class(endpoint)
       
-        data = data.json()["table_details"]
+        data = data.json()["object_details"]
 
         assert len(data) == 3
         
@@ -73,7 +73,7 @@ class TestReads:
                 ("table_2", "append"),
             ]
         )
-        assert set([(item["table_name"], item["update_mode"]) for item in data]) == data_set
+        assert set([(item["object_name"], item["update_mode"]) for item in data]) == data_set
 
         # Check all in same integration
         assert len(set([item["integration_id"] for item in data])) == 1
