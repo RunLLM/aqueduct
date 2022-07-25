8dab86091e16a06ed973bbfe9d02c1cfc6e76de5
Saurav Chhatrapati <sauravc@berkeley.edu>
2022-07-25 12:40:26 -0400

    Removes unused airflow code

--- src/golang/lib/airflow/utils.go
+++ src/golang/lib/airflow/utils.go
@@ -1,10 +1,7 @@
 package airflow
 
 import (
-	"net/http"
 	"strings"
-
-	"github.com/dropbox/godropbox/errors"
 )
 
 // generateDagId generates an Airflow DAG ID for a workflow.
@@ -15,10 +12,4 @@ func generateDagId(workflowName string) string {
 // generateTaskId generates an Airflow task ID for an operator.
 func generateTaskId(operatorName string) string {
 	return strings.ReplaceAll(operatorName, " ", "_")
-}
-
-// wrapApiErrors wraps an error from the Airflow API using the error returned
-// and the HTTP response.
-func wrapApiError(err error, api string, resp *http.Response) error {
-	return errors.Wrapf(err, "Airflow %v error with status: %v", api, resp.Status)
 }
