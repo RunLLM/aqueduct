fb1bf726d7a589fb6be95c9cb3a3b86514fb3884
Kenneth Xu <kenxu95@gmail.com>
2022-07-27 11:26:33 -0700

    coming back

--- integration_tests/sdk/requirement_test.py
+++ integration_tests/sdk/requirement_test.py
@@ -18,19 +18,6 @@ def check_if_transformers_exist():
     return True
 
 
-@op(reqs_path=INVALID_REQUIREMENTS_PATH)
-def sentiment_prediction_with_invalid_reqs_path(table: pd.DataFrame) -> pd.DataFrame:
-    return table
-
-
-@op(reqs_path=VALID_REQUIREMENTS_PATH)
-def sentiment_prediction_with_valid_reqs_path(reviews: pd.DataFrame) -> pd.DataFrame:
-    import transformers
-
-    model = transformers.pipeline("sentiment-analysis")
-    return reviews.join(pd.DataFrame(model(list(reviews["review"]))))
-
-
 @op()
 def sentiment_prediction_without_reqs_path(reviews: pd.DataFrame) -> pd.DataFrame:
     import transformers
@@ -39,25 +26,35 @@ def sentiment_prediction_without_reqs_path(reviews: pd.DataFrame) -> pd.DataFram
     return reviews.join(pd.DataFrame(model(list(reviews["review"]))))
 
 
-def test_invalid_path_operator(client):
+def test_bad_requirements_path(client):
+    @op(requirements=INVALID_REQUIREMENTS_PATH)
+    def sentiment_prediction_with_invalid_reqs_path(table: pd.DataFrame) -> pd.DataFrame:
+        return table
+
     db = client.integration(name=get_integration_name())
     table = db.sql(query=SENTIMENT_SQL_QUERY)
     with pytest.raises(FileNotFoundError):
-        invalid_path_table = sentiment_prediction_with_invalid_reqs_path(table)
+        _ = sentiment_prediction_with_invalid_reqs_path(table)
 
 
-# TODO(ENG-1217): Straighten out our requirements story and testing.
-# @pytest.mark.skipif(
-#     condition=check_if_transformers_exist(),
-#     reason="the transformers package already exists so the error can't be triggered.",
-# )
-# def test_path_operator(client):
-#     db = client.integration(name=get_integration_name())
-#     table = db.sql(query=SENTIMENT_SQL_QUERY)
-#     # test if operator with default path will error out because of missing requirement package
-#     default_path_table = sentiment_prediction_without_reqs_path(table)
-#     with pytest.raises(AqueductError):
-#         default_path_table.get()
-#     # test if operator with valid requirement path will install the package and return the correct dataframe
-#     valid_path_table = sentiment_prediction_with_valid_reqs_path(table)
-#     assert valid_path_table.get().shape[0] == 100
+def test_requirements_installation_from_path(client):
+    @op(requirements=VALID_REQUIREMENTS_PATH)
+    def sentiment_prediction_with_valid_reqs_path(reviews: pd.DataFrame) -> pd.DataFrame:
+        import transformers
+        model = transformers.pipeline("sentiment-analysis")
+        return reviews.join(pd.DataFrame(model(list(reviews["review"]))))
+
+    db = client.integration(name=get_integration_name())
+    table = db.sql(query=SENTIMENT_SQL_QUERY)
+    # test if operator with default path will error out because of missing requirement package
+    default_path_table = sentiment_prediction_without_reqs_path(table)
+    with pytest.raises(AqueductError):
+        default_path_table.get()
+
+    # test if operator with valid requirement path will install the package and return the correct dataframe
+    valid_path_table = sentiment_prediction_with_valid_reqs_path(table)
+    assert valid_path_table.get().shape[0] == 100
+
+# TODO: test requirements by listing strings.
+# TODO: test default requirements path success.
+# TODO: test requirements inference - you can check this with the high level `infer_requirements` call.
