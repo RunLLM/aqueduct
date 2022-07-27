52c0203cd7be8c34a2129ab804bcc76f552ed488
Eunice Chan <ekchan@berkeley.edu>
2022-07-27 10:19:22 -0700

    lint

--- integration_tests/sdk/delete_workflow_test.py
+++ integration_tests/sdk/delete_workflow_test.py
@@ -1,10 +1,17 @@
 import pytest
 from constants import SENTIMENT_SQL_QUERY
-from utils import delete_flow, generate_new_flow_name, get_integration_name, run_flow_test, get_response
+from utils import (
+    delete_flow,
+    generate_new_flow_name,
+    get_integration_name,
+    get_response,
+    run_flow_test,
+)
 
 from aqueduct import LoadUpdateMode
 
 LIST_INTEGRATION_OBJECTS_TEMPLATE = "/api/integration/%s/objects"
+
 
 @pytest.mark.publish
 def test_delete_workflow_invalid_saved_objects(client):
@@ -32,6 +39,7 @@ def test_delete_workflow_invalid_saved_objects(client):
     finally:
         delete_flow(client, flow_id)
 
+
 @pytest.mark.publish
 def test_delete_workflow_saved_objects(client):
     integration = client.integration(name=get_integration_name())
@@ -45,13 +53,17 @@ def test_delete_workflow_saved_objects(client):
 
     table.save(integration.config(table="delete_table", update_mode=LoadUpdateMode.REPLACE))
 
-    flow_ids_to_delete.add(run_flow_test(client, [table], name=name, num_runs=1, delete_flow_after=False).id())
+    flow_ids_to_delete.add(
+        run_flow_test(client, [table], name=name, num_runs=1, delete_flow_after=False).id()
+    )
 
     ###
 
     table.save(integration.config(table="delete_table", update_mode=LoadUpdateMode.APPEND))
 
-    flow_ids_to_delete.add(run_flow_test(client, [table], name=name, num_runs=1, delete_flow_after=False).id())
+    flow_ids_to_delete.add(
+        run_flow_test(client, [table], name=name, num_runs=1, delete_flow_after=False).id()
+    )
 
     ###
 
@@ -60,23 +72,23 @@ def test_delete_workflow_saved_objects(client):
         flow_id = list(flow_ids_to_delete)[0]
         tables = client.flow(flow_id).list_saved_objects()
 
-        assert 'delete_table' in tables[get_integration_name()]
+        assert "delete_table" in tables[get_integration_name()]
 
         tables_response = get_response(client, endpoint).json()
 
-        assert 'delete_table' in set(tables_response['object_names'])
+        assert "delete_table" in set(tables_response["object_names"])
 
         with pytest.raises(InvalidRequestError) as e_info:
             client.delete_flow(flow_id, saved_objects_to_delete=tables, force=False)
-        
+
         client.delete_flow(flow_id, saved_objects_to_delete=tables, force=True)
 
         # Wait for deletion to occur
         sleep(1)
 
         tables_response = get_response(client, endpoint).json()
-        
-        assert 'delete_table' not in set(tables_response['object_names'])
+
+        assert "delete_table" not in set(tables_response["object_names"])
 
     finally:
         for flow_id in flow_ids_to_delete:
