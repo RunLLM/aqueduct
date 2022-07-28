433ecee8a7df6aee9333aedf01652faaa2a61362
Ubuntu <ubuntu@ip-172-31-44-121.us-east-2.compute.internal>
2022-07-28 19:23:00 +0000

    refresh_workflow

--- src/golang/cmd/server/handler/refresh_workflow.go
+++ src/golang/cmd/server/handler/refresh_workflow.go
@@ -7,11 +7,23 @@ import (
 
 	"github.com/aqueducthq/aqueduct/cmd/server/request"
 	"github.com/aqueducthq/aqueduct/cmd/server/routes"
+	"github.com/aqueducthq/aqueduct/lib/collections/artifact"
+	"github.com/aqueducthq/aqueduct/lib/collections/artifact_result"
+	"github.com/aqueducthq/aqueduct/lib/collections/notification"
+	"github.com/aqueducthq/aqueduct/lib/collections/operator"
+	"github.com/aqueducthq/aqueduct/lib/collections/operator_result"
+	"github.com/aqueducthq/aqueduct/lib/collections/shared"
+	"github.com/aqueducthq/aqueduct/lib/collections/user"
 	"github.com/aqueducthq/aqueduct/lib/collections/workflow"
+	"github.com/aqueducthq/aqueduct/lib/collections/workflow_dag"
+	"github.com/aqueducthq/aqueduct/lib/collections/workflow_dag_edge"
+	"github.com/aqueducthq/aqueduct/lib/collections/workflow_dag_result"
 	aq_context "github.com/aqueducthq/aqueduct/lib/context"
 	"github.com/aqueducthq/aqueduct/lib/database"
 	"github.com/aqueducthq/aqueduct/lib/job"
 	"github.com/aqueducthq/aqueduct/lib/vault"
+	dag_utils "github.com/aqueducthq/aqueduct/lib/workflow/dag"
+	"github.com/aqueducthq/aqueduct/lib/workflow/engine"
 	"github.com/aqueducthq/aqueduct/lib/workflow/operator/connector/github"
 	"github.com/dropbox/godropbox/errors"
 	"github.com/go-chi/chi"
@@ -28,11 +40,21 @@ type RefreshWorkflowArgs struct {
 type RefreshWorkflowHandler struct {
 	PostHandler
 
-	Database       database.Database
-	JobManager     job.JobManager
-	GithubManager  github.Manager
-	Vault          vault.Vault
-	WorkflowReader workflow.Reader
+	Database                database.Database
+	JobManager              job.JobManager
+	GithubManager           github.Manager
+	Vault                   vault.Vault
+	StorageConfig           *shared.StorageConfig
+	OperatorReader          operator.Reader
+	ArtifactReader          artifact.Reader
+	ArtifactResultWriter    artifact_result.Writer
+	OperatorResultWriter    operator_result.Writer
+	NotificationWriter      notification.Writer
+	WorkflowDagEdgeReader   workflow_dag_edge.Reader
+	WorkflowDagResultWriter workflow_dag_result.Writer
+	WorkflowReader          workflow.Reader
+	WorkflowDagReader       workflow_dag.Reader
+	UserReader              user.Reader
 }
 
 func (*RefreshWorkflowHandler) Name() string {
@@ -85,38 +107,71 @@ func generateWorkflowJobName() string {
 
 func (h *RefreshWorkflowHandler) Perform(ctx context.Context, interfaceArgs interface{}) (interface{}, int, error) {
 	args := interfaceArgs.(*RefreshWorkflowArgs)
+	
+	//extract the dag based on workflow ID provided.
+	dag, err := h.WorkflowDagReader.GetLatestWorkflowDag(ctx, args.WorkflowId, h.Database)
 
-	workflowObject, err := h.WorkflowReader.GetWorkflow(
-		ctx,
-		args.WorkflowId,
-		h.Database,
-	)
 	if err != nil {
-		if err == database.ErrNoRows {
-			return nil, http.StatusBadRequest, errors.New("Unable to find workflow.")
-		}
-		return nil, http.StatusInternalServerError, errors.New("Unable to find workflow.")
+		return nil, http.StatusInternalServerError, errors.Wrap(err, "Error creating dag object.")
 	}
 
-	jobName := generateWorkflowJobName()
-
-	jobSpec := job.NewWorkflowSpec(
-		workflowObject.Name,
-		workflowObject.Id.String(),
-		h.Database.Config(),
-		h.Vault.Config(),
-		h.JobManager.Config(),
-		h.GithubManager.Config(),
-		args.Parameters,
-	)
-
-	err = h.JobManager.Launch(
+	workflowDag, err := dag_utils.NewWorkflowDag(
 		ctx,
-		jobName,
-		jobSpec,
+		dag,
+		h.WorkflowDagResultWriter,
+		h.OperatorResultWriter,
+		h.ArtifactResultWriter,
+		h.WorkflowReader,
+		h.NotificationWriter,
+		h.UserReader,
+		h.JobManager,
+		h.Vault,
+		h.StorageConfig,
+		h.Database,
 	)
+
 	if err != nil {
-		return nil, http.StatusInternalServerError, errors.Wrap(err, "Unable to trigger this workflow.")
+		return nil, http.StatusInternalServerError, errors.Wrap(err, "Error creating dag object.")
+	}
+
+	eng, err := engine.NewAqEngine(
+		workflowDag,
+		h.Database,
+		h.GithubManager,
+		h.Vault,
+		h.JobManager,
+		engine.AqueductTimeConfig{
+			OperatorPollInterval: previewPollIntervalMillisec,
+			ExecTimeout:          engine.DefaultExecutionTimeout,
+			CleanupTimeout:       engine.DefaultCleanupTimeout,
+		},
+		true,
+	)
+
+	if err != nil {
+		return nil, http.StatusInternalServerError, errors.Wrap(err, "Error creating Aqueduct Engine.")
+	}
+
+	_ , err = eng.ExecuteWorkflow(ctx,workflowDag)
+	// jobName := generateWorkflowJobName()
+
+	// jobSpec := job.NewWorkflowSpec(
+	// 	workflowObject.Name,
+	// 	workflowObject.Id.String(),
+	// 	h.Database.Config(),
+	// 	h.Vault.Config(),
+	// 	h.JobManager.Config(),
+	// 	h.GithubManager.Config(),
+	// 	args.Parameters,
+	// )
+
+	// err = h.JobManager.Launch(
+	// 	ctx,
+	// 	jobName,
+	// 	jobSpec,
+	// )
+	if err != nil {
+	return nil, http.StatusInternalServerError, errors.Wrap(err, "Error executing the workflow.")
 	}
 	return struct{}{}, http.StatusOK, nil
 }
