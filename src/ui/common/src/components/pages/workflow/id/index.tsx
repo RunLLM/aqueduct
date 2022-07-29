a827ce1c8137d987f149180df9f1eba3d0b5d6fb
Vikram Sreekanti <vsreekanti@gmail.com>
2022-07-28 17:42:55 -0700

    [ENG-1354, 1415] Fixes bottom sidesheet and ReactFlow alignment issues (#267)

--- src/ui/common/src/components/pages/workflow/id/index.tsx
+++ src/ui/common/src/components/pages/workflow/id/index.tsx
@@ -300,13 +300,13 @@ const WorkflowPage: React.FC<WorkflowPageProps> = ({
             mt: 2,
             p: 3,
             mb: contentBottomOffsetInPx,
+            width: '100%',
+            boxSizing: 'border-box',
             backgroundColor: 'gray.50',
           }}
         >
           <ReactFlowProvider>
             <ReactFlowCanvas
-              nodes={dagPosition.result?.nodes}
-              edges={dagPosition.result?.edges}
               switchSideSheet={switchSideSheet}
               onPaneClicked={onPaneClicked}
             />
