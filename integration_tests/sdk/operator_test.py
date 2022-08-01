dda8873bb4e05c3418b205541efe435fcb9e8cb4
Ubuntu <ubuntu@ip-172-31-44-121.us-east-2.compute.internal>
2022-08-01 19:22:32 +0000

    update test

--- integration_tests/sdk/operator_test.py
+++ integration_tests/sdk/operator_test.py
@@ -2,6 +2,7 @@ from aqueduct.decorator import to_operator
 from constants import SENTIMENT_SQL_QUERY
 from aqueduct import op
 from utils import get_integration_name, run_sentiment_model, run_sentiment_model_function
+from function import dummy_sentiment_model_function
 
 
 def test_to_operator_local_function(client):
@@ -29,10 +30,11 @@ def test_to_operator_imported_function(client):
     db = client.integration(name=get_integration_name())
     sql_artifact = db.sql(query=SENTIMENT_SQL_QUERY)
 
-    output_artifact_from_decorator = run_sentiment_model(sql_artifact)
-    df_normal = output_artifact_from_decorator.get()
-    sentimen_model_function = run_sentiment_model_function()
-    output_artifact_from_to_operator = to_operator(func=sentimen_model_function,file_dependencies=["function.py"])(sql_artifact)
-    df_func = output_artifact_from_to_operator.get()
+    @op(file_dependencies=["function.py"])
+    def decorated_func(df):
+        df = dummy_sentiment_model_function(df)
+        return df
+    df_decorate = decorated_func(sql_artifact).get()
+    df_function = to_operator(dummy_sentiment_model_function)(sql_artifact).get()
 
-    assert df_normal["positivity"].equals(df_func["positivity"])
+    assert df_decorate["positivity"].equals(df_function["positivity"])
