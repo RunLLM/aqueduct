a85ebb3c71b3e1ab538b9f4568f81ac29a638402
Andre Giron <andre@spiralai.co>
2022-08-04 15:50:07 -0700

    Implements UI elements for Artifact details page. Adds KeyValueTable component to be used for Metrics and Checks data.

--- src/ui/common/src/components/tables/StickyHeaderTable.tsx
+++ src/ui/common/src/components/tables/StickyHeaderTable.tsx
@@ -92,7 +92,7 @@ export default function StickyHeaderTable() {
     };
 
     return (
-        <Paper sx={{ width: '100%', overflow: 'hidden' }}>
+        <Paper sx={{ width: '800px', overflow: 'hidden' }}>
             <TableContainer sx={{ maxHeight: 440, height: 440 }}>
                 <Table stickyHeader aria-label="sticky table">
                     <TableHead>
