f909e2525e06d655412f3233b9b802708a104e28
cw75 <cgwu0530@gmail.com>
2022-08-05 00:16:23 +0000

    update install script

--- scripts/install_local.py
+++ scripts/install_local.py
@@ -118,14 +118,14 @@ if __name__ == "__main__":
         UI_APP_PATH = UI_PATH + "/app"
         
         print("Updating UI files...")
+        execute_command(["rm", "-rf", "node_modules"], cwd=join(cwd, UI_COMMON_PATH))
         execute_command(["rm", "-rf", ".parcel-cache"], cwd=join(cwd, UI_COMMON_PATH))
         execute_command(["rm", "-rf", "dist"], cwd=join(cwd, UI_COMMON_PATH))
         execute_command(["npm", "install"], cwd=join(cwd, UI_COMMON_PATH))
-        execute_command(["npm", "run", "build"], cwd=join(cwd, UI_COMMON_PATH))
         execute_command(["npm", "link"], cwd=join(cwd, UI_COMMON_PATH))
+        execute_command(["rm", "-rf", "node_modules"], cwd=join(cwd, UI_APP_PATH))
         execute_command(["rm", "-rf", ".parcel-cache"], cwd=join(cwd, UI_APP_PATH))
         execute_command(["rm", "-rf", "dist"], cwd=join(cwd, UI_APP_PATH))
-        execute_command(["npm", "install"], cwd=join(cwd, UI_APP_PATH))
         execute_command(["npm", "link", "@aqueducthq/common"], cwd=join(cwd, UI_APP_PATH))
         execute_command(["make", "dist"], cwd=join(cwd, "src/ui"))
 
