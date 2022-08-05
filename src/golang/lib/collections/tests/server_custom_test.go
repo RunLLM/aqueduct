190393a1eb988b478eb069f7671ed0e43bbe035a
Saurav Chhatrapati <sauravc@berkeley.edu>
2022-08-05 15:43:07 -0400

    Adds test case

--- src/golang/lib/collections/tests/server_custom_test.go
+++ src/golang/lib/collections/tests/server_custom_test.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/aqueducthq/aqueduct/lib/collections/artifact/table"
 	"github.com/aqueducthq/aqueduct/lib/collections/operator/connector"
+	"github.com/aqueducthq/aqueduct/lib/collections/shared"
 
 	"github.com/aqueducthq/aqueduct/cmd/server/queries"
 	"github.com/aqueducthq/aqueduct/lib/collections/artifact"
@@ -69,4 +70,43 @@ func TestGetLoadOperatorSpecByOrganization(t *testing.T) {
 	require.Len(t, loadOpSpecResp, 1)
 
 	requireDeepEqual(t, expectedResponse, loadOpSpecResp[0])
+}
+
+func TestGetLatestWorkflowDagIdsByOrganizationIdAndEngine(t *testing.T) {
+	defer resetDatabase(t)
+
+	testWorkflows := seedWorkflow(t, 2)
+	require.Len(t, testWorkflows, 2)
+
+	testWorkflow1, testWorkflow2 := testWorkflows[0], testWorkflows[1]
+
+	// Create 2 workflow dags with a default Aqueduct engine
+	seedWorkflowDagWithWorkflows(t, 1, []uuid.UUID{testWorkflow1.Id})
+	seedWorkflowDagWithWorkflows(t, 1, []uuid.UUID{testWorkflow2.Id})
+
+	// Create another workflow dag for testWorkflow1 using an Airflow engine
+	airflowTestDag1, err := writers.workflowDagWriter.CreateWorkflowDag(
+		context.Background(),
+		testWorkflow1.Id,
+		&shared.StorageConfig{
+			Type: shared.FileStorageType,
+		},
+		&shared.EngineConfig{
+			Type:          shared.AirflowEngineType,
+			AirflowConfig: &shared.AirflowConfig{},
+		},
+		db,
+	)
+	require.Nil(t, err)
+
+	airflowDagIds, err := readers.serverReader.GetLatestWorkflowDagIdsByOrganizationIdAndEngine(
+		context.Background(),
+		testOrganizationId,
+		shared.AirflowEngineType,
+		db,
+	)
+	require.Nil(t, err)
+
+	require.Len(t, airflowDagIds, 1)
+	require.Equal(t, airflowTestDag1.Id, airflowDagIds[0].Id)
 }
