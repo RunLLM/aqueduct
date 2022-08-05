0a3427705f69845d3609e44f0a8638d966417bf9
Chenggang Wu <cgwu0530@gmail.com>
2022-08-05 14:55:29 -0700

    Use virtual table to improve render efficiency (#302)

--- src/ui/common/src/components/integrations/integrationObjectPreview.tsx
+++ src/ui/common/src/components/integrations/integrationObjectPreview.tsx
@@ -1,11 +1,11 @@
-import { Alert, Typography } from '@mui/material';
+import { Alert, AlertTitle, Typography } from '@mui/material';
 import Box from '@mui/material/Box';
 import CircularProgress from '@mui/material/CircularProgress';
 import React from 'react';
 
-import DataTable from '../../components/tables/dataTable';
 import { ObjectState } from '../../reducers/integration';
 import { isFailed, isLoading, isSucceeded } from '../../utils/shared';
+import DataTable from '../tables/DataTable';
 
 type Props = {
   objectName: string;
@@ -13,30 +13,56 @@ type Props = {
 };
 
 const IntegrationObjectPreview: React.FC<Props> = ({ objectName, object }) => {
-  return (
-    <Box sx={{ mt: 3 }}>
-      {isLoading(object.status) && (
-        <Box sx={{ display: 'flex', flexDirection: 'row', mt: 3 }}>
-          <CircularProgress size={30} />
-          <Typography sx={{ ml: 2 }}>
-            Loading object <b>{objectName}</b>...
-          </Typography>
-        </Box>
-      )}
-      {isFailed(object.status) && (
-        <Alert style={{ marginTop: '10px' }} severity="error">
-          Object <b>{objectName}</b> failed to load. Try refreshing the page.{' '}
-          <br />
-          Error: {object.status.err}
-        </Alert>
-      )}
-      {isSucceeded(object.status) && !!object.data && (
-        <Box sx={{ height: '50vh', width: '100%' }}>
-          <DataTable data={object.data} />
-        </Box>
-      )}
-    </Box>
-  );
+  let content: React.ReactElement;
+  if (isLoading(object.status)) {
+    content = (
+      <Box sx={{ display: 'flex', flexDirection: 'row', mt: 3 }}>
+        <CircularProgress size={30} />
+        <Typography sx={{ ml: 2 }}>
+          Loading object <b>{objectName}</b>...
+        </Typography>
+      </Box>
+    );
+  }
+
+  if (isFailed(object.status)) {
+    content = (
+      <Alert style={{ marginTop: '10px' }} severity="error">
+        <AlertTitle>
+          Object <b>{objectName}</b> failed to load.
+        </AlertTitle>
+        <pre>Error: {object.status.err}</pre>
+      </Alert>
+    );
+  }
+
+  if (isSucceeded(object.status) && !!object.data) {
+    const columnsContent = object.data.schema.fields.map((column) => {
+      return {
+        dataKey: column.name,
+        label: column.name,
+        type: column.type,
+      };
+    });
+
+    content = (
+      <Box
+        sx={{
+          height: '50vh',
+          width: '100%',
+          overflow: 'auto',
+          overflowY: 'hidden',
+        }}
+      >
+        <DataTable
+          rowCount={object.data.data.length}
+          rowGetter={({ index }) => object.data.data[index]}
+          columns={columnsContent}
+        />
+      </Box>
+    );
+  }
+  return <Box sx={{ mt: 3 }}>{content}</Box>;
 };
 
 export default IntegrationObjectPreview;
