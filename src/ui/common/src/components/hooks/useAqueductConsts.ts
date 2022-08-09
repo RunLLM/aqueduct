a3511ed83b24dba8baf55d209303463d6881d785
Andre Giron <andre@spiralai.co>
2022-08-09 11:57:07 -0700

    fix lint

--- src/ui/common/src/components/hooks/useAqueductConsts.ts
+++ src/ui/common/src/components/hooks/useAqueductConsts.ts
@@ -7,6 +7,8 @@ const DEFAULT_SERVER_ADDRESS = 'http://localhost:8080';
 export const useAqueductConsts = (): AqueductConsts => {
   return {
     // Use default value for server address if there is not one set in the .env or env.local file.
-    apiAddress: process.env.SERVER_ADDRESS ? process.env.SERVER_ADDRESS : DEFAULT_SERVER_ADDRESS,
+    apiAddress: process.env.SERVER_ADDRESS
+      ? process.env.SERVER_ADDRESS
+      : DEFAULT_SERVER_ADDRESS,
   };
 };
