d9f5c70b34a8b255ac4e0a96620d44f440e6734b
Andre Giron <andre@spiralai.co>
2022-08-09 11:11:30 -0700

    UI: Set default value for SERVER_ADDRESS if there is not an environment variable set for it.

--- src/ui/common/src/components/hooks/useAqueductConsts.ts
+++ src/ui/common/src/components/hooks/useAqueductConsts.ts
@@ -2,8 +2,11 @@ export type AqueductConsts = {
   apiAddress: string;
 };
 
+const DEFAULT_SERVER_ADDRESS = 'http://localhost:8080';
+
 export const useAqueductConsts = (): AqueductConsts => {
   return {
-    apiAddress: process.env.SERVER_ADDRESS,
+    // Use default value for server address if there is not one set in the .env or env.local file.
+    apiAddress: process.env.SERVER_ADDRESS ? process.env.SERVER_ADDRESS : DEFAULT_SERVER_ADDRESS,
   };
 };
