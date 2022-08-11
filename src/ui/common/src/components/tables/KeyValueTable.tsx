edf036c319ec96d18f2aa8b3902d2d1e5aa2ba46
Andre Giron <andre@spiralai.co>
2022-08-11 11:57:10 -0700

    Adds additional props to KeyValueTable

--- src/ui/common/src/components/tables/KeyValueTable.tsx
+++ src/ui/common/src/components/tables/KeyValueTable.tsx
@@ -1,6 +1,6 @@
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
+import TableCell, { TableCellProps } from '@mui/material/TableCell';
 import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
@@ -8,45 +8,48 @@ import * as React from 'react';
 
 import { Data, DataSchema } from '../../utils/data';
 
-const dataSchema: DataSchema = {
-  fields: [
-    { name: 'Title', type: 'varchar' },
-    { name: 'Value', type: 'varchar' },
-  ],
-  pandas_version: '0.0.1', // TODO: Figure out what to set this value to.
-};
+// const dataSchema: DataSchema = {
+//   fields: [
+//     { name: 'Title', type: 'varchar' },
+//     { name: 'Value', type: 'varchar' },
+//   ],
+//   pandas_version: '0.0.1', // TODO: Figure out what to set this value to.
+// };
 
-function createData(): Data {
-  return {
-    schema: dataSchema,
-    data: [
-      ['avg_churn', '0.04'],
-      ['avg_workflows', '455'],
-      ['avg_users', '1.2'],
-      ['avg_users', '5'],
-    ],
-  };
-}
+// function createData(): Data {
+//   return {
+//     schema: dataSchema,
+//     data: [
+//       ['avg_churn', '0.04'],
+//       ['avg_workflows', '455'],
+//       ['avg_users', '1.2'],
+//       ['avg_users', '5'],
+//     ],
+//   };
+// }
 
-const rows: Data = createData();
+// const rows: Data = createData();
 
 interface KeyValueTableProps {
-  data: Data;
+  rows: Data;
+  schema: DataSchema;
+  height?: string;
+  width?: string;
+  maxHeight?: string;
+  stickyHeader?: boolean;
+  tableAlign?: string;
 }
 
-const tableAlign = 'left';
-
-export const KeyValueTable: React.FC = () => {
+export const KeyValueTable: React.FC<KeyValueTableProps> = ({ rows, schema, height = "440px", width = "100%", maxHeight = "440px", stickyHeader = true, tableAlign = "left" }) => {
   return (
-    <TableContainer sx={{ maxHeight: 440, height: 440, width: '100%' }}>
-      <Table stickyHeader aria-label="sticky table">
+    <TableContainer sx={{ maxHeight, height, width }}>
+      <Table stickyHeader={stickyHeader} aria-label={stickyHeader ? "sticky table" : "table"}>
         <TableHead>
           <TableRow>
-            {dataSchema.fields.map((column, idx) => (
+            {schema.fields.map((column, idx) => (
               <TableCell
                 key={`${column.name}-heading-${idx}`}
-                align={tableAlign}
-                style={{}}
+                align={tableAlign as TableCellProps["align"]}
               >
                 {column.name}
               </TableCell>
@@ -62,14 +65,13 @@ export const KeyValueTable: React.FC = () => {
                 tabIndex={-1}
                 key={`tableBody-${rowIndex}`}
               >
-                {dataSchema.fields.map((column, columnIndex) => {
+                {schema.fields.map((column, columnIndex) => {
                   const value = row[columnIndex];
 
-                  // TODO: Consider making cell alignment a property to pass in.
                   return (
                     <TableCell
                       key={`cell-${rowIndex}-${columnIndex}`}
-                      align={tableAlign}
+                      align={tableAlign as TableCellProps["align"]}
                     >
                       {value}
                     </TableCell>
