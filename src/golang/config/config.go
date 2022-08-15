9fdfec08d6db1c4caa8fc3489b4fc81c4103f769
Saurav Chhatrapati <sauravc@berkeley.edu>
2022-08-15 15:20:19 -0400

    Finalizes global config

--- src/golang/config/config.go
+++ src/golang/config/config.go
@@ -25,22 +25,35 @@ type ServerConfiguration struct {
 }
 
 // AqueductPath is the filepath to the Aqueduct installation.
-func AqueductPath() string
+func AqueductPath() string {
+	return globalConfig.AqPath
+}
 
 // EncryptionKey is used for encrypting objects stored in the Aqueduct vault.
-func EncryptionKey() string
+func EncryptionKey() string {
+	return globalConfig.EncryptionKey
+}
 
 // RetentionJobPeriod defines how long to wait before garbage collecting workflow runs.
-func RetentionJobPeriod() string
+func RetentionJobPeriod() string {
+	return globalConfig.RetentionJobPeriod
+}
 
 // APIKey returns the API key the user must use when issueing requests.
-func APIKey() string
+func APIKey() string {
+	return globalConfig.ApiKey
+}
 
 // Storage returns the storage layer config.
-func Storage() shared.StorageConfig
+func Storage() shared.StorageConfig {
+	return *globalConfig.StorageConfig
+}
 
 // UpdateStorage updates the storage layer config.
-func UpdateStorage(newStorage *shared.StorageConfig) error
+func UpdateStorage(newStorage *shared.StorageConfig) error {
+	globalConfig.StorageConfig = newStorage
+	return dumpConfig()
+}
 
 // Init initializes the global server configuration. It must be invoked before
 // any config field is accessed, otherwise the value will be incorrect.
@@ -49,6 +62,8 @@ func Init(path string) error {
 	if err := loadConfig(); err != nil {
 		return errors.Wrap(err, "Unable to initialize config. Please check that the config file is correctly formatted and retry.")
 	}
+
+	return nil
 }
 
 // loadConfig reads the file at `configPath` into `globalConfig`.
@@ -82,4 +97,11 @@ func loadConfig() error {
 }
 
 // dumpConfig writes `globalConfig` to the file at `configPath`.
-func dumpConfig()
+func dumpConfig() error {
+	data, err := yaml.Marshal(globalConfig)
+	if err != nil {
+		return err
+	}
+
+	return ioutil.WriteFile(globalConfigPath, data, 0x0644)
+}
