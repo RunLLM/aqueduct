17ab2c1c1af113298f0a0d9198ab145a1b83b336
Eunice Chan <ekchan@berkeley.edu>
2022-08-15 15:41:25 -0700

    PR review

--- src/python/aqueduct_executor/start-function-executor.sh
+++ src/python/aqueduct_executor/start-function-executor.sh
@@ -10,7 +10,7 @@ if [ $EXIT_CODE != "0" ]; then exit $(($EXIT_CODE)); fi
 
 if test -f "$FUNCTION_EXTRACT_PATH/op/requirements.txt"
 then
-      pip freeze >> "$FUNCTION_EXTRACT_PATH/op/local_deps.txt"
+      pip3 freeze >> "$FUNCTION_EXTRACT_PATH/op/local_deps.txt"
       python3 -m aqueduct_executor.operators.function_executor.prune_requirements --local_path="$FUNCTION_EXTRACT_PATH/op/local_deps.txt" --requirements_path="$FUNCTION_EXTRACT_PATH/op/requirements.txt" --missing_path="$FUNCTION_EXTRACT_PATH/op/missing.txt"
       EXIT_CODE=$?
       if [ $EXIT_CODE != "0" ]; then exit $(($EXIT_CODE)); fi
