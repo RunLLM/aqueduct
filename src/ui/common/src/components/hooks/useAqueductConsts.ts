98796b27ad1b2ce86ec2834e1bba0b9a3bced803
Chenggang Wu <cgwu0530@gmail.com>
2022-08-15 18:46:46 -0700

    Revert "ENG-1533 Frontend: Use default value for SERVER_ADDRESS (#316)"
    This reverts commit 0740a10b8548516d3890742974c9fac7be80a85a.

--- src/ui/common/src/components/hooks/useAqueductConsts.ts
+++ src/ui/common/src/components/hooks/useAqueductConsts.ts
@@ -2,13 +2,8 @@ export type AqueductConsts = {
   apiAddress: string;
 };
 
-const DEFAULT_SERVER_ADDRESS = 'http://localhost:8080';
-
 export const useAqueductConsts = (): AqueductConsts => {
   return {
-    // Use default value for server address if there is not one set in the .env or env.local file.
-    apiAddress: process.env.SERVER_ADDRESS
-      ? process.env.SERVER_ADDRESS
-      : DEFAULT_SERVER_ADDRESS,
+    apiAddress: process.env.SERVER_ADDRESS,
   };
 };
