dfd6e97496c27525db036fb8d5538dd8ef461ce6
hsubbaraj-spiral <hari@spiralai.co>
2022-08-18 00:02:21 +0000

    lint

--- src/python/aqueduct_executor/operators/utils/storage/s3.py
+++ src/python/aqueduct_executor/operators/utils/storage/s3.py
@@ -13,10 +13,7 @@ class S3Storage(Storage):
 
     def __init__(self, config: S3StorageConfig):
 
-        if (
-            "AWS_ACCESS_KEY_ID" in os.environ
-            and "AWS_SECRET_ACCESS_KEY" in os.environ
-        ):
+        if "AWS_ACCESS_KEY_ID" in os.environ and "AWS_SECRET_ACCESS_KEY" in os.environ:
             self._client = boto3.client(
                 "s3",
                 aws_access_key_id=os.environ["AWS_ACCESS_KEY_ID"],
