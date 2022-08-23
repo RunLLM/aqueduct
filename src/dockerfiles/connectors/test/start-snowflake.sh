d1e8bd71e9fd67dec7e96092464b5911c2abcd1e
hsubbaraj-spiral <hari@spiralai.co>
2022-08-23 21:45:18 +0000

    removed test files and added system metric to the makefile

--- src/dockerfiles/connectors/test/start-snowflake.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-git clone https://github.com/aqueducthq/aqueduct.git
-cd aqueduct/src/python
-
-pip install .
-
-python3 -m aqueduct_executor.operators.connectors.data.main --spec "$JOB_SPEC"
