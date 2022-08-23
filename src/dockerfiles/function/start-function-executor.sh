f64bf01bdd9eecc61e1534764f2d9499ab77eecc
hsubbaraj-spiral <hari@spiralai.co>
2022-08-23 20:59:46 +0000

    added aqueduct-ml to the envs + removed the install from the function executor

--- src/dockerfiles/function/start-function-executor.sh
+++ src/dockerfiles/function/start-function-executor.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 #!/bin/bash
-pip3 install aqueduct-ml
-
 echo $JOB_SPEC
 FUNCTION_EXTRACT_PATH=$(python3 -m aqueduct_executor.operators.function_executor.get_extract_path --spec "$JOB_SPEC")
 EXIT_CODE=$?
@@ -13,8 +11,6 @@ if [ $EXIT_CODE != "0" ]; then exit $(($EXIT_CODE)); fi
 
 PYTHON_VERSION=$(python3 -m aqueduct_executor.operators.function_executor.set_conda_version "$FUNCTION_EXTRACT_PATH")
 echo "Python version is $PYTHON_VERSION"
-
-conda run -n $PYTHON_VERSION pip3 install aqueduct-ml
 
 if test -f "$FUNCTION_EXTRACT_PATH/op/requirements.txt"
 then
