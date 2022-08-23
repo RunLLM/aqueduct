fafd614c06b10c49f1a7d8bb89a736729e70756c
Saurav Chhatrapati <sauravc@berkeley.edu>
2022-08-23 12:34:20 -0700

    Implements python gcs storage

---
+++ src/python/aqueduct_executor/operators/utils/storage/gcs.py
@@ -0,0 +1,44 @@
+import os
+from typing import Any
+import uuid
+import io
+
+from aqueduct_executor.operators.utils.storage.storage import Storage
+from aqueduct_executor.operators.utils.storage.config import GCSStorageConfig
+from google.cloud import storage
+
+_CREDENTIALS_ENV_VAR = "GCS_CREDENTIALS"
+
+class GCSStorage(Storage):
+    _client: Any # GCS client
+    _config: GCSStorageConfig
+
+    def __init__(self, config: GCSStorageConfig):
+        if _CREDENTIALS_ENV_VAR in os.environ in os.environ:
+            # GCS credentials were provided via env variables instead of a filepath
+            temp_path = os.path.join(os.getcwd(), str(uuid.uuid4()))
+            with open(temp_path, "w") as f:
+                f.write(os.environ[_CREDENTIALS_ENV_VAR])
+            config.credentials_path = temp_path
+        
+        self._client = storage.Client.from_service_account_json(
+            config.credentials_path
+        )
+        self._config = config
+    
+
+    def put(self, key: str, value: bytes) -> None:
+        bucket = self._client.bucket(self._config.bucket)
+        blob = bucket.blob(key)
+
+        print(f"writing to gcs: {key}")
+        f = io.BytesIO(value)
+        blob.upload_from_file(f)
+
+    
+    def get(self, key: str) -> bytes:
+        bucket = self._client.bucket(self._config.bucket)
+        blob = bucket.blob(key)
+
+        print(f"reading from gcs: {key}")
+        return blob.download_as_bytes()
