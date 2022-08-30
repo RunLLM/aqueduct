1778190ddb2ad8aecd252d3a46fb09b8125f26f5
Eunice Chan <ekchan@berkeley.edu>
2022-08-29 19:53:57 -0700

    Update mode created_at

--- src/golang/lib/collections/operator/postgres.go
+++ src/golang/lib/collections/operator/postgres.go
@@ -90,33 +90,29 @@ func (r *postgresReaderImpl) GetDistinctLoadOperatorsByWorkflowId(
 	db database.Database,
 ) ([]GetDistinctLoadOperatorsByWorkflowIdResponse, error) {
 	// Get all unique load operator (defined as a unqiue combination of operator name,
-	// integration name, integration id, service, object name, and update mode) that
-	// has an edge (in `from_id` or `to_id`) in a DAG belonging to the specified
-	// workflow.
+	// DAG creation time, integration name, integration id, service, object name, and
+	// update mode) that has an edge (in `from_id` or `to_id`) in a DAG belonging to
+	// the specified workflow.
 	query := `
 	SELECT DISTINCT 
-		operator.name AS operator_name, 
+		operator.name AS operator_name,
+		workflow_dag.created_at AS created_at,
 		integration.name AS integration_name, 
 		json_extract_path_text(operator.spec, 'load', 'integration_id') AS integration_id,
 		json_extract_path_text(operator.spec, 'load', 'service') AS service, 
 		json_extract_path_text(operator.spec, 'load', 'parameters', 'table') AS table_name, 
 		json_extract_path_text(operator.spec, 'load', 'parameters', 'update_mode')  AS update_mode
-	FROM operator, integration 
+	FROM 
+		operator, integration, workflow_dag_edge, workflow_dag  
 	WHERE (
 		json_extract_path_text(spec, 'type') = 'load' AND 
 		integration.id = json_extract_path_text(operator.spec, 'load', 'integration_id') AND
-		EXISTS (
-			SELECT 1 
-			FROM 
-				workflow_dag_edge, workflow_dag 
-			WHERE 
-				( 
-					workflow_dag_edge.from_id = operator.id OR 
-					workflow_dag_edge.to_id = operator.id 
-				) AND 
-				workflow_dag_edge.workflow_dag_id = workflow_dag.id AND 
-				workflow_dag.workflow_id = $1
-		)
+		( 
+			workflow_dag_edge.from_id = operator.id OR 
+			workflow_dag_edge.to_id = operator.id 
+		) AND 
+		workflow_dag_edge.workflow_dag_id = workflow_dag.id AND 
+		workflow_dag.workflow_id = $1
 	);`
 
 	var workflowSpecs []GetDistinctLoadOperatorsByWorkflowIdResponse
