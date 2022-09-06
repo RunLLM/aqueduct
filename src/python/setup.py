df6f04d6613d0ee7e8db9a9339e060ed877a9055
cw75 <cgwu0530@gmail.com>
2022-09-06 23:48:24 +0000

    bump version numbers

--- src/python/setup.py
+++ src/python/setup.py
@@ -10,7 +10,7 @@ long_description = open(readme_path).read()
 
 setup(
     name="aqueduct-ml",
-    version="0.0.12",
+    version="0.0.13",
     install_requires=install_requires,
     scripts=["bin/aqueduct"],
     packages=find_packages(),
