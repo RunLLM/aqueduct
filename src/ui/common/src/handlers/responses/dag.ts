785aa35cbdd68409282d4b239e721bbf2e419313
likawind <cwinddavid@gmail.com>
2022-09-07 17:55:10 +0000

    changes

--- src/ui/common/src/handlers/responses/dag.ts
+++ src/ui/common/src/handlers/responses/dag.ts
@@ -1,5 +1,6 @@
 // This file should mirror src/golang/workflow/dag/response.go
 import { EngineConfig } from '../../utils/engine';
+import { OperatorType } from '../../utils/operators';
 import { ExecutionStatus } from '../../utils/shared';
 import { StorageConfig } from '../../utils/storage';
 import {
@@ -40,3 +41,38 @@ export type DagResultResponse = DagMetadataResponse & {
   operators: { [id: string]: OperatorResultResponse };
   artifacts: { [id: string]: ArtifactResultResponse };
 };
+
+// This helper fetches all metrics and checks defined on an artifact. Which includes:
+// - metrics with this artifact as input
+// - checks with this artifact, and all above metrics' outputs as input
+export function getMetricsAndChecksOnArtifact(
+  dagResult: DagResultResponse,
+  artifactId: string
+): { checks: OperatorResultResponse[]; metrics: OperatorResultResponse[] } {
+  const metricsOp = Object.values(dagResult.operators).filter(
+    (opResult) =>
+      opResult.inputs.includes(artifactId) &&
+      (opResult.spec?.type === OperatorType.Metric ||
+        opResult.spec?.type === OperatorType.SystemMetric)
+  );
+  const checksOp = Object.values(dagResult.operators).filter(
+    (opResult) =>
+      opResult.inputs.includes(artifactId) &&
+      opResult.spec?.type === OperatorType.Check
+  );
+
+  const metricsArtfIds = metricsOp.flatMap((opResult) => opResult.outputs);
+  const metricsArtf = metricsArtfIds.map((id) => dagResult.artifacts[id]);
+  const metricsDownstreamIds = metricsArtf.flatMap(
+    (artfResult) => artfResult.to
+  );
+  const metricsDownstreamOps = metricsDownstreamIds.map(
+    (id) => dagResult.operators[id]
+  );
+  checksOp.concat(
+    metricsDownstreamOps.filter(
+      (opResult) => opResult.spec?.type === OperatorType.Check
+    )
+  );
+  return { checks: checksOp, metrics: metricsOp };
+}
