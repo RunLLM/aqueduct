823e9ffb9594e0f15485a821aa41e8e2f37d45fb
hsubbaraj-spiral <hari@spiralai.co>
2022-09-19 22:53:43 +0000

    simplify versions of images

--- src/golang/lib/job/constants.go
+++ src/golang/lib/job/constants.go
@@ -1,13 +1,14 @@
 package job
 
 const (
-	DefaultFunctionDockerImage           = "aqueducthq/function:0.0.14"
-	DefaultParameterDockerImage          = "aqueducthq/param:0.0.14"
-	DefaultSystemMetricDockerImage       = "aqueducthq/system-metric:0.0.14"
-	DefaultPostgresConnectorDockerImage  = "aqueducthq/postgres-connector:0.0.14"
-	DefaultSnowflakeConnectorDockerImage = "aqueducthq/snowflake-connector:0.0.14"
-	DefaultMySqlConnectorDockerImage     = "aqueducthq/mysql-connector:0.0.14"
-	DefaultSqlServerConnectorDockerImage = "aqueducthq/sqlserver-connector:0.0.14"
-	DefaultBigQueryConnectorDockerImage  = "aqueducthq/bigquery-connector:0.0.14"
-	DefaultS3ConnectorDockerImage        = "aqueducthq/s3-connector:0.0.14"
+	K8sImageVersionNumber                = "0.0.14"
+	DefaultFunctionDockerImage           = "aqueducthq/function"
+	DefaultParameterDockerImage          = "aqueducthq/param"
+	DefaultSystemMetricDockerImage       = "aqueducthq/system-metric"
+	DefaultPostgresConnectorDockerImage  = "aqueducthq/postgres-connector"
+	DefaultSnowflakeConnectorDockerImage = "aqueducthq/snowflake-connector"
+	DefaultMySqlConnectorDockerImage     = "aqueducthq/mysql-connector"
+	DefaultSqlServerConnectorDockerImage = "aqueducthq/sqlserver-connector"
+	DefaultBigQueryConnectorDockerImage  = "aqueducthq/bigquery-connector"
+	DefaultS3ConnectorDockerImage        = "aqueducthq/s3-connector"
 )
