e4690dad5492f38800f354a919b62ea0466ff2b5
hsubbaraj-spiral <87333321+hsubbaraj-spiral@users.noreply.github.com>
2022-09-20 14:19:46 -0700

    Activates lambda integration + adds build-connectors to make command (#488)

--- src/ui/common/src/utils/integrations.ts
+++ src/ui/common/src/utils/integrations.ts
@@ -336,7 +336,7 @@ export const SupportedIntegrations: ServiceInfoMap = {
   },
   ['Lambda']: {
     logo: 'https://aqueduct-public-assets-bucket.s3.us-east-2.amazonaws.com/webapp/pages/integrations/Lambda.png',
-    activated: false,
+    activated: true,
     category: 'compute',
   },
 };
