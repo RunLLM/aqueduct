74251e5735eb03fb9a8bbcba68eee9de10ac8d6b
hsubbaraj-spiral <87333321+hsubbaraj-spiral@users.noreply.github.com>
2022-09-22 12:48:47 -0700

    Launches versioned docker image for function operator (#500)

--- src/golang/lib/job/constants.go
+++ src/golang/lib/job/constants.go
@@ -1,14 +1,17 @@
 package job
 
 const (
-	K8sImageVersionNumber                = "0.0.15"
-	DefaultFunctionDockerImage           = "aqueducthq/function"
-	DefaultParameterDockerImage          = "aqueducthq/param"
-	DefaultSystemMetricDockerImage       = "aqueducthq/system-metric"
-	DefaultPostgresConnectorDockerImage  = "aqueducthq/postgres-connector"
-	DefaultSnowflakeConnectorDockerImage = "aqueducthq/snowflake-connector"
-	DefaultMySqlConnectorDockerImage     = "aqueducthq/mysql-connector"
-	DefaultSqlServerConnectorDockerImage = "aqueducthq/sqlserver-connector"
-	DefaultBigQueryConnectorDockerImage  = "aqueducthq/bigquery-connector"
-	DefaultS3ConnectorDockerImage        = "aqueducthq/s3-connector"
+	K8sImageVersionNumber         = "0.0.15"
+	Function37DockerImage         = "aqueducthq/function37"
+	Function38DockerImage         = "aqueducthq/function38"
+	Function39DockerImage         = "aqueducthq/function39"
+	Function310DockerImage        = "aqueducthq/function310"
+	ParameterDockerImage          = "aqueducthq/param"
+	SystemMetricDockerImage       = "aqueducthq/system-metric"
+	PostgresConnectorDockerImage  = "aqueducthq/postgres-connector"
+	SnowflakeConnectorDockerImage = "aqueducthq/snowflake-connector"
+	MySqlConnectorDockerImage     = "aqueducthq/mysql-connector"
+	SqlServerConnectorDockerImage = "aqueducthq/sqlserver-connector"
+	BigQueryConnectorDockerImage  = "aqueducthq/bigquery-connector"
+	S3ConnectorDockerImage        = "aqueducthq/s3-connector"
 )
--- src/golang/lib/job/k8s.go
+++ src/golang/lib/job/k8s.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 
 	"github.com/aqueducthq/aqueduct/lib/collections/integration"
+	"github.com/aqueducthq/aqueduct/lib/collections/operator/function"
 	"github.com/aqueducthq/aqueduct/lib/collections/shared"
 	"github.com/aqueducthq/aqueduct/lib/k8s"
 	"github.com/dropbox/godropbox/errors"
@@ -145,10 +146,27 @@ func (j *k8sJobManager) DeleteCronJob(ctx context.Context, name string) error {
 // Maps a job Spec to Docker image.
 func mapJobTypeToDockerImage(spec Spec) (string, error) {
 	switch spec.Type() {
-	// case WorkflowJobType:
-	// 	return j.conf.ExecutorDockerImage, nil
 	case FunctionJobType:
-		return DefaultFunctionDockerImage, nil
+		functionSpec, ok := spec.(*FunctionSpec)
+		if !ok {
+			return "", errors.New("Unable to determine Python Version.")
+		}
+		pythonVersion, err := function.GetPythonVersion(context.TODO(), functionSpec.FunctionPath, &functionSpec.StorageConfig)
+		if err != nil {
+			return "", errors.New("Unable to determine Python Version.")
+		}
+		switch pythonVersion {
+		case function.PythonVersion37:
+			return Function37DockerImage, nil
+		case function.PythonVersion38:
+			return Function38DockerImage, nil
+		case function.PythonVersion39:
+			return Function39DockerImage, nil
+		case function.PythonVersion310:
+			return Function310DockerImage, nil
+		default:
+			return "", errors.New("Unable to determine Python Version.")
+		}
 	case AuthenticateJobType:
 		authenticateSpec := spec.(*AuthenticateSpec)
 		return mapIntegrationServiceToDockerImage(authenticateSpec.ConnectorName)
@@ -162,9 +180,9 @@ func mapJobTypeToDockerImage(spec Spec) (string, error) {
 		discoverSpec := spec.(*DiscoverSpec)
 		return mapIntegrationServiceToDockerImage(discoverSpec.ConnectorName)
 	case ParamJobType:
-		return DefaultParameterDockerImage, nil
+		return ParameterDockerImage, nil
 	case SystemMetricJobType:
-		return DefaultSystemMetricDockerImage, nil
+		return SystemMetricDockerImage, nil
 	default:
 		return "", errors.Newf("Unsupported job type %v provided", spec.Type())
 	}
@@ -173,17 +191,17 @@ func mapJobTypeToDockerImage(spec Spec) (string, error) {
 func mapIntegrationServiceToDockerImage(service integration.Service) (string, error) {
 	switch service {
 	case integration.Postgres, integration.Redshift, integration.AqueductDemo:
-		return DefaultPostgresConnectorDockerImage, nil
+		return PostgresConnectorDockerImage, nil
 	case integration.Snowflake:
-		return DefaultSnowflakeConnectorDockerImage, nil
+		return SnowflakeConnectorDockerImage, nil
 	case integration.MySql, integration.MariaDb:
-		return DefaultMySqlConnectorDockerImage, nil
+		return MySqlConnectorDockerImage, nil
 	case integration.SqlServer:
-		return DefaultSqlServerConnectorDockerImage, nil
+		return SqlServerConnectorDockerImage, nil
 	case integration.BigQuery:
-		return DefaultBigQueryConnectorDockerImage, nil
+		return BigQueryConnectorDockerImage, nil
 	case integration.S3:
-		return DefaultS3ConnectorDockerImage, nil
+		return S3ConnectorDockerImage, nil
 	default:
 		return "", errors.Newf("Unknown integration service provided %v", service)
 	}
