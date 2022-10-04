76b0f837338d175d40fae0cbf7c56007d8e6970e
cw75 <cgwu0530@gmail.com>
2022-10-04 00:18:18 +0000

    add new data

---
+++ src/resources/demo_db/sqlite/v0.1.0/create_tables.py
@@ -0,0 +1,52 @@
+import pandas as pd
+from sqlalchemy import create_engine, engine
+from pathlib import Path
+from os.path import join
+
+def create_diabetes_table(engine):
+    df = pd.read_csv(
+        "diabetes.csv",
+        names=[
+            "pregnancies",
+            "glucose",
+            "diastolic_bp",
+            "skin_thickness",
+            "2_hr_insulin",
+            "bmi",
+            "pedigree_fn",
+            "age",
+            "has_diabetes",
+        ],
+        header=None,
+    )
+
+    df.to_sql(
+        "diabetes",
+        con=engine,
+        index=False,
+        if_exists="replace",
+    )
+
+def create_housing_table(engine):
+    df = pd.read_csv("house_prices.csv", index_col="Id")
+
+    df.to_sql(
+        "house_prices",
+        con=engine,
+        index=True,
+        if_exists="replace",
+    )
+
+if __name__ == "__main__":
+    url = "sqlite:///{database}".format(
+        database=join(Path.home(), ".aqueduct", "server", "db", "demo.db"),
+    )
+
+    try:
+        engine = create_engine(url)
+        engine.connect()
+
+        create_diabetes_table(engine)
+        create_housing_table(engine)
+    finally:
+        engine.dispose()
