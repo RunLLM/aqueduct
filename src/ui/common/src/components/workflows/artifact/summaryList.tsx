268d50aaf103d58e6942e8f0101c965a2d6aa8b6
Andre Giron <andre@spiralai.co>
2022-10-07 12:18:26 -0400

    Fixes bug where extra closing bracket broke link in ArtifactSummaryList component. Fixes bug where attempting to render null value in PaginatedTable caused app to crash.

--- src/ui/common/src/components/workflows/artifact/summaryList.tsx
+++ src/ui/common/src/components/workflows/artifact/summaryList.tsx
@@ -37,9 +37,7 @@ const SummaryList: React.FC<Props> = ({
     if (artifactResult.result?.content_serialized) {
       content = artifactResult.result.content_serialized;
     } else {
-      link = `${getPathPrefix()}/workflow/${workflowId}/result/${dagResultId}/artifact/${
-        artifactResult.id
-      }}`;
+      link = `${getPathPrefix()}/workflow/${workflowId}/result/${dagResultId}/artifact/${artifactResult.id}`;
       content = artifactResult.name;
     }
 
