c6ed8fb10981d96261e4e1d6d38b735afc533979
cw75 <cgwu0530@gmail.com>
2022-10-10 21:17:43 +0000

    initial impl

--- src/ui/common/src/components/integrations/dialogs/kubernetesDialog.tsx
+++ src/ui/common/src/components/integrations/dialogs/kubernetesDialog.tsx
@@ -1,10 +1,11 @@
+import { Checkbox, FormControlLabel } from '@mui/material';
 import Box from '@mui/material/Box';
 import React from 'react';
-import { Checkbox, FormControlLabel } from '@mui/material';
+import { useEffect, useState } from 'react';
 
 import { KubernetesConfig } from '../../../utils/integrations';
+import { useAqueductConsts } from '../../hooks/useAqueductConsts';
 import { IntegrationTextInputField } from './IntegrationTextInputField';
-import { useEffect } from 'react';
 
 const Placeholders: KubernetesConfig = {
   kubeconfig_path: 'home/ubuntu/.kube/config',
@@ -15,37 +16,59 @@ const Placeholders: KubernetesConfig = {
 type Props = {
   onUpdateField: (field: keyof KubernetesConfig, value: string) => void;
   value?: KubernetesConfig;
+  apiKey: string;
 };
 
-export const KubernetesDialog: React.FC<Props> = ({ onUpdateField, value }) => {
+const { apiAddress } = useAqueductConsts();
+
+export const KubernetesDialog: React.FC<Props> = ({
+  onUpdateField,
+  value,
+  apiKey,
+}) => {
+  const [inK8sCluster, setInK8sCluster] = useState(false);
   useEffect(() => {
     if (!value?.use_same_cluster) {
       onUpdateField('use_same_cluster', 'false');
     }
+
+    const fetchEnvironment = async () => {
+      const environmentResponse = await fetch(`${apiAddress}/api/environment`, {
+        method: 'GET',
+        headers: {
+          'api-key': apiKey,
+        },
+      });
+
+      const responseBody = await environmentResponse.json();
+      setInK8sCluster(responseBody['inK8sCluster']);
+    };
+
+    fetchEnvironment().catch(console.error);
   }, []);
 
   return (
     <Box sx={{ mt: 2 }}>
-
-      <FormControlLabel
-        label="Use the same Kubernetes cluster that the server is running on."
-        control={
-          <Checkbox
-            checked={value?.use_same_cluster === 'true'}
-            onChange={(event) =>
-              onUpdateField(
-                'use_same_cluster',
-                event.target.checked ? 'true' : 'false'
-              )
-            }
-          />
-        }
-      />
-      
+      {inK8sCluster && (
+        <FormControlLabel
+          label="Use the same Kubernetes cluster that the server is running on."
+          control={
+            <Checkbox
+              checked={value?.use_same_cluster === 'true'}
+              onChange={(event) =>
+                onUpdateField(
+                  'use_same_cluster',
+                  event.target.checked ? 'true' : 'false'
+                )
+              }
+            />
+          }
+        />
+      )}
 
       <IntegrationTextInputField
         spellCheck={false}
-        required={true}
+        required={!(value?.use_same_cluster === 'true')}
         label="Kubernetes Config Path*"
         description="The path to the kubeconfig file."
         placeholder={Placeholders.kubeconfig_path}
@@ -58,7 +81,7 @@ export const KubernetesDialog: React.FC<Props> = ({ onUpdateField, value }) => {
 
       <IntegrationTextInputField
         spellCheck={false}
-        required={true}
+        required={!(value?.use_same_cluster === 'true')}
         label="Cluster Name*"
         description="The name of the cluster that will be used."
         placeholder={Placeholders.cluster_name}
@@ -69,3 +92,11 @@ export const KubernetesDialog: React.FC<Props> = ({ onUpdateField, value }) => {
     </Box>
   );
 };
+
+export function isK8sConfigComplete(config: KubernetesConfig): boolean {
+  if (config.use_same_cluster !== 'true') {
+    return !!config.kubeconfig_path && !!config.cluster_name;
+  }
+
+  return true;
+}
