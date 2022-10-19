f76e70ebca920657121b4a3972e617c56d0b90d9
Andre Giron <andre@spiralai.co>
2022-10-19 15:02:07 -0700

    lint

--- src/ui/common/src/components/notifications/NotificationListItem.tsx
+++ src/ui/common/src/components/notifications/NotificationListItem.tsx
@@ -100,10 +100,11 @@ export const NotificationListItem: React.FC<Props> = ({
     <Link
       underline="none"
       color="inherit"
-      href={`${getPathPrefix()}/workflow/${notification.workflowMetadata.id
-        }?workflowDagResultId=${encodeURI(
-          notification.workflowMetadata.dag_result_id
-        )}`}
+      href={`${getPathPrefix()}/workflow/${
+        notification.workflowMetadata.id
+      }?workflowDagResultId=${encodeURI(
+        notification.workflowMetadata.dag_result_id
+      )}`}
     >
       <ListItem
         sx={{
