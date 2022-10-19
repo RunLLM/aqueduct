86497650448cb45b28f6adc4d4a66e6cd9d0bacf
Andre Giron <andre@spiralai.co>
2022-10-19 16:41:58 -0700

    Wraps storybook in MUI context to show our theme.

--- src/ui/common/.storybook/preview.js
+++ src/ui/common/.storybook/preview.js
@@ -1,3 +1,18 @@
+// import styles to load faster.
+// see here for more info: 
+// https://storybook.js.org/blog/material-ui-in-storybook/
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+import '@fontsource/material-icons';
+import { theme } from '../src/styles/theme/theme';
+
+import { createTheme, CssBaseline, ThemeProvider } from "@mui/material";
+//import { darkTheme } from "../src/themes/dark.theme";
+
+const muiTheme = createTheme(theme);
+
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
@@ -7,3 +22,13 @@ export const parameters = {
     },
   },
 }
+
+// Wrap MUI stories in theme context.
+export const withMuiTheme = (Story) => (
+  <ThemeProvider theme={muiTheme}>
+    <CssBaseline />
+    <Story />
+  </ThemeProvider>
+);
+
+export const decorators = [withMuiTheme];
