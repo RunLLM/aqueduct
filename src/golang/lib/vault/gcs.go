17309cccc813cc18f11168bef366e14efbad2861
Saurav Chhatrapati <saurav@aqueducthq.com>
2022-10-21 10:47:13 -0700

    [5/7] Implements S3 vault (#609)
    * [6/7] Implements GCS vault (#616)
    
    * [7/7] Syncs vault with storage at server start (#610)

---
+++ src/golang/lib/vault/gcs.go
@@ -0,0 +1,27 @@
+package vault
+
+import (
+	"path"
+
+	"github.com/aqueducthq/aqueduct/lib/collections/shared"
+	"github.com/aqueducthq/aqueduct/lib/storage"
+)
+
+const (
+	gcsVaultDir = "vault"
+)
+
+func newGCSVault(gcsStoreConf shared.GCSConfig, key string) Vault {
+	// The GCS vault stores secrets under the ../vault path
+	gcsStoreConf.Bucket = path.Join(gcsStoreConf.Bucket, gcsVaultDir)
+
+	store := storage.NewStorage(&shared.StorageConfig{
+		Type:      shared.GCSStorageType,
+		GCSConfig: &gcsStoreConf,
+	})
+
+	return &vault{
+		store: store,
+		key:   key,
+	}
+}
--- src/golang/lib/vault/vault.go
+++ src/golang/lib/vault/vault.go
@@ -4,6 +4,7 @@ import (
 	"context"
 
 	"github.com/aqueducthq/aqueduct/lib/collections/shared"
+	"github.com/aqueducthq/aqueduct/lib/storage"
 	"github.com/dropbox/godropbox/errors"
 )
 
@@ -19,8 +20,39 @@ type Vault interface {
 func NewVault(storageConf *shared.StorageConfig, key string) (Vault, error) {
 	switch storageConf.Type {
 	case shared.FileStorageType:
-		return newFileVault(*storageConf.FileConfig, key)
+		return newFileVault(*storageConf.FileConfig, key), nil
+	case shared.S3StorageType:
+		return newS3Vault(*storageConf.S3Config, key), nil
+	case shared.GCSStorageType:
+		return newGCSVault(*storageConf.GCSConfig, key), nil
 	default:
 		return nil, errors.Newf("Unsupported vault type: %v", storageConf.Type)
 	}
+}
+
+type vault struct {
+	store storage.Storage
+	key   string
+}
+
+func (v *vault) Put(ctx context.Context, name string, secrets map[string]string) error {
+	encrypted, err := encrypt(secrets, v.key)
+	if err != nil {
+		return err
+	}
+
+	return v.store.Put(ctx, name, encrypted)
+}
+
+func (v *vault) Get(ctx context.Context, name string) (map[string]string, error) {
+	ciphertext, err := v.store.Get(ctx, name)
+	if err != nil {
+		return nil, err
+	}
+
+	return decrypt(ciphertext, v.key)
+}
+
+func (v *vault) Delete(ctx context.Context, name string) error {
+	return v.store.Delete(ctx, name)
 }
