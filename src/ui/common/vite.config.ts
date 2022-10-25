ed7fdf87d3d08be42eab03f3753a9aade3fdb0ec
Andre Giron <andre@spiralai.co>
2022-10-24 17:00:24 -0700

    fix code review feedback

--- src/ui/common/vite.config.ts
+++ src/ui/common/vite.config.ts
@@ -3,20 +3,7 @@ import { defineConfig } from 'vite';
 import { externalizeDeps } from 'vite-plugin-externalize-deps';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
-// export default defineConfig(({ command, mode }) => {
-//     plugins: [react()],
-
-// })
-
-export default defineConfig(({ command, mode }) => {
-    if (command === 'serve') {
-        console.log('running in serve mode.')
-    } else {
-        // command === 'build'
-        console.log('running vite in build mode.')
-    }
-
+export default defineConfig(() => {
     return {
         plugins: [
             react(),
