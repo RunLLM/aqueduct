ba585ed7d32fa43c182ad48cfa7e5237823a3bf1
Andre Giron <andre@spiralai.co>
2022-10-27 17:23:08 +0000

    lint fixes

--- src/ui/common/src/stories/Button.tsx
+++ src/ui/common/src/stories/Button.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
 import './button.css';
+
+import React from 'react';
 
 interface ButtonProps {
   /**
@@ -34,11 +35,15 @@ export const Button = ({
   label,
   ...props
 }: ButtonProps) => {
-  const mode = primary ? 'storybook-button--primary' : 'storybook-button--secondary';
+  const mode = primary
+    ? 'storybook-button--primary'
+    : 'storybook-button--secondary';
   return (
     <button
       type="button"
-      className={['storybook-button', `storybook-button--${size}`, mode].join(' ')}
+      className={['storybook-button', `storybook-button--${size}`, mode].join(
+        ' '
+      )}
       style={{ backgroundColor }}
       {...props}
     >
--- src/ui/common/src/stories/Page.stories.tsx
+++ src/ui/common/src/stories/Page.stories.tsx
@@ -1,6 +1,7 @@
+import { ComponentMeta, ComponentStory } from '@storybook/react';
+import { userEvent, within } from '@storybook/testing-library';
 import React from 'react';
-import { ComponentStory, ComponentMeta } from '@storybook/react';
-import { within, userEvent } from '@storybook/testing-library';
+
 import { Page } from './Page';
 
 export default {
