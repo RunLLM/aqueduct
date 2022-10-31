9a5b3bc588261b018f13c16446e8a4d3645a2993
Andre Giron <andre@spiralai.co>
2022-10-31 14:15:04 -0700

    fix code review feedback

--- src/ui/common/.storybook/preview.js
+++ src/ui/common/.storybook/preview.js
@@ -6,7 +6,6 @@ import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 import '@fontsource/material-icons';
-//import '@fontsource/roboto/300.css'
 import { theme } from '../src/styles/theme/theme';
 
 import { createTheme, CssBaseline, ThemeProvider } from "@mui/material";
