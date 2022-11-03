8897a591e626bd9d27052bd100e412938748f6c1
Eunice Chan <ekchan@berkeley.edu>
2022-11-03 09:17:32 -0700

    id -> ID

--- src/golang/lib/repos/artifact.go
+++ src/golang/lib/repos/artifact.go
@@ -16,20 +16,20 @@ type Artifact interface {
 }
 
 type artifactReader interface {
-	// Exists returns whether an Artifact with id exists.
-	Exists(ctx context.Context, id uuid.UUID, db database.Database) (bool, error)
+	// Exists returns whether an Artifact with ID exists.
+	Exists(ctx context.Context, ID uuid.UUID, db database.Database) (bool, error)
 
-	// Get returns the Artifact with id.
-	Get(ctx context.Context, id uuid.UUID, db database.Database) (*models.Artifact, error)
+	// Get returns the Artifact with ID.
+	Get(ctx context.Context, ID uuid.UUID, db database.Database) (*models.Artifact, error)
 
-	// Get returns the Artifacts with ids.
-	GetMultiple(ctx context.Context, ids []uuid.UUID, db database.Database) ([]models.Artifact, error)
+	// Get returns the Artifacts with IDs.
+	GetMultiple(ctx context.Context, IDs []uuid.UUID, db database.Database) ([]models.Artifact, error)
 
-	// GetByWorkflowDagId returns the workflows created by workflow dag with id workflowDagId.
-	GetByWorkflowDagId(ctx context.Context, workflowDagId uuid.UUID, db database.Database) ([]models.Artifact, error)
+	// GetByWorkflowDagId returns the workflows created by workflow dag with ID workflowDagId.
+	GetByWorkflowDagId(ctx context.Context, workflowDagID uuid.UUID, db database.Database) ([]models.Artifact, error)
 
 	// ValidateOrg returns whether the Workflow was created by a user in orgID.
-	ValidateOrg(ctx context.Context, id uuid.UUID, orgID uuid.UUID, db database.Database) (bool, error)
+	ValidateOrg(ctx context.Context, ID uuid.UUID, orgID uuid.UUID, db database.Database) (bool, error)
 }
 
 type artifactWriter interface {
@@ -42,12 +42,12 @@ type artifactWriter interface {
 		db database.Database,
 	) (*models.Artifact, error)
 
-	// Update applies changes to the Artifact with id. It returns the updated Artifact.
-	Update(ctx context.Context, id uuid.UUID, changes map[string]interface{}, db database.Database) (*models.Artifact, error)
+	// Update applies changes to the Artifact with ID. It returns the updated Artifact.
+	Update(ctx context.Context, ID uuid.UUID, changes map[string]interface{}, db database.Database) (*models.Artifact, error)
 
-	// Delete deletes the Artifact with id.
-	Delete(ctx context.Context, id uuid.UUID, db database.Database) error
+	// Delete deletes the Artifact with ID.
+	Delete(ctx context.Context, ID uuid.UUID, db database.Database) error
 
-	// DeleteMultiple deletes the Artifacts with ids.
-	DeleteMultiple(ctx context.Context, ids []uuid.UUID, db database.Database) error
+	// DeleteMultiple deletes the Artifacts with IDs.
+	DeleteMultiple(ctx context.Context, IDs []uuid.UUID, db database.Database) error
 }
