88761798f00962e9829afa8d671edefd72d7ac16
Andre Giron <andre@spiralai.co>
2022-11-07 15:57:27 -0800

    Storybook story for workflows list table

---
+++ src/ui/common/src/stories/PaginatedTableStory.stories.tsx
@@ -0,0 +1,33 @@
+import { Box } from '@mui/material';
+import { Data } from '../utils/data';
+import { PaginatedTable } from '../components/tables/PaginatedTable';
+
+export const PaginatedTableStory: React.FC = () => {
+    const mockData: Data = {
+        schema: {
+            fields: [
+                { name: 'name', type: 'varchar' },
+                { name: 'last_run', type: 'varchar' },
+                { name: 'engine', type: 'varchar' },
+                { name: 'metrics', type: 'varchar' },
+                { name: 'checks', type: 'varchar' }
+            ],
+            pandas_version: '1.5.1'
+        },
+        data: [
+            { name: 'churn', last_run: '11/1/2022 2:00PM', engine: 'k8s_us_east', metrics: 'avg_churn, revenue lost', checks: 'min_churn, max_churn' },
+            { name: 'churn', last_run: '11/1/2022 2:00PM', engine: 'k8s_us_east', metrics: 'avg_churn, revenue lost', checks: 'min_churn, max_churn' },
+            { name: 'churn', last_run: '11/1/2022 2:00PM', engine: 'k8s_us_east', metrics: 'avg_churn, revenue lost', checks: 'min_churn, max_churn' },
+            { name: 'churn', last_run: '11/1/2022 2:00PM', engine: 'k8s_us_east', metrics: 'avg_churn, revenue lost', checks: 'min_churn, max_churn' },
+            { name: 'churn', last_run: '11/1/2022 2:00PM', engine: 'k8s_us_east', metrics: 'avg_churn, revenue lost', checks: 'min_churn, max_churn' },
+        ]
+    };
+
+    return (
+        <Box>
+            <PaginatedTable data={mockData} />
+        </Box>
+    )
+};
+
+export default PaginatedTableStory;
