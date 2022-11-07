363b56858532baa96b01c2a0abecbeea395fc489
Andre Giron <andre@spiralai.co>
2022-11-07 10:14:48 -0800

    wip setting up vite on app build.

---
+++ src/ui/app/vite.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+    plugins: [react()],
+    define: {
+        'process.env': {}
+    },
+    build: {
+        sourcemap: true,
+        minify: false,
+    }
+})
