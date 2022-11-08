1cbbebe197f4ce214d704192ade57e557321f2ce
Andre Giron <andre@spiralai.co>
2022-11-08 13:53:42 -0800

    Fixes bug where operator sidesheet names showed as operator node

--- src/ui/common/src/components/pages/workflow/id/index.tsx
+++ src/ui/common/src/components/pages/workflow/id/index.tsx
@@ -282,7 +282,7 @@ const WorkflowPage: React.FC<WorkflowPageProps> = ({
       }
       return 'Artifact Node';
     } else {
-      if (selectedDag.artifacts[currentNode.id]) {
+      if (selectedDag.operators[currentNode.id]) {
         return selectedDag.operators[currentNode.id].name;
       }
       return 'Operator Node';
@@ -313,8 +313,7 @@ const WorkflowPage: React.FC<WorkflowPageProps> = ({
             onClick={() => {
               // All we're really doing here is adding the artifactId onto the end of the URL.
               navigate(
-                `${getPathPrefix()}/workflow/${workflowId}/result/${
-                  workflow.selectedResult.id
+                `${getPathPrefix()}/workflow/${workflowId}/result/${workflow.selectedResult.id
                 }/artifact/${currentNode.id}`
               );
             }}
@@ -361,8 +360,7 @@ const WorkflowPage: React.FC<WorkflowPageProps> = ({
             style={buttonStyle}
             onClick={() => {
               navigate(
-                `${getPathPrefix()}/workflow/${workflowId}/result/${
-                  workflow.selectedResult.id
+                `${getPathPrefix()}/workflow/${workflowId}/result/${workflow.selectedResult.id
                 }/metric/${currentNode.id}`
               );
             }}
@@ -381,8 +379,7 @@ const WorkflowPage: React.FC<WorkflowPageProps> = ({
             style={buttonStyle}
             onClick={() => {
               navigate(
-                `${getPathPrefix()}/workflow/${workflowId}/result/${
-                  workflow.selectedResult.id
+                `${getPathPrefix()}/workflow/${workflowId}/result/${workflow.selectedResult.id
                 }/operator/${currentNode.id}`
               );
             }}
@@ -401,8 +398,7 @@ const WorkflowPage: React.FC<WorkflowPageProps> = ({
             style={buttonStyle}
             onClick={() => {
               navigate(
-                `${getPathPrefix()}/workflow/${workflowId}/result/${
-                  workflow.selectedResult.id
+                `${getPathPrefix()}/workflow/${workflowId}/result/${workflow.selectedResult.id
                 }/check/${currentNode.id}`
               );
             }}
