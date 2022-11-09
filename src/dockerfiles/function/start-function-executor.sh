1c5fa16d6446049f207f7849a6f923e83c8232e5
Kenneth Xu <kenxu95@gmail.com>
2022-11-09 15:11:08 -0800

    done

--- src/dockerfiles/function/start-function-executor.sh
+++ src/dockerfiles/function/start-function-executor.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-#!/bin/bash
-echo $JOB_SPEC
+JOB_SPEC=$1
 FUNCTION_EXTRACT_PATH=$(python3 -m aqueduct_executor.operators.function_executor.get_extract_path --spec "$JOB_SPEC")
 EXIT_CODE=$?
 if [ $EXIT_CODE != "0" ]; then exit $(($EXIT_CODE)); fi
@@ -11,14 +10,10 @@ if [ $EXIT_CODE != "0" ]; then exit $(($EXIT_CODE)); fi
 
 if test -f "$FUNCTION_EXTRACT_PATH/op/requirements.txt"
 then
-      pip freeze >> "$FUNCTION_EXTRACT_PATH/op/local_deps.txt"
-      python3 -m aqueduct_executor.operators.function_executor.prune_requirements --local_path="$FUNCTION_EXTRACT_PATH/op/local_deps.txt" --requirements_path="$FUNCTION_EXTRACT_PATH/op/requirements.txt" --missing_path="$FUNCTION_EXTRACT_PATH/op/missing.txt"
+      python3 -m pip freeze >> "$FUNCTION_EXTRACT_PATH/op/local_deps.txt"
+      python3 -m aqueduct_executor.operators.function_executor.install_requirements --local_path="$FUNCTION_EXTRACT_PATH/op/local_deps.txt" --requirements_path="$FUNCTION_EXTRACT_PATH/op/requirements.txt" --missing_path="$FUNCTION_EXTRACT_PATH/op/missing.txt" --spec "$JOB_SPEC"
       EXIT_CODE=$?
       if [ $EXIT_CODE != "0" ]; then exit $(($EXIT_CODE)); fi
-      if test -f "$FUNCTION_EXTRACT_PATH/op/missing.txt"
-      then
-            pip3 install -r "$FUNCTION_EXTRACT_PATH/op/missing.txt" --no-cache-dir
-      fi
 fi
 
 python3 -m aqueduct_executor.operators.function_executor.main --spec "$JOB_SPEC"
