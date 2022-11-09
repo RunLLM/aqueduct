85febb6c18c7de570f178d88e4e51d4d4bfd454a
Saurav Chhatrapati <saurav@aqueducthq.com>
2022-11-09 15:26:45 -0800

    Defines and implements workflow model and repo (#646)
    * [2/N] Implements workflow repo (#656)

---
+++ src/golang/lib/models/workflow.go
@@ -0,0 +1,62 @@
+package models
+
+import (
+	"fmt"
+	"strings"
+	"time"
+
+	"github.com/aqueducthq/aqueduct/lib/models/shared"
+	"github.com/google/uuid"
+)
+
+const (
+	WorkflowTable = "workflow"
+
+	// Workflow column names
+	WorkflowID          = "id"
+	WorkflowUserID      = "user_id"
+	WorkflowName        = "name"
+	WorkflowDescription = "description"
+	WorkflowSchedule    = "schedule"
+	WorkflowCreatedAt   = "created_at"
+	WorkflowRetention   = "retention_policy"
+)
+
+// A Workflow maps to the workflow table.
+type Workflow struct {
+	ID              uuid.UUID              `db:"id" json:"id"`
+	UserID          uuid.UUID              `db:"user_id" json:"user_id"`
+	Name            string                 `db:"name" json:"name"`
+	Description     string                 `db:"description" json:"description"`
+	Schedule        shared.Schedule        `db:"schedule" json:"schedule"`
+	CreatedAt       time.Time              `db:"created_at" json:"created_at"`
+	RetentionPolicy shared.RetentionPolicy `db:"retention_policy" json:"retention_policy"`
+}
+
+// WorkflowCols returns a comma-separated string of all Workflow columns.
+func WorkflowCols() string {
+	return strings.Join(allWorkflowCols(), ",")
+}
+
+// WorkflowColsWithPrefix returns a comma-separated string of all
+// Workflow columns prefixed by the table name.
+func WorkflowColsWithPrefix() string {
+	cols := allWorkflowCols()
+	for i, col := range cols {
+		cols[i] = fmt.Sprintf("%s.%s", WorkflowTable, col)
+	}
+
+	return strings.Join(cols, ",")
+}
+
+func allWorkflowCols() []string {
+	return []string{
+		WorkflowID,
+		WorkflowUserID,
+		WorkflowName,
+		WorkflowDescription,
+		WorkflowSchedule,
+		WorkflowCreatedAt,
+		WorkflowRetention,
+	}
+}
