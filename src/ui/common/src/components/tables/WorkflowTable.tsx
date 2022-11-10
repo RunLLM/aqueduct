263f833e686927a3be3dfee16fc319a26ca6616d
Andre Giron <andre@spiralai.co>
2022-11-10 12:07:43 -0800

    Split out workflows table components into separate files.

--- src/ui/common/src/components/tables/WorkflowTable.tsx
+++ src/ui/common/src/components/tables/WorkflowTable.tsx
@@ -12,108 +12,110 @@ import * as React from 'react';
 
 import { DataSchema } from '../../utils/data';
 
-export type WorkflowTableRow = { [key: string]: string | number | boolean | JSX.Element };
+export type WorkflowTableRow = {
+  [key: string]: string | number | boolean | JSX.Element;
+};
 
 export interface WorkflowTableData {
-    schema?: DataSchema,
-    data: WorkflowTableRow[]
+  schema?: DataSchema;
+  data: WorkflowTableRow[];
 }
 
 export interface WorkflowsTableProps {
-    data: WorkflowTableData;
+  data: WorkflowTableData;
 }
 
 export const WorkflowTable: React.FC<WorkflowsTableProps> = ({ data }) => {
-    const [page, setPage] = React.useState(0);
-    const [rowsPerPage, setRowsPerPage] = React.useState(5);
+  const [page, setPage] = React.useState(0);
+  const [rowsPerPage, setRowsPerPage] = React.useState(5);
 
-    const rows = data.data;
-    const columns = data.schema.fields;
+  const rows = data.data;
+  const columns = data.schema.fields;
 
-    const handleChangePage = (event: unknown, newPage: number) => {
-        setPage(newPage);
-    };
+  const handleChangePage = (event: unknown, newPage: number) => {
+    setPage(newPage);
+  };
 
-    const handleChangeRowsPerPage = (
-        event: React.ChangeEvent<HTMLInputElement>
-    ) => {
-        setRowsPerPage(+event.target.value);
-        setPage(0);
-    };
+  const handleChangeRowsPerPage = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    setRowsPerPage(+event.target.value);
+    setPage(0);
+  };
 
-    return (
-        <Paper sx={{ overflow: 'hidden' }}>
-            <TableContainer sx={{ maxHeight: '400px' }}>
-                <Table stickyHeader aria-label="sticky table">
-                    <TableHead>
-                        <TableRow>
-                            {columns.map((column, columnIndex) => {
-                                return (
-                                    <TableCell
-                                        key={`table-header-col-${columnIndex}`}
-                                        align={'left'}
-                                        sx={{
-                                            backgroundColor: 'blue.900',
-                                            color: 'white',
-                                            minWidth: '80px',
-                                        }}
-                                    >
-                                        <Box flexDirection="column">
-                                            <Typography
-                                                variant="body1"
-                                                sx={{
-                                                    textTransform: 'none',
-                                                    fontFamily: 'monospace',
-                                                    fontSize: '16px',
-                                                }}
-                                            >
-                                                {column.name}
-                                            </Typography>
-                                        </Box>
-                                    </TableCell>
-                                );
-                            })}
-                        </TableRow>
-                    </TableHead>
-                    <TableBody>
-                        {rows
-                            .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-                            .map((row, rowIndex) => {
-                                return (
-                                    <TableRow
-                                        hover
-                                        role="checkbox"
-                                        tabIndex={-1}
-                                        key={`table-row-${rowIndex}`}
-                                    >
-                                        {columns.map((column, columnIndex) => {
-                                            const value = row[column.name];
-                                            return (
-                                                <TableCell
-                                                    key={`table-col-${columnIndex}`}
-                                                    align={'left'}
-                                                >
-                                                    {value}
-                                                </TableCell>
-                                            );
-                                        })}
-                                    </TableRow>
-                                );
-                            })}
-                    </TableBody>
-                </Table>
-            </TableContainer>
-            <TablePagination
-                rowsPerPageOptions={[5, 10, 25, 50, 100]}
-                component="div"
-                count={rows.length}
-                rowsPerPage={rowsPerPage}
-                page={page}
-                onPageChange={handleChangePage}
-                onRowsPerPageChange={handleChangeRowsPerPage}
-            />
-        </Paper>
-    );
+  return (
+    <Paper sx={{ overflow: 'hidden' }}>
+      <TableContainer sx={{ maxHeight: '400px' }}>
+        <Table stickyHeader aria-label="sticky table">
+          <TableHead>
+            <TableRow>
+              {columns.map((column, columnIndex) => {
+                return (
+                  <TableCell
+                    key={`table-header-col-${columnIndex}`}
+                    align={'left'}
+                    sx={{
+                      backgroundColor: 'blue.900',
+                      color: 'white',
+                      minWidth: '80px',
+                    }}
+                  >
+                    <Box flexDirection="column">
+                      <Typography
+                        variant="body1"
+                        sx={{
+                          textTransform: 'none',
+                          fontFamily: 'monospace',
+                          fontSize: '16px',
+                        }}
+                      >
+                        {column.name}
+                      </Typography>
+                    </Box>
+                  </TableCell>
+                );
+              })}
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {rows
+              .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+              .map((row, rowIndex) => {
+                return (
+                  <TableRow
+                    hover
+                    role="checkbox"
+                    tabIndex={-1}
+                    key={`table-row-${rowIndex}`}
+                  >
+                    {columns.map((column, columnIndex) => {
+                      const value = row[column.name];
+                      return (
+                        <TableCell
+                          key={`table-col-${columnIndex}`}
+                          align={'left'}
+                        >
+                          {value}
+                        </TableCell>
+                      );
+                    })}
+                  </TableRow>
+                );
+              })}
+          </TableBody>
+        </Table>
+      </TableContainer>
+      <TablePagination
+        rowsPerPageOptions={[5, 10, 25, 50, 100]}
+        component="div"
+        count={rows.length}
+        rowsPerPage={rowsPerPage}
+        page={page}
+        onPageChange={handleChangePage}
+        onRowsPerPageChange={handleChangeRowsPerPage}
+      />
+    </Paper>
+  );
 };
 
 export default WorkflowTable;
--- src/ui/common/src/stories/WorkflowsTable.stories.tsx
+++ src/ui/common/src/stories/WorkflowsTable.stories.tsx
@@ -1,151 +1,164 @@
-import React, { useState } from 'react';
-import { Box, Typography } from '@mui/material';
-import WorkflowTable, { WorkflowTableData } from '../components/tables/WorkflowTable';
-import ExecutionStatus from '../utils/shared';
-import Status from '../components/workflows/workflowStatus';
+import { Box } from '@mui/material';
+import React from 'react';
+import { CheckLevel } from '../utils/operators';
+
+import WorkflowTable, {
+  WorkflowTableData,
+} from '../components/tables/WorkflowTable';
 import { SupportedIntegrations } from '../utils/integrations';
+import ExecutionStatus from '../utils/shared';
+import CheckItem from '../components/pages/workflows/components/CheckItem';
+import EngineItem from '../components/pages/workflows/components/EngineItem';
+import MetricItem, { MetricPreview } from '../components/pages/workflows/components/MetricItem';
+import WorkflowNameItem from '../components/pages/workflows/components/WorkflowNameItem';
 
 export const WorkflowsTable: React.FC = () => {
-    interface EngineItemProps {
-        engineName: string;
-        engineIconUrl: string
-    };
+  const checkPreviews = [
+    {
+      checkId: '1',
+      name: 'min_churn',
+      status: ExecutionStatus.Succeeded,
+      level: CheckLevel.Error,
+      value: 'True',
+      timestamp: new Date().toLocaleString()
+    },
+    {
+      checkId: '2',
+      name: 'max_churn',
+      status: ExecutionStatus.Failed,
+      level: CheckLevel.Error,
+      value: 'True',
+      timestamp: new Date().toLocaleString()
+    },
+    {
+      checkId: '3',
+      name: 'avg_churn_check',
+      // TODO: Come up with coherent color scheme for all of these different status levels.
+      status: ExecutionStatus.Pending,
+      level: CheckLevel.Warning,
+      value: null,
+      timestamp: new Date().toLocaleString()
+    },
+    {
+      checkId: '4',
+      name: 'warning_test',
+      // TODO: Come up with coherent color scheme for all of these different status levels.
+      status: ExecutionStatus.Succeeded,
+      level: CheckLevel.Warning,
+      value: 'False',
+      timestamp: new Date().toLocaleString()
+    },
+  ];
 
-    const EngineItem: React.FC<EngineItemProps> = ({ engineName, engineIconUrl }) => {
-        return (
-            <Box display="flex" alignItems="left" justifyContent="left">
-                <img src={engineIconUrl} style={{ marginTop: '4px', marginRight: '8px' }} width="16px" height="16px" />
-                <Typography variant="body1">{engineName}</Typography>
-            </Box>
-        );
-    }
+  const checkTableItem = <CheckItem checks={checkPreviews} />;
 
-    interface WorkflowNameItemProps {
-        name: string,
-        status: ExecutionStatus
-    }
+  const metricsShort: MetricPreview[] = [
+    { metricId: '1', name: 'avg_churn', value: '10' },
+    { metricId: '2', name: 'sentiment', value: '100.5' },
+    { metricId: '3', name: 'revenue_lost', value: '$20M' },
+    { metricId: '4', name: 'more_metrics', value: '$500' },
+  ];
 
-    const WorkflowNameItem: React.FC<WorkflowNameItemProps> = ({ name, status }) => {
-        return (
-            <Box display="flex" alignItems="left" justifyContent="space-between">
-                <Status status={status} />
-                <Typography sx={{ justifyContent: 'right' }} variant="body1">{name}</Typography>
-            </Box>
-        );
-    };
+  const metricsList = <MetricItem metrics={metricsShort} />;
 
-    interface MetricPreview {
-        // used to fetch additional metrics and information to be shown in table.
-        // TODO: Consider showing other metric related meta data here.
-        metricId: string;
-        name: string;
-        value: string;
-    }
+  const airflowEngine = (
+    <EngineItem
+      engineName="airflow"
+      engineIconUrl={SupportedIntegrations['Airflow'].logo}
+    />
+  );
 
-    interface MetricItemProps {
-        metrics: MetricPreview[];
-    }
+  const lambdaEngine = (
+    <EngineItem
+      engineName="lambda"
+      engineIconUrl={SupportedIntegrations['Lambda'].logo}
+    />
+  );
 
-    const MetricItem: React.FC<MetricItemProps> = ({ metrics }) => {
-        const [expanded, setExpanded] = useState(false);
-        let metricList = [];
-        let metricsToShow = metrics.length;
-        if (!expanded && metrics.length > 3) {
-            metricsToShow = 3;
-        }
+  const kubernetesEngine = (
+    <EngineItem
+      engineName="kubernetes"
+      engineIconUrl={SupportedIntegrations['Kubernetes'].logo}
+    />
+  );
 
-        for (let i = 0; i < metricsToShow; i++) {
-            metricList.push(
-                <Box display="flex" key={metrics[i].metricId}>
-                    <Typography variant="body1">{metrics[i].name}</Typography>
-                    <Typography variant="body1">{metrics[i].value}</Typography>
-                </Box>
-            );
-        }
+  const mockData: WorkflowTableData = {
+    schema: {
+      fields: [
+        { name: 'name', type: 'varchar' },
+        { name: 'last_run', type: 'varchar' },
+        { name: 'engine', type: 'varchar' },
+        { name: 'metrics', type: 'varchar' },
+        { name: 'checks', type: 'varchar' },
+      ],
+      pandas_version: '1.5.1',
+    },
+    data: [
+      {
+        name: (
+          <WorkflowNameItem name="churn" status={ExecutionStatus.Succeeded} />
+        ),
+        last_run: '11/1/2022 2:00PM',
+        engine: airflowEngine,
+        metrics: metricsList,
+        checks: checkTableItem,
+      },
+      {
+        name: (
+          <WorkflowNameItem
+            name="wine_ratings"
+            status={ExecutionStatus.Failed}
+          />
+        ),
+        last_run: '11/1/2022 2:00PM',
+        engine: lambdaEngine,
+        metrics: metricsList,
+        checks: checkTableItem,
+      },
+      {
+        name: (
+          <WorkflowNameItem
+            name="diabetes_classifier"
+            status={ExecutionStatus.Pending}
+          />
+        ),
+        last_run: '11/1/2022 2:00PM',
+        engine: kubernetesEngine,
+        metrics: metricsList,
+        checks: checkTableItem,
+      },
+      {
+        name: (
+          <WorkflowNameItem
+            name="mpg_regressor"
+            status={ExecutionStatus.Canceled}
+          />
+        ),
+        last_run: '11/1/2022 2:00PM',
+        engine: lambdaEngine,
+        metrics: metricsList,
+        checks: checkTableItem,
+      },
+      {
+        name: (
+          <WorkflowNameItem
+            name="house_price_prediction"
+            status={ExecutionStatus.Registered}
+          />
+        ),
+        last_run: '11/1/2022 2:00PM',
+        engine: kubernetesEngine,
+        metrics: metricsList,
+        checks: checkTableItem,
+      },
+    ],
+  };
 
-        const toggleExpanded = () => {
-            setExpanded(!expanded);
-        };
-
-        const showLess = <Box><Typography variant='body1' onClick={toggleExpanded}>Show Less ...</Typography></Box>;
-        const showMore = <Box><Typography variant='body1' onClick={toggleExpanded}>Show More ...</Typography></Box>;
-
-        return (
-            <Box>
-                {metricList}
-                {expanded ? showLess : showMore}
-            </Box>
-        );
-    }
-
-    const metricsShort: MetricPreview[] = [
-        { metricId: '1', name: 'avg_churn', value: '10' },
-        { metricId: '2', name: 'sentiment', value: '100.5' },
-        { metricId: '3', name: 'revenue_lost', value: '$20M' },
-        { metricId: '4', name: 'more_metrics', value: '$500' }
-    ];
-
-    const metricsList = <MetricItem metrics={metricsShort} />;
-
-    const airflowEngine = <EngineItem engineName="airflow" engineIconUrl={SupportedIntegrations['Airflow'].logo} />;
-    const lambdaEngine = <EngineItem engineName="lambda" engineIconUrl={SupportedIntegrations['Lambda'].logo} />;
-    const kubernetesEngine = <EngineItem engineName="kubernetes" engineIconUrl={SupportedIntegrations['Kubernetes'].logo} />;
-
-    const mockData: WorkflowTableData = {
-        schema: {
-            fields: [
-                { name: 'name', type: 'varchar' },
-                { name: 'last_run', type: 'varchar' },
-                { name: 'engine', type: 'varchar' },
-                { name: 'metrics', type: 'varchar' },
-                { name: 'checks', type: 'varchar' }
-            ],
-            pandas_version: '1.5.1'
-        },
-        data: [
-            {
-                name: <WorkflowNameItem name="churn" status={ExecutionStatus.Succeeded} />,
-                last_run: '11/1/2022 2:00PM',
-                engine: airflowEngine,
-                metrics: metricsList,
-                checks: 'min_churn, max_churn'
-            },
-            {
-                name: <WorkflowNameItem name="wine_ratings" status={ExecutionStatus.Failed} />,
-                last_run: '11/1/2022 2:00PM',
-                engine: lambdaEngine,
-                metrics: metricsList,
-                checks: 'min_churn, max_churn'
-            },
-            {
-                name: <WorkflowNameItem name="diabetes_classifier" status={ExecutionStatus.Pending} />,
-                last_run: '11/1/2022 2:00PM',
-                engine: kubernetesEngine,
-                metrics: metricsList,
-                checks: 'min_churn, max_churn'
-            },
-            {
-                name: <WorkflowNameItem name="mpg_regressor" status={ExecutionStatus.Canceled} />,
-                last_run: '11/1/2022 2:00PM',
-                engine: lambdaEngine,
-                metrics: metricsList,
-                checks: 'min_churn, max_churn'
-            },
-            {
-                name: <WorkflowNameItem name="house_price_prediction" status={ExecutionStatus.Registered} />,
-                last_run: '11/1/2022 2:00PM',
-                engine: kubernetesEngine,
-                metrics: metricsList,
-                checks: 'min_churn, max_churn'
-            },
-        ]
-    };
-
-    return (
-        <Box>
-            <WorkflowTable data={mockData} />
-        </Box>
-    )
+  return (
+    <Box>
+      <WorkflowTable data={mockData} />
+    </Box>
+  );
 };
 
 export default WorkflowsTable;
