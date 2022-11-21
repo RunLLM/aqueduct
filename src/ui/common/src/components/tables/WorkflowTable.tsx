07182dececeb32099dea9571677dd024aa00b8ad
Andre Giron <andre@spiralai.co>
2022-11-21 17:38:26 -0500

    Refactoring to add onGetColumnValue function to make searching table items easier.

--- src/ui/common/src/components/tables/WorkflowTable.tsx
+++ src/ui/common/src/components/tables/WorkflowTable.tsx
@@ -8,29 +8,59 @@ import TableHead from '@mui/material/TableHead';
 import TablePagination from '@mui/material/TablePagination';
 import TableRow from '@mui/material/TableRow';
 import Typography from '@mui/material/Typography';
+import TextField from '@mui/material/TextField';
 import * as React from 'react';
 
 import { DataSchema } from '../../utils/data';
+import { CheckPreview } from '../pages/workflows/components/CheckItem';
+import { MetricPreview } from '../pages/workflows/components/MetricItem';
+import { ExecutionStatusLinkProps } from '../pages/workflows/components/ExecutionStatusLink';
+
+export type WorkflowTableElement = string | number | boolean | JSX.Element;
 
 export type WorkflowTableRow = {
-  [key: string]: string | number | boolean | JSX.Element;
+  [key: string]: WorkflowTableElement;
 };
+
+export type WorkflowTableRowData = {
+  [key: string]: string | number | boolean | CheckPreview[] | MetricPreview[] | ExecutionStatusLinkProps;
+}
 
 export interface WorkflowTableData {
   schema?: DataSchema;
   data: WorkflowTableRow[];
+  meta: WorkflowTableRowData[];
 }
 
 export interface WorkflowsTableProps {
   data: WorkflowTableData;
+  searchEnabled?: boolean;
+  onGetColumnValue?: (row, column) => WorkflowTableElement;
 }
 
-export const WorkflowTable: React.FC<WorkflowsTableProps> = ({ data }) => {
+export const WorkflowTable: React.FC<WorkflowsTableProps> = ({ data, onGetColumnValue, searchEnabled = false }) => {
   const [page, setPage] = React.useState(0);
   const [rowsPerPage, setRowsPerPage] = React.useState(5);
+  const [searchQuery, setSearchQuery] = React.useState('');
+  const [filterColumn, setFilterColumn] = React.useState('name');
 
-  const rows = data.data;
+  let rows = data.data;
   const columns = data.schema.fields;
+
+  let filteredRows = [];
+  //let filteredColumns = columns;
+
+  if (searchQuery.length > 0) {
+    filteredRows = data.meta.filter((rowItem, index) => {
+      // filter rows by name, which is our default filter column.
+      // rowItem.meta.name.toLowercase().includes(searchQuery)
+      console.log('rowItem: ', rowItem);
+      const name = rowItem.name as string;
+      return name.toLowerCase().includes(searchQuery.toLowerCase());
+    })
+  }
+
+  console.log('filteredRows: ', filteredRows);
 
   const handleChangePage = (event: unknown, newPage: number) => {
     setPage(newPage);
@@ -43,78 +73,103 @@ export const WorkflowTable: React.FC<WorkflowsTableProps> = ({ data }) => {
     setPage(0);
   };
 
+  /*
+    Returns the value to be inserted at row, column.
+    If a callback is passed in, uses the onGetColumnValue to support things like rendering arbitrary react components.
+  */
+  const getColumnValue = (row, column) => {
+    // use callback if passed in
+    if (onGetColumnValue) {
+      return onGetColumnValue(row, column);
+    }
+
+    console.log('getColumnValue row: ', row);
+    console.log('getColumnValue column: ', column);
+
+    const value = row[column.name];
+    console.log('getColumnValue: ', value);
+
+    return value;
+  };
+
   return (
-    <Paper sx={{ overflow: 'hidden' }}>
-      <TableContainer sx={{ maxHeight: '400px' }}>
-        <Table stickyHeader aria-label="sticky table">
-          <TableHead>
-            <TableRow>
-              {columns.map((column, columnIndex) => {
-                return (
-                  <TableCell
-                    key={`table-header-col-${columnIndex}`}
-                    align={'left'}
-                    sx={{
-                      backgroundColor: 'blue.900',
-                      color: 'white',
-                      minWidth: '80px',
-                    }}
-                  >
-                    <Box flexDirection="column">
-                      <Typography
-                        variant="body1"
-                        sx={{
-                          textTransform: 'none',
-                          fontFamily: 'monospace',
-                          fontSize: '16px',
-                        }}
-                      >
-                        {column.name}
-                      </Typography>
-                    </Box>
-                  </TableCell>
-                );
-              })}
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {rows
-              .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-              .map((row, rowIndex) => {
-                return (
-                  <TableRow
-                    hover
-                    role="checkbox"
-                    tabIndex={-1}
-                    key={`table-row-${rowIndex}`}
-                  >
-                    {columns.map((column, columnIndex) => {
-                      const value = row[column.name];
-                      return (
-                        <TableCell
-                          key={`table-col-${columnIndex}`}
-                          align={'left'}
+    <>
+      <TextField value={searchQuery} onChange={(event) => setSearchQuery(event.target.value)} id="outlined-basic" label="Search" variant="outlined" />
+      <Paper sx={{ overflow: 'hidden' }}>
+        <TableContainer>
+          <Table stickyHeader aria-label="sticky table">
+            <TableHead>
+              <TableRow>
+                {columns.map((column, columnIndex) => {
+                  return (
+                    <TableCell
+                      key={`table-header-col-${columnIndex}`}
+                      align={'left'}
+                      sx={{
+                        backgroundColor: 'blue.900',
+                        color: 'white',
+                        minWidth: '80px',
+                      }}
+                      onClick={() => {
+                        console.log('tableColumn clicked colIndex: ', columnIndex);
+                        console.log('tableColumn clicked column: ', column);
+                      }}
+                    >
+                      <Box flexDirection="column">
+                        <Typography
+                          variant="body1"
+                          sx={{
+                            textTransform: 'none',
+                            fontFamily: 'monospace',
+                            fontSize: '16px',
+                          }}
                         >
-                          {value}
-                        </TableCell>
-                      );
-                    })}
-                  </TableRow>
-                );
-              })}
-          </TableBody>
-        </Table>
-      </TableContainer>
-      <TablePagination
-        rowsPerPageOptions={[5, 10, 25, 50, 100]}
-        component="div"
-        count={rows.length}
-        rowsPerPage={rowsPerPage}
-        page={page}
-        onPageChange={handleChangePage}
-        onRowsPerPageChange={handleChangeRowsPerPage}
-      />
-    </Paper>
+                          {column.name}
+                        </Typography>
+                      </Box>
+                    </TableCell>
+                  );
+                })}
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {rows
+                .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                .map((row, rowIndex) => {
+                  return (
+                    <TableRow
+                      hover
+                      role="checkbox"
+                      tabIndex={-1}
+                      key={`table-row-${rowIndex}`}
+                    >
+                      {columns.map((column, columnIndex) => {
+                        return (
+                          <TableCell
+                            key={`table-col-${columnIndex}`}
+                            align={'left'}
+                          >
+                            {getColumnValue(row, column)}
+                          </TableCell>
+                        );
+                      })}
+                    </TableRow>
+                  );
+                })}
+            </TableBody>
+          </Table>
+        </TableContainer>
+        <TablePagination
+          rowsPerPageOptions={[5, 10, 25, 50, 100]}
+          component="div"
+          count={rows.length}
+          rowsPerPage={rowsPerPage}
+          page={page}
+          onPageChange={handleChangePage}
+          onRowsPerPageChange={handleChangeRowsPerPage}
+        />
+      </Paper>
+    </>
   );
 };
 
