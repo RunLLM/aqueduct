7eb2062ae1fcf81762f0fcbbcbb8b9bbf891a56c
Eunice Chan <ekchan@berkeley.edu>
2022-11-21 21:16:33 -0800

    alphabetical

--- src/golang/lib/repos/integration.go
+++ src/golang/lib/repos/integration.go
@@ -22,20 +22,20 @@ type integrationReader interface {
 	// GetBatch returns the Integrations with IDs.
 	GetBatch(ctx context.Context, IDs []uuid.UUID, DB database.Database) ([]models.Integration, error)
 
-	// GetByServiceAndUser returns the Integrations with the specified service created by the user with the ID userID.
-	GetByServiceAndUser(ctx context.Context, service shared.Service, userID uuid.UUID, DB database.Database) ([]models.Integration, error)
-
-	// GetByOrg returns the Integrations by the organization with the ID orgID.
-	GetByOrg(ctx context.Context, orgId string, DB database.Database) ([]models.Integration, error)
-
-	// GetByUser returns the Integrations by the user with the ID userID in the organization with the ID orgID.
-	GetByUser(ctx context.Context, orgID string, userID uuid.UUID, DB database.Database) ([]models.Integration, error)
+	// GetByConfigField returns the Integrations with config fieldName=fieldValue.
+	GetByConfigField(ctx context.Context, fieldName string, fieldValue string, DB database.Database) ([]models.Integration, error)
 
 	// GetByNameAndUser returns the Integrations with name integrationName by the user with the ID userID in the organization with the ID orgID.
 	GetByNameAndUser(ctx context.Context, integrationName string, userID uuid.UUID, orgID string, DB database.Database) ([]models.Integration, error)
 
-	// GetByConfigField returns the Integrations with config fieldName=fieldValue.
-	GetByConfigField(ctx context.Context, fieldName string, fieldValue string, DB database.Database) ([]models.Integration, error)
+	// GetByOrg returns the Integrations by the organization with the ID orgID.
+	GetByOrg(ctx context.Context, orgId string, DB database.Database) ([]models.Integration, error)
+
+	// GetByServiceAndUser returns the Integrations with the specified service created by the user with the ID userID.
+	GetByServiceAndUser(ctx context.Context, service shared.Service, userID uuid.UUID, DB database.Database) ([]models.Integration, error)
+
+	// GetByUser returns the Integrations by the user with the ID userID in the organization with the ID orgID.
+	GetByUser(ctx context.Context, orgID string, userID uuid.UUID, DB database.Database) ([]models.Integration, error)
 
 	// ValidateOwnership checks whether the integration is owned by the user if the integration is of type userOnly otherwise, checks whether the integration is owned by the organizaion orgID.
 	ValidateOwnership(ctx context.Context, integrationID uuid.UUID, orgID string, userID uuid.UUID, DB database.Database) (bool, error)
