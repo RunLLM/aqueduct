743685bc102264753db645628dd2ebc3446fae15
Eunice Chan <ekchan@berkeley.edu>
2022-11-23 09:21:10 -0800

    sqlite impl

--- src/golang/lib/repos/sqlite/notification.go
+++ src/golang/lib/repos/sqlite/notification.go
@@ -5,6 +5,7 @@ import (
 	"crypto/rand"
 	"fmt"
 
+	"github.com/aqueducthq/aqueduct/lib/models/shared"
 	"github.com/aqueducthq/aqueduct/lib/collections/utils"
 	"github.com/aqueducthq/aqueduct/lib/database"
 	"github.com/aqueducthq/aqueduct/lib/models"
@@ -30,10 +31,25 @@ func NewNotificationRepo() repos.Notification {
 }
 
 func (*notificationReader) GetByReceiver(ctx context.Context, receiverID uuid.UUID, status shared.NotificationStatus, DB database.Database) ([]models.Notification, error) {
-	
+	query := fmt.Sprintf(
+		`SELECT %s FROM notification WHERE receiver_id = $1 AND status = $2;`,
+		models.NotificationCols(),
+	)
+	args := []interface{}{receiverID, status}
+
+	return getNotifications(ctx, DB, query, args...)
 }
 
 func (*notificationReader) ValidateUser(ctx context.Context, notificationID uuid.UUID, userID uuid.UUID, DB database.Database) (bool, error) {
+	query := `SELECT COUNT(*) AS count FROM notification WHERE id = $1 AND receiver_id = $2;`
+	var count utils.CountResult
+
+	err := db.Query(ctx, &count, query, notificationId, userId)
+	if err != nil {
+		return false, err
+	}
+
+	return count.Count == 1, nil
 
 }
 
@@ -45,9 +61,67 @@ func (*notificationWriter) Create(
 	association shared.NotificationAssociation,
 	DB database.Database,
 ) (*models.Notification, error) {
+	cols := []string{
+		models.NotificationID,
+		models.NotificationReceiverID,
+		models.NotificationContent,
+		models.NotificationStatus,
+		models.NotificationLevel,
+		models.NotificationAssociation,
+		models.NotificationCreatedAt,
+	}
+	query := DB.PrepareInsertWithReturnAllStmt(models.NotificationTable, cols, models.NotificationCols())
+
+	ID, err := utils.GenerateUniqueUUID(ctx, models.UserTable, DB)
+	if err != nil {
+		return nil, err
+	}
+
+	args := []interface{}{
+		ID,
+		receiverID, 
+		content, 
+		shared.UnreadStatus, 
+		level, 
+		association, 
+		time.Now(),
+	}
+	return getNotification(ctx, DB, query, args...)
 	
 }
 
 func (*notificationWriter) Update(ctx context.Context, ID uuid.UUID, status shared.NotificationStatus, DB database.Database) (*models.Notification, error) {
-	
+	changedColumns := map[string]interface{}{
+		StatusColumn: status,
+	}
+	return updateNotification(ctx, id, changedColumns, db)
+}
+
+func updateNotification(ctx context.Context, ID uuid.UUID, changes map[string]interface{}, DB database.Database) (*models.Notification, error) {
+	var notification models.Notification
+	err := utils.UpdateRecordToDest(ctx, &notification, changes, models.NotificationTable, moddels.NotificationID, ID, model.NotificationCols(), DB)
+	return &notification, err
+}
+
+func getNotifications(ctx context.Context, DB database.Database, query string, args ...interface{}) ([]models.Notification, error) {
+	var notifications []models.Notification
+	err := DB.Query(ctx, &notifications, query, args...)
+	return notifications, err
+}
+
+func getNotification(ctx context.Context, DB database.Database, query string, args ...interface{}) (*models.Notification, error) {
+	notifications, err := getNotifications(ctx, DB, query, args...)
+	if err != nil {
+		return nil, err
+	}
+
+	if len(notifications) == 0 {
+		return nil, database.ErrNoRows
+	}
+
+	if len(notifications) != 1 {
+		return nil, errors.Newf("Expected 1 artifact but got %v", len(artifacts))
+	}
+
+	return &notifications[0], nil
 }
