5c2c518baf889493be7533014c9394fd6dc2740e
Eunice Chan <ekchan@berkeley.edu>
2022-11-30 11:10:20 -0800

    Test framework

--- src/golang/lib/repos/tests/artifact_result.go
+++ src/golang/lib/repos/tests/artifact_result.go
@@ -6,7 +6,7 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func (ts *TestSuite) TestUser_GetByAPIKey() {
+func (ts *TestSuite) TestArtifactResult_Get() {
 	users := ts.seedUser(1)
 	expectedUser := &users[0]
 
@@ -15,7 +15,52 @@ func (ts *TestSuite) TestUser_GetByAPIKey() {
 	requireDeepEqual(ts.T(), expectedUser, actualUser)
 }
 
-func (ts *TestSuite) TestUser_Create() {
+func (ts *TestSuite) TestArtifactResult_GetBatch() {
+	users := ts.seedUser(1)
+	expectedUser := &users[0]
+
+	actualUser, err := ts.user.GetByAPIKey(ts.ctx, expectedUser.APIKey, ts.DB)
+	require.Nil(ts.T(), err)
+	requireDeepEqual(ts.T(), expectedUser, actualUser)
+}
+
+func (ts *TestSuite) TestArtifactResult_GetByArtifact() {
+	users := ts.seedUser(1)
+	expectedUser := &users[0]
+
+	actualUser, err := ts.user.GetByAPIKey(ts.ctx, expectedUser.APIKey, ts.DB)
+	require.Nil(ts.T(), err)
+	requireDeepEqual(ts.T(), expectedUser, actualUser)
+}
+
+func (ts *TestSuite) TestArtifactResult_GetByArtifactAndWorkflow() {
+	users := ts.seedUser(1)
+	expectedUser := &users[0]
+
+	actualUser, err := ts.user.GetByAPIKey(ts.ctx, expectedUser.APIKey, ts.DB)
+	require.Nil(ts.T(), err)
+	requireDeepEqual(ts.T(), expectedUser, actualUser)
+}
+
+func (ts *TestSuite) TestArtifactResult_GetByArtifactAndDAGResult() {
+	users := ts.seedUser(1)
+	expectedUser := &users[0]
+
+	actualUser, err := ts.user.GetByAPIKey(ts.ctx, expectedUser.APIKey, ts.DB)
+	require.Nil(ts.T(), err)
+	requireDeepEqual(ts.T(), expectedUser, actualUser)
+}
+
+func (ts *TestSuite) TestArtifactResult_GetByDAGResults() {
+	users := ts.seedUser(1)
+	expectedUser := &users[0]
+
+	actualUser, err := ts.user.GetByAPIKey(ts.ctx, expectedUser.APIKey, ts.DB)
+	require.Nil(ts.T(), err)
+	requireDeepEqual(ts.T(), expectedUser, actualUser)
+}
+
+func (ts *TestSuite) TestArtifactResult_Create() {
 	apiKey := randAPIKey()
 
 	expectedUser := &models.User{
@@ -35,12 +80,82 @@ func (ts *TestSuite) TestUser_Create() {
 	requireDeepEqual(ts.T(), expectedUser, actualUser)
 }
 
-func (ts *TestSuite) TestUser_ResetAPIKey() {
-	users := ts.seedUser(1)
-	user := users[0]
+func (ts *TestSuite) TestArtifactResult_CreateWithExecStateAndMetadata() {
+	apiKey := randAPIKey()
 
-	updatedUser, err := ts.user.ResetAPIKey(ts.ctx, user.ID, ts.DB)
+	expectedUser := &models.User{
+		APIKey: apiKey,
+	}
+
+	actualUser, err := ts.user.Create(ts.ctx, testOrgID, apiKey, ts.DB)
 	require.Nil(ts.T(), err)
 
-	require.NotEqual(ts.T(), user.APIKey, updatedUser.APIKey)
+	require.NotEqual(ts.T(), uuid.Nil, actualUser.ID)
+
+	expectedUser.ID = actualUser.ID
+	expectedUser.Email = actualUser.Email
+	expectedUser.OrgID = actualUser.OrgID
+	expectedUser.Role = actualUser.Role
+	expectedUser.Auth0ID = actualUser.Auth0ID
+	requireDeepEqual(ts.T(), expectedUser, actualUser)
+}
+
+func (ts *TestSuite) TestArtifactResult_Delete() {
+	apiKey := randAPIKey()
+
+	expectedUser := &models.User{
+		APIKey: apiKey,
+	}
+
+	actualUser, err := ts.user.Create(ts.ctx, testOrgID, apiKey, ts.DB)
+	require.Nil(ts.T(), err)
+
+	require.NotEqual(ts.T(), uuid.Nil, actualUser.ID)
+
+	expectedUser.ID = actualUser.ID
+	expectedUser.Email = actualUser.Email
+	expectedUser.OrgID = actualUser.OrgID
+	expectedUser.Role = actualUser.Role
+	expectedUser.Auth0ID = actualUser.Auth0ID
+	requireDeepEqual(ts.T(), expectedUser, actualUser)
+}
+
+func (ts *TestSuite) TestArtifactResult_DeleteBatch() {
+	apiKey := randAPIKey()
+
+	expectedUser := &models.User{
+		APIKey: apiKey,
+	}
+
+	actualUser, err := ts.user.Create(ts.ctx, testOrgID, apiKey, ts.DB)
+	require.Nil(ts.T(), err)
+
+	require.NotEqual(ts.T(), uuid.Nil, actualUser.ID)
+
+	expectedUser.ID = actualUser.ID
+	expectedUser.Email = actualUser.Email
+	expectedUser.OrgID = actualUser.OrgID
+	expectedUser.Role = actualUser.Role
+	expectedUser.Auth0ID = actualUser.Auth0ID
+	requireDeepEqual(ts.T(), expectedUser, actualUser)
+}
+
+func (ts *TestSuite) TestArtifactResult_Update() {
+	apiKey := randAPIKey()
+
+	expectedUser := &models.User{
+		APIKey: apiKey,
+	}
+
+	actualUser, err := ts.user.Create(ts.ctx, testOrgID, apiKey, ts.DB)
+	require.Nil(ts.T(), err)
+
+	require.NotEqual(ts.T(), uuid.Nil, actualUser.ID)
+
+	expectedUser.ID = actualUser.ID
+	expectedUser.Email = actualUser.Email
+	expectedUser.OrgID = actualUser.OrgID
+	expectedUser.Role = actualUser.Role
+	expectedUser.Auth0ID = actualUser.Auth0ID
+	requireDeepEqual(ts.T(), expectedUser, actualUser)
 }
