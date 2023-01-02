71490d7f256b2a166a6710c3b5ddb5de04e533a8
Kenneth Xu <kenxu95@gmail.com>
2023-01-02 11:36:19 -0800

    unpointered the load spec

--- src/golang/lib/models/views/operator.go
+++ src/golang/lib/models/views/operator.go
@@ -10,11 +10,10 @@ import (
 
 // LoadOperator contains metadata about a Load Operator
 type LoadOperator struct {
-	OperatorName    string    `db:"operator_name" json:"operator_name"`
-	ModifiedAt      time.Time `db:"modified_at" json:"modified_at"`
-	IntegrationName string    `db:"integration_name" json:"integration_name"`
-
-	Spec *connector.Load `db:"spec" json:"spec"`
+	OperatorName    string         `db:"operator_name" json:"operator_name"`
+	ModifiedAt      time.Time      `db:"modified_at" json:"modified_at"`
+	IntegrationName string         `db:"integration_name" json:"integration_name"`
+	Spec            connector.Load `db:"spec" json:"spec"`
 }
 
 // LoadOperatorSpec is a wrapper around a Load Operator's spec and other metadata
