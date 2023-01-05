71a60567c9a277f501e6f3760fb77bb24d69ab81
Eunice Chan <ekchan@berkeley.edu>
2023-01-05 10:15:21 -0800

    Update look

--- src/ui/common/src/components/tables/PaginatedSearchTable.tsx
+++ src/ui/common/src/components/tables/PaginatedSearchTable.tsx
@@ -186,13 +186,14 @@ export const PaginatedSearchTable: React.FC<PaginatedSearchTableProps> = ({
                       key={`table-header-col-${columnIndex}`}
                       align={'left'}
                     >
-                      <Box flexDirection="column" padding="8px">
+                      <Box flexDirection="column" padding="8px" sx={{ backgroundColor: theme.palette.gray['50'] }}>
                         <Typography
                           variant="body1"
                           sx={{
                             textTransform: 'capitalize',
-                            fontSize: '14px',
+                            fontSize: '12px',
                             fontWeight: 800,
+                            color: theme.palette.gray['900'],
                           }}
                         >
                           {columnName}
