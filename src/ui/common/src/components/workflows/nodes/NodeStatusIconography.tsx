61264c6848f2e6b44ed1da8a1b80b677a478754f
Eunice Chan <ekchan@berkeley.edu>
2023-01-13 06:37:16 -0800

    Document new function

--- src/ui/common/src/components/workflows/nodes/NodeStatusIconography.tsx
+++ src/ui/common/src/components/workflows/nodes/NodeStatusIconography.tsx
@@ -9,6 +9,15 @@ type Props = {
   successDisplay: JSX.Element;
 };
 
+  /**
+   * Function used to determine what to display for DAG check or metric nodes.
+   * The icons are a bit larger than the default StatusIndicator size, monochrome (black), 
+   * and are followed by a label. It also handles the state when we haven't fetched execState 
+   * yet and displays the check/metric value if the node was successfully executed.
+   * @param execState - The operator's execution state, if it exists.
+   * @param successDisplay - What to display if the operator was successfully executed.
+   * @returns - JSX element to be displayed on the node.
+   */
 export const NodeStatusIconography: React.FC<Props> = ({
   execState,
   successDisplay,
