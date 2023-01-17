b3c43762fe15d60d186ea142a0d3dfb3649ac906
kenxu95 <kenxu@aqueducthq.com>
2023-01-17 10:02:05 -0800

    Add ability to connect to integrations from the SDK (#882)

---
+++ sdk/aqueduct/integrations/__init__.py
@@ -0,0 +1,12 @@
+from aqueduct.integrations.connect_config import (
+    AthenaConfig,
+    BigQueryConfig,
+    IntegrationConfig,
+    MongoDBConfig,
+    MySQLConfig,
+    PostgresConfig,
+    S3Config,
+    SnowflakeConfig,
+    SQLiteConfig,
+    SqlServerConfig,
+)
