41ca18124b2b2fca0291721065a727d02aec57e5
likawind <cwinddavid@gmail.com>
2023-01-19 09:26:48 +0000

    fix auth

--- src/golang/lib/notification/email.go
+++ src/golang/lib/notification/email.go
@@ -1,18 +1,24 @@
 package notification
 
 import (
+	"crypto/tls"
 	"net/smtp"
 
 	"github.com/aqueducthq/aqueduct/lib/collections/integration"
 )
 
 func AuthenticateEmail(conf *integration.EmailConfig) error {
-	auth := smtp.PlainAuth("", conf.User, conf.Password, conf.FullHost())
+	auth := smtp.PlainAuth("", conf.User, conf.Password, conf.Host)
 	client, err := smtp.Dial(conf.FullHost())
 	if err != nil {
 		return err
 	}
 
-	defer client.Close()
+	err = client.StartTLS(&tls.Config{ServerName: conf.Host})
+	if err != nil {
+		return err
+	}
+
+	defer client.Quit()
 	return client.Auth(auth)
 }
