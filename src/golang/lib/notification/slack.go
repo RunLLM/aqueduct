aaec7c67240a1acd1e8e2136f5cec92bcc442bb9
Wei Chen <wei@aqueducthq.com>
2023-01-27 13:35:38 -0800

    Eng 2194 m2 implement go interface for (#923)

--- src/golang/lib/notification/slack.go
+++ src/golang/lib/notification/slack.go
@@ -5,6 +5,19 @@ import (
 	"github.com/slack-go/slack"
 )
 
+type SlackNotification struct {
+	conf *shared.SlackConfig
+}
+
+func newSlackNotification(conf *shared.SlackConfig) *SlackNotification {
+	return &SlackNotification{conf: conf}
+}
+
+func (e *SlackNotification) Send(msg string, level shared.NotificationLevel) error {
+	// TODO: Implement
+	return nil
+}
+
 func AuthenticateSlack(conf *shared.SlackConfig) error {
 	client := slack.New(conf.Token)
 	_, err := client.AuthTest()
