610e222587d85648689f15b0e492df1f7dce1234
cw75 <cgwu0530@gmail.com>
2023-02-07 00:47:58 +0000

    add param test

--- integration_tests/sdk/data_integration_tests/athena_test.py
+++ integration_tests/sdk/data_integration_tests/athena_test.py
@@ -1,9 +1,9 @@
 import pandas as pd
 import pytest
-from aqueduct.error import InvalidUserActionException
+from aqueduct.error import InvalidUserActionException, InvalidUserArgumentException
 from aqueduct.integrations.sql_integration import RelationalDBIntegration
 
-from aqueduct import LoadUpdateMode, op
+from aqueduct import LoadUpdateMode, metric, op
 
 from ..shared.naming import generate_table_name
 from .save import save
@@ -46,3 +46,57 @@ def test_athena_save(client, data_integration):
         match="Save operation not supported for Athena.",
     ):
         save(data_integration, generate_table(), generate_table_name(), LoadUpdateMode.REPLACE)
+
+
+def test_athena_query_with_parameter(client, data_integration):
+    # Missing parameters.
+    with pytest.raises(InvalidUserArgumentException):
+        _ = data_integration.sql(query="select * from {{missing_parameter}}")
+
+    # The parameter is not a string type.
+    _ = client.create_param("table_name", default=1234)
+    with pytest.raises(InvalidUserArgumentException):
+        _ = data_integration.sql(query="select * from {{ table_name }}")
+
+    client.create_param("table_name", default="hotel_reviews")
+    table_artifact = data_integration.sql(query="select * from {{ table_name }}")
+
+    expected_table_artifact = data_integration.sql(query="select * from hotel_reviews")
+    assert table_artifact.get().equals(expected_table_artifact.get())
+
+    # Trigger the parameter with invalid values.
+    with pytest.raises(InvalidUserArgumentException):
+        _ = table_artifact.get(parameters={"table_name": ["this is the incorrect type"]})
+    with pytest.raises(InvalidUserArgumentException):
+        _ = table_artifact.get(parameters={"non-existant parameter": "blah"})
+
+
+def test_athena_query_with_multiple_parameters(client, flow_manager, data_integration):
+    _ = client.create_param("table_name", default="hotel_reviews")
+    nationality = client.create_param(
+        "reviewer-nationality", default="United Kingdom"
+    )  # check that dashes work.
+    table_artifact = data_integration.sql(
+        query="select * from {{ table_name }} where reviewer_nationality='{{ reviewer-nationality }}' and review_date < {{ today}}"
+    )
+    expected_table_artifact = data_integration.sql(
+        "select * from hotel_reviews where reviewer_nationality='United Kingdom' and review_date < {{today}}"
+    )
+    assert table_artifact.get().equals(expected_table_artifact.get())
+    expected_table_artifact = data_integration.sql(
+        "select * from hotel_reviews where reviewer_nationality='Australia' and review_date < {{today}}"
+    )
+    assert table_artifact.get(parameters={"reviewer-nationality": "Australia"}).equals(
+        expected_table_artifact.get()
+    )
+
+    # Use the parameters in another operator.
+    @metric
+    def noop(sql_output, param):
+        return len(param)
+
+    result = noop(table_artifact, nationality)
+    assert result.get() == len(nationality.get())
+    assert result.get(parameters={"reviewer-nationality": "Australia"}) == len("Australia")
+
+    flow_manager.publish_flow_test(artifacts=[result])
