0ff941b52a765cbcdfd5f7219d0552eaffa43d5c
Eunice Chan <eunice@aqueducthq.com>
2023-02-07 12:38:19 -0800

    bump version numbers

--- sdk/setup.py
+++ sdk/setup.py
@@ -18,7 +18,7 @@ else:
 
 setuptools.setup(
     name="aqueduct-sdk",
-    version="0.2.0",
+    version="0.2.1",
     author="Aqueduct, Inc.",
     author_email="hello@aqueducthq.com",
     description="Python SDK for the Aqueduct prediction infrastructure",
