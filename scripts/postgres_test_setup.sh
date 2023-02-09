79e920f53b503528889541ec305714551fc72c35
Andre Giron <andre@spiralai.co>
2023-02-09 14:31:02 -0800

    Updates postgres start script to be just one line.

--- scripts/postgres_test_setup.sh
+++ scripts/postgres_test_setup.sh
@@ -1,16 +1,4 @@
 #!/bin/bash
 
 # Create the Docker container
-docker run --name aqueduct-postgres -e POSTGRES_PASSWORD=aqueduct -d -p 5432:5432 postgres
-
-# Wait for the database to be ready
-echo "Waiting for database container to be ready ..."
-until docker exec aqueduct-postgres psql -U postgres -c '\l' &> /dev/null; do
-  sleep 1
-done
-
-# Create the 'aqueducttest' database
-echo "Creating aqueducttest database ..."
-docker exec -it aqueduct-postgres psql -U postgres -c "CREATE DATABASE aqueducttest;"
-
-echo "PostgreSQL container with the 'aqueducttest' database created successfully."
+docker run --name aqueduct-postgres -e POSTGRES_PASSWORD=aqueduct -e POSTGRES_DB=aqueducttest -d -p 5432:5432 postgres
