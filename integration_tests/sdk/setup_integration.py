6d80293a69f482cc6657f0c79987d5b961c88f82
Eunice Chan <eunice@aqueducthq.com>
2023-02-10 16:21:10 +0000

    MariaDB table_name -> tablename

--- integration_tests/sdk/setup_integration.py
+++ integration_tests/sdk/setup_integration.py
@@ -135,13 +135,7 @@ def _setup_mongo_db_data(client: Client, mongo_db: MongoDBIntegration) -> None:
 def _setup_relational_data(client: Client, db: RelationalDBIntegration) -> None:
     # Find all the tables that already exist.
     columns = list(db.list_tables().columns)
-    if "tablename" in columns:
-        existing_table_names = set(db.list_tables()["tablename"])
-    elif "table_name" in columns:
-        existing_table_names = set(db.list_tables()["table_name"])
-    else:
-        raise Exception(f"Cannot find table names in: {columns}")
-    _add_missing_artifacts(client, db, existing_table_names)
+    _add_missing_artifacts(client, db, set(db.list_tables()["tablename"]))
 
 
 def _setup_s3_data(client: Client, s3: S3Integration):
