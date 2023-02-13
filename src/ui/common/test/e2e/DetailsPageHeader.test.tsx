330a6ecd27079979a645642a46a8c113c75c43f3
Andre Giron <andre@spiralai.co>
2023-02-13 14:15:23 -0800

    lint

--- src/ui/common/test/e2e/DetailsPageHeader.test.tsx
+++ src/ui/common/test/e2e/DetailsPageHeader.test.tsx
@@ -1,12 +1,19 @@
 import * as React from 'react';
-import * as ReactDOM from 'react-dom';
-import { mockUser, render, screen } from '../testUtils';
+
 import { DetailsPageHeader } from '../../src/components/pages/components/DetailsPageHeader';
 import ExecutionStatus from '../../src/utils/shared';
+import { render } from '../testUtils';
 
 describe('it', () => {
   it('renders DetailsPageHeader component', () => {
     const now = new Date().toString();
-    render(<DetailsPageHeader name={'TestUser'} status={ExecutionStatus.Succeeded} createdAt={now} sourceLocation={'/src/test/e23'} />);
+    render(
+      <DetailsPageHeader
+        name={'TestUser'}
+        status={ExecutionStatus.Succeeded}
+        createdAt={now}
+        sourceLocation={'/src/test/e23'}
+      />
+    );
   });
 });
