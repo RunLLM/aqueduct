e9c85e4963f833b9414ed84d4e5afc3d67320cd3
Andre Giron <andre@spiralai.co>
2023-02-22 10:51:27 -0800

    Remove storageLocation from StorageSelector component.

--- src/ui/common/src/components/workflows/storageSelector.tsx
+++ src/ui/common/src/components/workflows/storageSelector.tsx
@@ -14,24 +14,8 @@ export const StorageSelector: React.FC = () => {
   const workflow = useSelector((state: RootState) => state.workflowReducer);
   const dag = workflow.selectedDag;
   let selected = 'file';
-  let selectedLocation = null;
   if (dag) {
     selected = dag.storage_config.type;
-    switch (selected) {
-      case 's3': {
-        selectedLocation = dag.storage_config.s3_config.bucket;
-        break;
-      }
-      case 'gcs': {
-        selectedLocation = dag.storage_config.gcs_config.bucket;
-        break;
-      }
-      case 'file':
-      default: {
-        selectedLocation = dag.storage_config.file_config.directory;
-        break;
-      }
-    }
   }
 
   const getMenuItems = () => {
@@ -59,10 +43,7 @@ export const StorageSelector: React.FC = () => {
   return (
     <Box>
       <Box sx={{ display: 'flex' }}>
-        <Typography style={{ fontWeight: 'bold' }}>
-          Metadata Storage Location:
-        </Typography>
-        <Typography sx={{ marginLeft: '8px' }}>{selectedLocation}</Typography>
+        <Typography style={{ fontWeight: 'bold' }}>Metadata Storage</Typography>
       </Box>
       <Typography variant="body2">
         For more details on modifying the Aqueduct metadata store, please see{' '}
