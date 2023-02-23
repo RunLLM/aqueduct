c78128e0eb6ee947a8169e40d96d812a7c5804f4
Andre Giron <andre@spiralai.co>
2023-02-23 15:12:20 -0800

    Lint and update demo code

--- src/ui/common/cypress/e2e/hellocypress.cy.ts
+++ src/ui/common/cypress/e2e/hellocypress.cy.ts
@@ -1,7 +1,7 @@
 describe('template spec', () => {
   it('passes', () => {
     cy.visit('http://localhost:8080');
-    cy.get('input').type('0O54NMDIGET2RJQ1XLVK3YUZB9FC7S86');
-    cy.get('button').click()
-  })
-})
+    cy.get('input').type('O8H9E273PQJ4FWMA61VSDIB0GZNXLKYT');
+    cy.get('button').click();
+  });
+});
