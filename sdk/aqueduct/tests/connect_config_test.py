4e53e8215a759dcc60a5087841dffe1886696c61
Kenneth Xu <kenxu95@gmail.com>
2023-02-24 19:39:51 -0800

    ready now

--- sdk/aqueduct/tests/connect_config_test.py
+++ sdk/aqueduct/tests/connect_config_test.py
@@ -5,5 +5,5 @@ def test_consistent_use_as_storage_field_name_for_storage_layer_integrations():
     # Checks that all data connection configs have the same field name for using as storage layer.
     # This is a necessary assumption to enforce for our integration test setup.
     field_name = "use_as_storage"
-    assert hasattr(S3Config, field_name)
-    assert hasattr(GCSConfig, field_name)
+    assert field_name in S3Config.__fields__.keys()
+    assert field_name in GCSConfig.__fields__.keys()
