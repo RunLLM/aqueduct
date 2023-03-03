17a7307ccbc7cc1f25b908f34de871a16e9fab6f
Andre Giron <andre@spiralai.co>
2023-03-02 17:37:31 -0800

    Adds metadata label to GCS card."

--- src/ui/common/src/components/integrations/cards/gcsCard.tsx
+++ src/ui/common/src/components/integrations/cards/gcsCard.tsx
@@ -1,21 +1,62 @@
+import { faTags } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 
+import { handleGetServerConfig } from '../../../handlers/getServerConfig';
+import { RootState } from '../../../stores/store';
 import { GCSConfig, Integration } from '../../../utils/integrations';
+import useUser from '../../hooks/useUser';
 
 type Props = {
   integration: Integration;
 };
 
 export const GCSCard: React.FC<Props> = ({ integration }) => {
+  const { user } = useUser();
+  const dispatch = useDispatch();
   const config = integration.config as GCSConfig;
+  const serverConfig = useSelector(
+    (state: RootState) => state.serverConfigReducer
+  );
+  const storageConfig = serverConfig?.config?.storageConfig;
+
+  useEffect(() => {
+    async function fetchServerConfig() {
+      if (user) {
+        dispatch(handleGetServerConfig({ apiKey: user.apiKey }));
+      }
+    }
+
+    fetchServerConfig();
+  }, [user]);
+
+  let dataStorageInfo,
+    dataStorageText = null;
+
+  if (storageConfig && storageConfig.type == 'gcs') {
+    dataStorageInfo = (
+      <Box component="span">
+        <FontAwesomeIcon icon={faTags} />
+      </Box>
+    );
+
+    dataStorageText = (
+      <Typography variant={'body2'}>
+        <strong>Storage Type:</strong> {dataStorageInfo}
+      </Typography>
+    );
+  }
+
   return (
     <Box>
       <Typography variant="body2">
         <strong>Bucket: </strong>
         {config.bucket}
       </Typography>
+      {dataStorageText}
     </Box>
   );
 };
