2f7a07a7681562b2509e4740b55d40de7778f421
Chenggang Wu <cgwu0530@gmail.com>
2023-03-03 23:43:26 -0800

    Make dynamic k8s cluster resilient against server failures and race conditions (#1040)

--- src/golang/lib/engine/engine.go
+++ src/golang/lib/engine/engine.go
@@ -14,7 +14,7 @@ import (
 )
 
 const (
-	DefaultExecutionTimeout     = 15 * time.Minute
+	DefaultExecutionTimeout     = 30 * time.Minute
 	DefaultCleanupTimeout       = 2 * time.Minute
 	DefaultPollIntervalMillisec = 300
 )
