4388d1eead6ac9c9a8bd21c6c63510c2ab17c4e1
Vikram Sreekanti <vsreekanti@gmail.com>
2023-03-07 08:49:08 -0800

    [ENG-2573] Updates DAG node design (#1057)

--- src/ui/common/src/components/workflows/nodes/DatabaseNode.tsx
+++ src/ui/common/src/components/workflows/nodes/DatabaseNode.tsx
@@ -3,6 +3,7 @@ import React, { memo } from 'react';
 
 import { ReactFlowNodeData } from '../../../utils/reactflow';
 import Node from './Node';
+import { operatorNodeStatusLabels } from './nodeTypes';
 
 type Props = {
   data: ReactFlowNodeData;
@@ -18,6 +19,7 @@ const DatabaseNode: React.FC<Props> = ({ data, isConnectable }) => {
       data={data}
       isConnectable={isConnectable}
       defaultLabel="Database"
+      statusLabels={operatorNodeStatusLabels}
     />
   );
 };
