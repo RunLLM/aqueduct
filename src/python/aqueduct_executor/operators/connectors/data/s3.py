66e691b49331f34032ccd5aa07087420d8d30bcf
Fanjia Yan <fanjiayan@Fanjias-MacBook-Pro.local>
2023-03-09 00:27:14 -0800

    add error handling

--- src/python/aqueduct_executor/operators/connectors/data/s3.py
+++ src/python/aqueduct_executor/operators/connectors/data/s3.py
@@ -127,7 +127,17 @@ class S3Connector(connector.DataConnector):
             if len(path) == 0:
                 raise Exception("S3 file path cannot be an empty string.")
             if path[-1] == "/":
-                # This means the path is a directory, and we will do a prefix search.
+                # Check whether the folder path exists in S3 by just checking the folder 
+                # rather than the content in the folder. `MaxKeys = 2` allows file and
+                # directory sharing the same name. The return value will only contains
+                # `CommonPrefixes` if there is a folder match.
+                list_object_dic = self.s3.meta.client.list_objects(Bucket=self.bucket, Prefix=path[:-1], Delimiter='/', MaxKeys = 2)
+                if "CommonPrefixes" not in list_object_dic:
+                    return Exception(
+                    "Given S3 directory path '%s' does not exist"
+                    % (path)
+                    )
+                # This means the path is a valid directory, and we will do a prefix search.
                 files = []
                 for obj in self.s3.Bucket(self.bucket).objects.filter(Prefix=path):
                     # The filter api also returns the directories, so we filter them out.
