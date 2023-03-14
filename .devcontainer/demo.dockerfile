af1ec1aa3ddcfa3302f9009f10e184b535e7a029
Wei Chen <wei@aqueducthq.com>
2023-03-14 00:38:41 -0700

    include notebooks

--- .devcontainer/demo.dockerfile
+++ .devcontainer/demo.dockerfile
@@ -7,10 +7,19 @@ USER root
 ENV PYTHONUNBUFFERED 1
 
 RUN pip install aqueduct-ml
+RUN aqueduct install postgres
 RUN pip install scikit-learn
 
 COPY ./.devcontainer/deploy_demo_workflow.sh /
 
-COPY ./manual_qa_tests /manual_qa_tests
+COPY ./.devcontainer/demo_setup.py /
+
+COPY ./.devcontainer/deploy_example.py /
+
+COPY ./.devcontainer/demo.ipynb /notebook/demo.ipynb
 
 RUN bash /deploy_demo_workflow.sh
+
+RUN rm /demo_setup.py
+RUN rm /deploy_example.py
+RUN rm /deploy_demo_workflow.sh
--- manual_qa_tests/workflows/fail_bad_operator.py
+++ manual_qa_tests/workflows/fail_bad_operator.py
@@ -1,6 +1,4 @@
 import aqueduct as aq
-from aqueduct.constants.enums import ExecutionStatus
-import time
 
 NAME = "fail_bad_op"
 DESCRIPTION = """* Workflows Page: should fail.
@@ -35,46 +33,9 @@ def deploy(client, integration_name):
     row_count_artf = row_count.lazy(bad_op_artf)
     # using lazy() to bypass preview
     check_artf = check.lazy(row_count_artf)
-    flow = client.publish_flow(
+    client.publish_flow(
         artifacts=[check_artf],
         name=NAME,
         description=DESCRIPTION,
         schedule="",
     )
-
-    polling(
-        lambda: stop_condition(flow),
-        timeout=600,
-        poll_threshold=10,
-        timeout_comment="Timed out waiting for workflow run to complete.",
-    )
-
-def stop_condition(flow):
-    # Last run is prepended to this list. We need to reverse it in order to compare with expected statuses,
-    # which is sorted in chronologically ascending order.
-    runs = list(reversed(flow.list_runs()))
-    if len(runs) < 1:
-        return False
-    
-    flow_run = runs[0]
-
-    # Continue checking as long as there are still runs pending.
-    if flow_run["status"] == ExecutionStatus.PENDING:
-        return False
-    return True
-
-def polling(
-    stop_condition_fn,
-    timeout=60,
-    poll_threshold=5,
-    timeout_comment="Timed out waiting for workflow run to complete.",
-):
-    begin = time.time()
-
-    while True:
-        assert time.time() - begin < timeout, timeout_comment
-
-        if stop_condition_fn():
-            break
-        else:
-            time.sleep(poll_threshold)
