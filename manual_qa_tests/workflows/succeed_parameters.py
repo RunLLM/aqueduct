9c8d21a88a957d5fa77ab06b781de16077e1c978
Andre Giron <andre@spiralai.co>
2023-03-18 21:01:04 -0700

    Updaates succeed_parameters manual qa test to include empty string parameter example.

--- manual_qa_tests/workflows/succeed_parameters.py
+++ manual_qa_tests/workflows/succeed_parameters.py
@@ -7,21 +7,27 @@ DESCRIPTION = """* Workflows Page: should succeed.
 * Workflow Details Page: everything should be green.
     * There should be 2 versions.
     * Click into `bound` parameters, the value of the later version should be 5.
-    The value of the older version should be 10."""
+    The value of the older version should be 10.
+    * Click into `empty_str` parameter. Nothing should show underneath Preview header."""
 
 
 @aq.check(requirements=[])
 def check(df, bound):
     return df.shape[0] > bound
 
+@aq.check(requirements=[])
+def empty_str_check(df, empty_str):
+    return len(empty_str) == 0
 
 def deploy(client, integration_name):
     integration = client.integration(integration_name)
     client.create_param("table", default="hotel_reviews")
     bound = client.create_param("bound", default=10)
+    empty_str = client.create_param("empty_str", default="")
 
     reviews = integration.sql("SELECT * FROM {{ table }}")
     check(reviews, bound)
+    empty_str_check(reviews, empty_str)
     flow = client.publish_flow(
         artifacts=[reviews],
         name=NAME,
@@ -29,4 +35,4 @@ def deploy(client, integration_name):
         schedule="",
     )
     time.sleep(5)
-    client.trigger(flow.id(), parameters={"bound": 5})
+    client.trigger(flow.id(), parameters={"bound": 5, "empty_str": ""})
