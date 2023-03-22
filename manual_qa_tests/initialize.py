52c14d8f3f061f8b8cf7a960cecd93a613703286
Andre Giron <andre@spiralai.co>
2023-03-22 15:41:48 -0700

    Adds test case for dag node layout

--- manual_qa_tests/initialize.py
+++ manual_qa_tests/initialize.py
@@ -5,13 +5,14 @@ from aqueduct.constants.enums import NotificationLevel
 from notification import connect_slack
 from wait_for_flows import wait_for_all_flows_to_complete
 from workflows import (
-    check_status_test,
-    fail_bad_check,
-    fail_bad_operator,
-    no_run,
-    succeed_complex,
-    succeed_parameters,
-    warning_bad_check,
+    dag_layout_test,
+    # check_status_test,
+    # fail_bad_check,
+    # fail_bad_operator,
+    # no_run,
+    # succeed_complex,
+    # succeed_parameters,
+    # warning_bad_check,
 )
 
 import aqueduct as aq
@@ -19,13 +20,14 @@ import aqueduct as aq
 # when adding new deployments, keep the order of `fail`, `warning`, and `succeed`
 # such that the UI would approximately show these workflows in reverse order.
 WORKFLOW_PKGS = [
-    check_status_test,
-    fail_bad_check,
-    warning_bad_check,
-    succeed_parameters,
-    succeed_complex,
-    fail_bad_operator,
-    no_run,
+    dag_layout_test
+    # check_status_test,
+    # fail_bad_check,
+    # warning_bad_check,
+    # succeed_parameters,
+    # succeed_complex,
+    # fail_bad_operator,
+    # no_run,
 ]
 
 DEMO_NOTEBOOKS_PATHS = [
