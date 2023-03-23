7208b8e41493978daa1be75da37c51b74ea97191
Fanjia Yan <fanjiayan@fanjias-mbp.lan>
2023-03-22 19:27:00 -0700

    golang lint

--- src/golang/cmd/server/request/payload.go
+++ src/golang/cmd/server/request/payload.go
@@ -55,10 +55,10 @@ func ExtractHttpPayload(contentType, fileName string, isFile bool, r *http.Reque
 		} else {
 			// If we reach here, it means the value is sent as `String` instead of `File`,
 			// so we use `FormValue` to extract the payload and return its byte form.
-			// The caller can later parse the returned value to string. By default, the 
+			// The caller can later parse the returned value to string. By default, the
 			// request's body size is capped at `defaultMaxMemory = 32 << 20`. In addition
-			// `multipartReader` gives an addition of 10MB upon defaultMaxMemory for 
-			// nonfile data. Therefore this adds up to a maximum request body size of 42MB. 
+			// `multipartReader` gives an addition of 10MB upon defaultMaxMemory for
+			// nonfile data. Therefore this adds up to a maximum request body size of 42MB.
 			value := r.FormValue(fileName)
 			payload = []byte(value)
 		}
