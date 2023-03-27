862b85304b2d1984d5ba530d31f77e949e744692
Eunice Chan <eunice@aqueducthq.com>
2023-03-27 11:25:49 -0700

    PR edits

---
+++ src/ui/common/src/components/pages/components/S3LoadParamDisplay.tsx
@@ -0,0 +1,51 @@
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import React from 'react';
+
+import {
+  S3LoadParams,
+} from '../../../utils/operators';
+
+type S3LoadParamDisplayProps = {
+  parameters: S3LoadParams;
+};
+
+export const S3LoadParamDisplay: React.FC<S3LoadParamDisplayProps> = ({ parameters }) => {
+  return (
+    <Box 
+    sx={{
+      display: 'flex',
+      flexDirection: 'row',
+      justifyContent: 'space-evenly',
+    }}>
+        <Box sx={{
+          textAlign: 'center',
+        }}>
+          <Typography variant="body2" sx={{ 
+            py: 1,
+            color: 'gray.800',
+         }}>
+            Filepath
+          </Typography>
+          <Typography variant="body1">
+            {parameters.filepath}
+          </Typography>
+        </Box>
+        <Box sx={{
+          textAlign: 'center',
+        }}>
+          <Typography variant="body2" sx={{ 
+            py: 1,
+            color: 'gray.800',
+         }}>
+            Format
+          </Typography>
+          <Typography variant="body1">
+            {parameters.format}
+          </Typography>
+        </Box>
+      </Box>
+  );
+};
+
+export default S3LoadParamDisplay;
--- src/ui/common/src/utils/operators.ts
+++ src/ui/common/src/utils/operators.ts
@@ -142,7 +142,7 @@ export type RelationalDBLoadParams = {
   update_mode: UpdateMode;
 };
 export const isRelationalDBLoadParams = (
-  input: any
+  input: LoadParameters
 ): input is RelationalDBLoadParams =>
   input.table !== undefined && input.update_mode !== undefined;
 
@@ -150,10 +150,6 @@ export type GoogleSheetsLoadParams = {
   filepath: string;
   save_mode: string;
 };
-export const isGoogleSheetsLoadParams = (
-  input: any
-): input is GoogleSheetsLoadParams =>
-  input.filepath !== undefined && input.save_mode !== undefined;
 
 export enum S3TableFormat {
   Csv = 'CSV',
@@ -165,8 +161,21 @@ export type S3LoadParams = {
   filepath: string;
   format: S3TableFormat;
 };
-export const isS3LoadParams = (input: any): input is S3LoadParams =>
+export const isS3LoadParams = (input: LoadParameters): input is S3LoadParams =>
   input.filepath !== undefined && input.format !== undefined;
+
+export enum LoadParametersType {
+  RelationalDBLoadParamsType = 'RelationalDBLoadParamsType',
+  S3LoadParamsType = 'S3LoadParamsType',
+}
+
+export const getLoadParametersType = (input: LoadParameters): LoadParametersType => {
+  if (isRelationalDBLoadParams(input)) {
+    return LoadParametersType.RelationalDBLoadParamsType;
+  } else if (isS3LoadParams(input)) {
+    return LoadParametersType.S3LoadParamsType;
+  }
+}
 
 export type Load = {
   service: ServiceType;
