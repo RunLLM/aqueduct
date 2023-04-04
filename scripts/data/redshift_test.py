791f604cef78618f731ac04be19bf46d4611777d
Saurav Chhatrapati <sauravc@berkeley.edu>
2023-04-04 12:01:55 -0400

    Adds cluster status checks

--- scripts/data/redshift_test.py
+++ scripts/data/redshift_test.py
@@ -1,26 +1,69 @@
 import boto3
+import sys
 
-CLUSTER_NAME = 'integration-test-shared'
+CLUSTER_NAME = "integration-test-shared"
+
+STATUS_AVAILABLE = "available"
+STATUS_PAUSED = "paused"
+
 
 def resume_redshift(aws_access_key_id, aws_secret_access_key):
     client = _create_client(aws_access_key_id, aws_secret_access_key)
 
-    response = client.resume_cluster(
-        ClusterIdentifier=CLUSTER_NAME
-    )
+    status = _get_cluster_status(client, CLUSTER_NAME)
+    if status == STATUS_AVAILABLE:
+        print(
+            f"The {CLUSTER_NAME} cluster is already available, it does not need to be resumed"
+        )
+    elif status == STATUS_PAUSED:
+        client.resume_cluster(ClusterIdentifier=CLUSTER_NAME)
+    else:
+        sys.exit(
+            f"Cannot resume {CLUSTER_NAME} cluster because it is in the {status} state"
+        )
 
 
 def pause_redshift(aws_access_key_id, aws_secret_access_key):
     client = _create_client(aws_access_key_id, aws_secret_access_key)
 
-    client.pause_cluster(
-        ClusterIdentifier=CLUSTER_NAME
-    )
+    status = _get_cluster_status(client, CLUSTER_NAME)
+    if status == STATUS_PAUSED:
+        print(
+            f"The {CLUSTER_NAME} cluster is already paused, it does not need to be paused"
+        )
+    elif status == STATUS_AVAILABLE:
+        client.pause_cluster(ClusterIdentifier=CLUSTER_NAME)
+    else:
+        sys.exit(
+            f"Cannot pause {CLUSTER_NAME} cluster because it is in the {status} state"
+        )
 
 
 def _create_client(aws_access_key_id, aws_secret_access_key):
     return boto3.client(
-        'redshift',
+        "redshift",
         aws_access_key_id=aws_access_key_id,
-        aws_secret_access_key=aws_secret_access_key
+        aws_secret_access_key=aws_secret_access_key,
     )
+
+
+def _get_cluster_status(client, cluster_identifier):
+    response = client.describe_clusters(ClusterIdentifier=cluster_identifier)
+
+    if "Clusters" not in response:
+        sys.exit(f"Unable to {cluster_identifier} cluster response")
+
+    clusters = response["Clusters"]
+    if not clusters or len(clusters) == 0:
+        sys.exit(f"Unable to find {cluster_identifier} cluster in response")
+
+    cluster = clusters[0]
+    if "ClusterStatus" not in cluster:
+        sys.exit(f"Unable to find {cluster_identifier} cluster status in response")
+
+    status = cluster["ClusterStatus"]
+    if status == "available":
+        return STATUS_AVAILABLE
+    elif status == "paused":
+        return STATUS_PAUSED
+    return status
--- scripts/data/setup_hosted.py
+++ scripts/data/setup_hosted.py
@@ -2,11 +2,12 @@ import argparse
 
 from redshift_test import resume_redshift
 
+
 def main():
     parser = argparse.ArgumentParser()
 
-    parser.add_argument('--aws-key-id', required=True, help='AWS Access Key ID')
-    parser.add_argument('--aws-secret-key', required=True, help='AWS Secret Access Key')
+    parser.add_argument("--aws-key-id", required=True, help="AWS Access Key ID")
+    parser.add_argument("--aws-secret-key", required=True, help="AWS Secret Access Key")
     args = parser.parse_args()
 
     aws_access_key_id = args.aws_key_id
@@ -19,5 +20,5 @@ def setup_redshift(aws_access_key_id, aws_secret_access_key):
     resume_redshift(aws_access_key_id, aws_secret_access_key)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
