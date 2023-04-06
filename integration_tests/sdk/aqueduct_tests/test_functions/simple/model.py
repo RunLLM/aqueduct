3faf7afc0af3d0b05bf6ce21f9064a235914f06d
hsubbaraj <hsubbaraj@aqueducthq.com>
2023-04-06 02:50:08 +0000

    moved back to pandas fxn

--- integration_tests/sdk/aqueduct_tests/test_functions/simple/model.py
+++ integration_tests/sdk/aqueduct_tests/test_functions/simple/model.py
@@ -3,28 +3,29 @@ from aqueduct import op
 
 @op()
 def dummy_sentiment_model(df):
-    columns = df.columns
+    df["positivity"] = 123
     return df
 
 
 def dummy_sentiment_model_function(df):
-    columns = df.columns
+    df["positivity"] = 123
     return df
 
 
 @op
 def dummy_sentiment_model_multiple_input(df1, df2):
-    columns = df1.columns
+    df1["positivity"] = 123
+    df1["positivity_2"] = 456
     return df1
 
 
 @op()
 def dummy_model(df):
-    columns = df.columns
+    df["newcol"] = "value"
     return df
 
 
 @op()
 def dummy_model_2(df):
-    columns = df.columns
+    df["newcol_2"] = "value"
     return df
