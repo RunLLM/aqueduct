71b9f619096e44c720af545785e2100a4cbee9bd
Andre Giron <andre@spiralai.co>
2023-04-06 11:52:25 -0700

    mysql periodic integration test

--- integration_tests/sdk/setup_integration.py
+++ integration_tests/sdk/setup_integration.py
@@ -164,6 +164,11 @@ def _setup_postgres_db():
     _execute_command(["aqueduct", "install", "postgres"])
 
 
+def _setup_mysql_db():
+    print("Setting up MySQL database ...")
+    _execute_command(["aqueduct", "install", "mysql"])
+
+
 def _setup_relational_data(client: Client, db: RelationalDBIntegration) -> None:
     # Find all the tables that already exist.
     existing_table_names = set(db.list_tables()["tablename"])
@@ -175,7 +180,8 @@ def _setup_s3_data(client: Client, s3: S3Integration):
     existing_names = set()
     for object_name in demo_db_tables():
         try:
-            s3.file(object_name, artifact_type=ArtifactType.TABLE, format="parquet")
+            s3.file(object_name, artifact_type=ArtifactType.TABLE,
+                    format="parquet")
             existing_names.add(object_name)
         except:
             # Failing to fetch simply means we will need to populate this data.
@@ -210,7 +216,8 @@ def setup_data_integrations(client: Client, filter_to: Optional[str] = None) -> 
     for integration_name in data_integrations:
         # Only connect to integrations that don't already exist.
         if integration_name not in connected_integrations.keys():
-            integration_config = _fetch_integration_credentials("data", integration_name)
+            integration_config = _fetch_integration_credentials(
+                "data", integration_name)
 
             # Stand up the external integration first.
             if integration_config["type"] == ServiceType.SQLITE:
@@ -218,6 +225,9 @@ def setup_data_integrations(client: Client, filter_to: Optional[str] = None) -> 
 
             if integration_config["type"] == ServiceType.POSTGRES:
                 _setup_postgres_db()
+
+            if integration_config["type"] == ServiceType.MYSQL:
+                _setup_mysql_db()
 
             client.connect_integration(
                 integration_name,
@@ -237,7 +247,8 @@ def setup_data_integrations(client: Client, filter_to: Optional[str] = None) -> 
             # We only support reading from Athena, so no setup is necessary.
             pass
         else:
-            raise Exception("Test suite does not yet support %s." % integration.type())
+            raise Exception("Test suite does not yet support %s." %
+                            integration.type())
 
 
 def setup_compute_integrations(client: Client, filter_to: Optional[str] = None) -> None:
@@ -263,7 +274,8 @@ def setup_compute_integrations(client: Client, filter_to: Optional[str] = None) 
     for integration_name in compute_integrations:
         # Only connect to integrations that don't already exist.
         if integration_name not in connected_integrations.keys():
-            integration_config = _fetch_integration_credentials("compute", integration_name)
+            integration_config = _fetch_integration_credentials(
+                "compute", integration_name)
 
             client.connect_integration(
                 integration_name,
