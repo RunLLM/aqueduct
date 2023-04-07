7d4c4d3b6228a181b71b201ebcd249f0d588ce9f
Eunice Chan <eunice@aqueducthq.com>
2023-04-07 07:25:31 -0700

    Remove files from another PR

--- sdk/aqueduct/artifacts/numeric_artifact.py
+++ sdk/aqueduct/artifacts/numeric_artifact.py
@@ -183,8 +183,7 @@ class NumericArtifact(BaseArtifact):
                 % (",".join(input_mapping.keys()))
             )
 
-        # Specify is not None because 0 is a Falsey value
-        assert bound_name is not None and bound_value is not None
+        assert bound_name and bound_value
 
         accepted_types = [float, int]
         if type(bound_value) not in accepted_types:
