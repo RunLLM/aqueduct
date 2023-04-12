bd24ad686b7e1f8185f1e9f229cff205ff4cdc5e
Saurav Chhatrapati <sauravc@berkeley.edu>
2023-04-12 16:07:33 -0700

    Fixes airflow start script

--- scripts/compute/airflow_test_setup.sh
+++ scripts/compute/airflow_test_setup.sh
@@ -8,4 +8,10 @@ PYTHON_VERSION="$(python3 --version | cut -d " " -f 2 | cut -d "." -f 1-2)"
 CONSTRAINT_URL="https://raw.githubusercontent.com/apache/airflow/constraints-${AIRFLOW_VERSION}/constraints-${PYTHON_VERSION}.txt"
 pip3 install "apache-airflow==${AIRFLOW_VERSION}" --constraint "${CONSTRAINT_URL}"
 
-AIRFLOW__WEBSERVER__WEB_SERVER_PORT=8000 airflow standalone
+export AIRFLOW__WEBSERVER__WEB_SERVER_PORT=8000
+# Allows basic username and password based auth 
+export AIRFLOW__API__AUTH_BACKENDS=airflow.api.auth.backend.basic_auth
+# Has scheduler check for new DAGs every 15s
+export AIRFLOW__SCHEDULER__DAG_DIR_LIST_INTERVAL=15
+
+airflow standalone
