8857d24e134f63c6e6366658ccfa1af9e9458b0c
Wei Chen <wei@aqueducthq.com>
2023-04-11 17:27:41 -0700

    doc

--- src/golang/cmd/server/routes/params.go
+++ src/golang/cmd/server/routes/params.go
@@ -10,6 +10,8 @@ const (
 	IntegrationIdUrlParam       = "integrationId"
 
 	// v2 params
+	// Each V2 parameters should have a corresponding parser
+	// in request/parser package.
 	WorkflowIDParam  = "workflowID"
 	DagIDParam       = "dagID"
 	DAGResultIDParam = "dagResultID"
