12a8193b68bacc892861ac899dd7879d5331af7b
Andre Giron <andre@spiralai.co>
2023-04-12 21:56:42 -0700

    Fixes issue where airflow dialog renders infinitely.

--- src/ui/common/src/components/integrations/dialogs/airflowDialog.tsx
+++ src/ui/common/src/components/integrations/dialogs/airflowDialog.tsx
@@ -24,28 +24,7 @@ export const AirflowDialog: React.FC<Props> = ({
   value,
   editMode,
 }) => {
-  const [host, setHost] = useState<string>(value?.host ?? null);
-  const [s3CredsProfile, setS3CredsProfile] = useState<string>(
-    value?.s3_credentials_profile ?? Placeholders.s3_credentials_profile
-  );
-
-  useEffect(() => {
-    if (host) {
-      if (host.startsWith('http://')) {
-        // Backend requires the protocol to be stripped
-        onUpdateField('host', host.substring(7));
-      } else if (host.startsWith('https://')) {
-        // Backend requires the protocol to be stripped
-        onUpdateField('host', host.substring(8));
-      } else {
-        onUpdateField('host', host);
-      }
-    }
-
-    if (s3CredsProfile) {
-      onUpdateField('s3_credentials_profile', s3CredsProfile);
-    }
-  }, [host, onUpdateField, s3CredsProfile]);
+  const [host, setHost] = useState<string>(value?.host ?? '');
 
   console.log('rendering airflow dialog ...');
 
@@ -57,7 +36,18 @@ export const AirflowDialog: React.FC<Props> = ({
         label="Host *"
         description="The hostname of the Airflow server."
         placeholder={Placeholders.host}
-        onChange={(event) => setHost(event.target.value)}
+        onChange={(event) => {
+          setHost(event.target.value)
+          if (event.target.value.startsWith('http://')) {
+            // Backend requires the protocol to be stripped
+            onUpdateField('host', event.target.value.substring(7));
+          } else if (event.target.value.startsWith('https://')) {
+            // Backend requires the protocol to be stripped
+            onUpdateField('host', event.target.value.substring(8));
+          } else {
+            onUpdateField('host', event.target.value);
+          }
+        }}
         value={host}
         disabled={editMode}
         warning={editMode ? undefined : readOnlyFieldWarning}
@@ -71,7 +61,7 @@ export const AirflowDialog: React.FC<Props> = ({
         description="The username of a user with access to the above server."
         placeholder={Placeholders.username}
         onChange={(event) => onUpdateField('username', event.target.value)}
-        value={value?.username ?? null}
+        value={value?.username ?? ''}
       />
 
       <IntegrationTextInputField
@@ -82,7 +72,7 @@ export const AirflowDialog: React.FC<Props> = ({
         placeholder={Placeholders.password}
         type="password"
         onChange={(event) => onUpdateField('password', event.target.value)}
-        value={value?.password ?? null}
+        value={value?.password ?? ''}
       />
 
       <IntegrationTextInputField
@@ -91,10 +81,8 @@ export const AirflowDialog: React.FC<Props> = ({
         label="S3 Credentials Path *"
         description="The path on the Airflow server to the AWS credentials that have access to the same S3 bucket configured for Aqueduct storage."
         placeholder={Placeholders.s3_credentials_path}
-        onChange={(event) =>
-          onUpdateField('s3_credentials_path', event.target.value)
-        }
-        value={value?.s3_credentials_path ?? null}
+        onChange={(event) => { onUpdateField('s3_credentials_path', event.target.value)}}
+        value={value?.s3_credentials_path ?? ''}
       />
 
       <IntegrationTextInputField
@@ -103,12 +91,10 @@ export const AirflowDialog: React.FC<Props> = ({
         label="S3 Credentials Profile"
         description="The profile to use for the AWS credentials above. The default profile will be used if none is provided."
         placeholder={Placeholders.s3_credentials_profile}
-        onChange={(event) => setS3CredsProfile(event.target.value)}
-        value={
-          s3CredsProfile !== Placeholders.s3_credentials_profile
-            ? s3CredsProfile
-            : null
-        }
+        onChange={(event) => {
+          onUpdateField('s3_credentials_profile', event.target.value);
+        }}
+        value={value?.s3_credentials_profile ?? ''}
       />
     </Box>
   );
