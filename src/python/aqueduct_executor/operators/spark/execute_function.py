5acb44457080c5931c159970efd0c472c9172347
hsubbaraj <hsubbaraj@aqueducthq.com>
2023-04-20 19:09:50 +0000

    remove unneeded imports

--- src/python/aqueduct_executor/operators/spark/execute_function.py
+++ src/python/aqueduct_executor/operators/spark/execute_function.py
@@ -1,48 +1,11 @@
-import importlib
-import json
-import os
-import shutil
-import sys
-import tracemalloc
-import uuid
-from typing import Any, Callable, Dict, List, Tuple
-
-import numpy as np
-import pandas as pd
+from typing import Any
 from aqueduct.utils.type_inference import infer_artifact_type
-from aqueduct_executor.operators.function_executor import extract_function, get_extract_path
-from aqueduct_executor.operators.function_executor.execute import (
-    cleanup,
-    get_py_import_path,
-    import_invoke_method,
-    run_helper,
-    validate_spec,
-)
+from aqueduct_executor.operators.function_executor.execute import run_helper
 from aqueduct_executor.operators.function_executor.spec import FunctionSpec
-from aqueduct_executor.operators.function_executor.utils import OP_DIR
 from aqueduct_executor.operators.spark.utils import read_artifacts_spark, write_artifact_spark
-from aqueduct_executor.operators.utils import utils
-from aqueduct_executor.operators.utils.enums import (
-    ArtifactType,
-    CheckSeverity,
-    ExecutionStatus,
-    FailureType,
-    OperatorType,
-    SerializationType,
-)
-from aqueduct_executor.operators.utils.execution import (
-    TIP_CHECK_DID_NOT_PASS,
-    TIP_NOT_BOOL,
-    TIP_NOT_NUMERIC,
-    TIP_OP_EXECUTION,
-    TIP_UNKNOWN_ERROR,
-    ExecFailureException,
-    ExecutionState,
-    Logs,
-    exception_traceback,
-)
-from aqueduct_executor.operators.utils.storage.parse import parse_storage
-from aqueduct_executor.operators.utils.timer import Timer
+from aqueduct_executor.operators.utils.enums import ArtifactType
+
+
 from pyspark.sql import SparkSession, dataframe
 
 
