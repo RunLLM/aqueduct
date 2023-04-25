984354997591b80572cfefd663608cf4d2798a12
cw75 <cgwu0530@gmail.com>
2023-04-24 17:19:11 -0700

    path

--- src/golang/cmd/server/handler/cloud_integration_utils.go
+++ src/golang/cmd/server/handler/cloud_integration_utils.go
@@ -125,7 +125,7 @@ func setupTerraformDirectory(dst string) error {
 		return err
 	}
 
-	_, stdErr, err := lib_utils.RunCmd("cp", []string{"-r", fmt.Sprintf("%s/.", dynamic.TerraformTemplateDir), dst}, "", false)
+	_, stdErr, err := lib_utils.RunCmd("cp", []string{"-r", fmt.Sprintf("%s%s.", dynamic.TerraformTemplateDir, string(filepath.Separator)), dst}, "", false)
 	if err != nil {
 		return errors.New(stdErr)
 	}
