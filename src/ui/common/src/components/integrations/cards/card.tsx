34069ae060a64a4826cbfb4fa2eec0d134bbf31d
kenxu95 <kenxu@aqueducthq.com>
2023-04-27 17:43:01 -0700

    ready to make the dropdown for more fields work

--- src/ui/common/src/components/integrations/cards/card.tsx
+++ src/ui/common/src/components/integrations/cards/card.tsx
@@ -5,30 +5,13 @@ import { Integration } from '../../../utils/integrations';
 import ExecutionStatus from '../../../utils/shared';
 import { StatusIndicator } from '../../workflows/workflowStatus';
 import IntegrationLogo from '../logo';
-import { AirflowCard } from './airflowCard';
-import { AqueductDemoCard } from './aqueductDemoCard';
-import { AWSCard } from './awsCard';
-import { BigQueryCard } from './bigqueryCard';
-import { DatabricksCard } from './databricksCard';
-import { EmailCard } from './emailCard';
-import { GCSCard } from './gcsCard';
-import { KubernetesCard } from './kubernetesCard';
-import { LambdaCard } from './lambdaCard';
-import { MariaDbCard } from './mariadbCard';
-import { MongoDBCard } from './mongoDbCard';
-import { MySqlCard } from './mysqlCard';
-import { PostgresCard } from './postgresCard';
-import { RedshiftCard } from './redshiftCard';
-import { S3Card } from './s3Card';
-import { SlackCard } from './slackCard';
-import { SnowflakeCard } from './snowflakeCard';
-import { SparkCard } from './sparkCard';
+import { ResourceFieldsDetailsCard } from './resourceFieldsDetailsCard';
 import { TruncatedText } from './text';
 
 type IntegrationProps = {
   integration: Integration;
 
-  // Eg: "2 workflows using this integration"
+  // Eg: "Used by 2 workflows"
   numWorkflowsUsingMsg: string;
 };
 
@@ -36,66 +19,6 @@ export const IntegrationCard: React.FC<IntegrationProps> = ({
   integration,
   numWorkflowsUsingMsg,
 }) => {
-  let serviceCard;
-  switch (integration.service) {
-    case 'Postgres':
-      serviceCard = <PostgresCard integration={integration} />;
-      break;
-    case 'Snowflake':
-      serviceCard = <SnowflakeCard integration={integration} />;
-      break;
-    case 'Aqueduct Demo':
-      serviceCard = <AqueductDemoCard integration={integration} />;
-      break;
-    case 'MySQL':
-      serviceCard = <MySqlCard integration={integration} />;
-      break;
-    case 'Redshift':
-      serviceCard = <RedshiftCard integration={integration} />;
-      break;
-    case 'MariaDB':
-      serviceCard = <MariaDbCard integration={integration} />;
-      break;
-    case 'MongoDB':
-      serviceCard = <MongoDBCard integration={integration} />;
-      break;
-    case 'BigQuery':
-      serviceCard = <BigQueryCard integration={integration} />;
-      break;
-    case 'S3':
-      serviceCard = <S3Card integration={integration} />;
-      break;
-    case 'GCS':
-      serviceCard = <GCSCard integration={integration} />;
-      break;
-    case 'Airflow':
-      serviceCard = <AirflowCard integration={integration} />;
-      break;
-    case 'Kubernetes':
-      serviceCard = <KubernetesCard integration={integration} />;
-      break;
-    case 'Lambda':
-      serviceCard = <LambdaCard integration={integration} />;
-      break;
-    case 'Databricks':
-      serviceCard = <DatabricksCard integration={integration} />;
-      break;
-    case 'Email':
-      serviceCard = <EmailCard integration={integration} />;
-      break;
-    case 'Slack':
-      serviceCard = <SlackCard integration={integration} />;
-      break;
-    case 'Spark':
-      serviceCard = <SparkCard integration={integration} />;
-      break;
-    case 'AWS':
-      serviceCard = <AWSCard integration={integration} />;
-      break;
-    default:
-      serviceCard = null;
-  }
-
   return (
     <Box sx={{ display: 'flex', flexDirection: 'column' }}>
       <Box sx={{ display: 'flex', flexDirection: 'row', alignItems: 'center' }}>
@@ -124,7 +47,10 @@ export const IntegrationCard: React.FC<IntegrationProps> = ({
         {new Date(integration.createdAt * 1000).toLocaleString()}
       </TruncatedText>
 
-      {serviceCard}
+      <ResourceFieldsDetailsCard
+        integration={integration}
+        detailedView={false}
+      />
 
       <Box
         sx={{
