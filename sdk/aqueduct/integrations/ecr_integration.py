56cf0ee00835170ea7f4960afcac8953dbb91165
cw75 <cgwu0530@gmail.com>
2023-04-28 22:14:16 +0000

    update

--- sdk/aqueduct/integrations/ecr_integration.py
+++ sdk/aqueduct/integrations/ecr_integration.py
@@ -1,5 +1,6 @@
 from typing import Dict
 
+from aqueduct.error import InvalidUserArgumentException
 from aqueduct.models.integration import Integration, IntegrationInfo
 
 from aqueduct import globals
@@ -23,7 +24,17 @@ class ECRIntegration(Integration):
         Returns a dictionary with the name of the ECR resource and the image url, which can be
         used as input to the `image` field of an operator's decorator. This method also verifies
         that the image exists in the ECR repository.
+
+        Args:
+            image_name: The name of the image to retrieve. Should be in the form of `image:tag`.
+            No need to include the endpoint URL such as 123456789012.dkr.ecr.us-east-1.amazonaws.com.
         """
+        if len(image_name.split("/")) == 2:
+            image_name = image_name.split("/")[1]
+
+        if len(image_name.split(":")) != 2:
+            raise InvalidUserArgumentException("Image name must be of the form `image:tag`.")
+
         response = globals.__GLOBAL_API_CLIENT__.get_image_url(
             integration_id=str(self._metadata.id),
             service=self._metadata.service,
