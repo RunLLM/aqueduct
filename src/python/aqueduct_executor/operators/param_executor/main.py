b1b491f81c9daa733e7df8636835e5177b0d6714
Wei Chen <wei@aqueducthq.com>
2023-05-08 11:41:06 -0700

    linters

--- src/python/aqueduct_executor/operators/param_executor/main.py
+++ src/python/aqueduct_executor/operators/param_executor/main.py
@@ -8,17 +8,20 @@ if __name__ == "__main__":
     args = parser.parse_args()
     if args.version_tag:
         import subprocess
-        install_process = subprocess.run([
-            "pip",
-            "install",
-            "-i",
-            "https://test.pypi.org/simple/",
-            f"aqueduct-ml={args.version_tag}",
-        ])
+
+        install_process = subprocess.run(
+            [
+                "pip",
+                "install",
+                "-i",
+                "https://test.pypi.org/simple/",
+                f"aqueduct-ml={args.version_tag}",
+            ]
+        )
         print(install_process.stderr)
         print(install_process.stdout)
         install_process.check_returncode()
-    
+
     from aqueduct_executor.operators.param_executor import execute
     from aqueduct_executor.operators.param_executor.spec import parse_spec
 
