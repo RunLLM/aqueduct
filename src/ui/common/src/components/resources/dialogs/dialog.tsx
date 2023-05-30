3ee4df9a7ee96a9e586363c62fe106f50029e189
Eunice Chan <eunice@aqueducthq.com>
2023-05-30 13:42:39 -0700

    {...data} instead

--- src/ui/common/src/components/resources/dialogs/dialog.tsx
+++ src/ui/common/src/components/resources/dialogs/dialog.tsx
@@ -108,7 +108,7 @@ const ResourceDialog: React.FC<Props> = ({
     // We do this so we can collect name form inputs inside the same form context.
     const name = data.name;
     // Make copy so we can reuse `data`.
-    const config = Object.assign({}, data);
+    const config = { ...data };
     // remove the name key from data so pydantic doesn't throw error.
     delete config.name;
 
