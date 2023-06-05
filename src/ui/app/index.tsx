a3702c81ffe5f3e0073b6a3165c210edea6d0c5f
Andre Giron <andre@aqueducthq.com>
2023-06-05 16:54:48 -0700

    ENG-3034 Frontend Linter Fixes (#1405)
    Co-authored-by: Andre Giron <andre@spiralai.co>

--- src/ui/app/index.tsx
+++ src/ui/app/index.tsx
@@ -1,67 +1,211 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-import { HomePage, DataPage, ResourcesPage, ResourceDetailsPage, WorkflowPage, WorkflowsPage, LoginPage, ErrorPage, AccountPage, OperatorDetailsPage, ArtifactDetailsPage, MetricDetailsPage, CheckDetailsPage } from '@aqueducthq/common';
-import { store } from './stores/store';
-import { Provider } from 'react-redux';
-import { useUser, UserProfile } from '@aqueducthq/common';
-import { getPathPrefix } from '@aqueducthq/common/src/utils/getPathPrefix';
 import '@aqueducthq/common/src/styles/globals.css';
 
-function RequireAuth({ children, user }): { children: JSX.Element, user: UserProfile | undefined } {
-  const pathPrefix = getPathPrefix();
+import {
+    AccountPage,
+    ArtifactDetailsPage,
+    CheckDetailsPage,
+    DataPage,
+    ErrorPage,
+    HomePage,
+    LoginPage,
+    MetricDetailsPage,
+    OperatorDetailsPage,
+    ResourceDetailsPage,
+    ResourcesPage,
+    WorkflowPage,
+    WorkflowsPage,
+} from '@aqueducthq/common';
+import { UserProfile, useUser } from '@aqueducthq/common';
+import { getPathPrefix } from '@aqueducthq/common/src/utils/getPathPrefix';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 
-  if (!user || !user.apiKey) {
-    return <Navigate to={`${pathPrefix}/login`} replace />;
-  }
+import { store } from './stores/store';
 
-  return children;
+function RequireAuth({ children, user }): { children: JSX.Element; user: UserProfile | undefined } {
+    const pathPrefix = getPathPrefix();
+
+    if (!user || !user.apiKey) {
+        return <Navigate to={`${pathPrefix}/login`} replace />;
+    }
+
+    return children;
 }
 
 const App = () => {
-  const { user, loading } = useUser();
-  if (loading) {
-    return null;
-  }
+    const { user, loading } = useUser();
+    if (loading) {
+        return null;
+    }
 
-  const pathPrefix = getPathPrefix();
-  let routesContent: React.ReactElement;
-  routesContent = (
-    <Routes>
-      <Route path={`${pathPrefix ?? "/"}`} element={<RequireAuth user={user}><HomePage user={user} /> </RequireAuth>} />
-      <Route path={`/${pathPrefix}/data`} element={<RequireAuth user={user}><DataPage user={user} /> </RequireAuth>} />
-      <Route path={`/${pathPrefix}/resources`} element={<RequireAuth user={user}><ResourcesPage user={user} /> </RequireAuth>} />
-      <Route path={`/${pathPrefix}/resource/:id`} element={<RequireAuth user={user}><ResourceDetailsPage user={user} /> </RequireAuth>} />
-      <Route path={`/${pathPrefix}/workflows`} element={<RequireAuth user={user}><WorkflowsPage user={user} /> </RequireAuth>} />
-      <Route path={`/${pathPrefix}/login`} element={user && user.apiKey ? <Navigate to="/" replace /> : <LoginPage />} />
-      <Route path={`/${pathPrefix}/account`} element={<RequireAuth user={user}><AccountPage user={user} /> </RequireAuth>} />
-      <Route path={`/${pathPrefix}/workflow/:workflowId`} element={<RequireAuth user={user}><WorkflowPage user={user} /> </RequireAuth>} />
-      <Route path={`/${pathPrefix}/workflow/:workflowId/dag/:dagId`} element={<RequireAuth user={user}><WorkflowPage user={user} /> </RequireAuth>} />
-      <Route path={`/${pathPrefix}/workflow/:workflowId/result/:dagResultId`} element={<RequireAuth user={user}><WorkflowPage user={user} /> </RequireAuth>} />
-      <Route path={`/${pathPrefix}/workflow/:workflowId/result/:dagResultId/operator/:nodeId`} element={<RequireAuth user={user}><OperatorDetailsPage user={user} /> </RequireAuth>} />
-      <Route path={`/${pathPrefix}/workflow/:workflowId/result/:dagResultId/artifact/:nodeId`} element={<RequireAuth user={user}><ArtifactDetailsPage user={user} /> </RequireAuth>} />
-      <Route path={`/${pathPrefix}/workflow/:workflowId/result/:dagResultId/metric/:nodeId`} element={<RequireAuth user={user}><MetricDetailsPage user={user} /> </RequireAuth>} />
-      <Route path={`/${pathPrefix}/workflow/:workflowId/result/:dagResultId/check/:nodeId`} element={<RequireAuth user={user}><CheckDetailsPage user={user} /> </RequireAuth>} />
-      <Route path={`/${pathPrefix}/workflow/:workflowId/dag/:dagId/operator/:nodeId`} element={<RequireAuth user={user}><OperatorDetailsPage user={user} /> </RequireAuth>} />
-      <Route path={`/${pathPrefix}/workflow/:workflowId/dag/:dagId/artifact/:nodeId`} element={<RequireAuth user={user}><ArtifactDetailsPage user={user} /> </RequireAuth>} />
-      <Route path={`/${pathPrefix}/workflow/:workflowId/dag/:dagId/metric/:nodeId`} element={<RequireAuth user={user}><MetricDetailsPage user={user} /> </RequireAuth>} />
-      <Route path={`/${pathPrefix}/workflow/:workflowId/dag/:dagId/check/:nodeId`} element={<RequireAuth user={user}><CheckDetailsPage user={user} /> </RequireAuth>} />
-      <Route path={`/${pathPrefix}/404`} element={user && user.apiKey ? <RequireAuth user={user}><ErrorPage user={user} /> </RequireAuth> : <ErrorPage />} />
-      <Route path="*" element={<Navigate replace to={`/404`} />} />
-    </Routes>
-  );
+    const pathPrefix = getPathPrefix();
+    const routesContent: React.ReactElement = (
+        <Routes>
+            <Route
+                path={`${pathPrefix ?? '/'}`}
+                element={
+                    <RequireAuth user={user}>
+                        <HomePage user={user} />{' '}
+                    </RequireAuth>
+                }
+            />
+            <Route
+                path={`/${pathPrefix}/data`}
+                element={
+                    <RequireAuth user={user}>
+                        <DataPage user={user} />{' '}
+                    </RequireAuth>
+                }
+            />
+            <Route
+                path={`/${pathPrefix}/resources`}
+                element={
+                    <RequireAuth user={user}>
+                        <ResourcesPage user={user} />{' '}
+                    </RequireAuth>
+                }
+            />
+            <Route
+                path={`/${pathPrefix}/resource/:id`}
+                element={
+                    <RequireAuth user={user}>
+                        <ResourceDetailsPage user={user} />{' '}
+                    </RequireAuth>
+                }
+            />
+            <Route
+                path={`/${pathPrefix}/workflows`}
+                element={
+                    <RequireAuth user={user}>
+                        <WorkflowsPage user={user} />{' '}
+                    </RequireAuth>
+                }
+            />
+            <Route
+                path={`/${pathPrefix}/login`}
+                element={user && user.apiKey ? <Navigate to="/" replace /> : <LoginPage />}
+            />
+            <Route
+                path={`/${pathPrefix}/account`}
+                element={
+                    <RequireAuth user={user}>
+                        <AccountPage user={user} />{' '}
+                    </RequireAuth>
+                }
+            />
+            <Route
+                path={`/${pathPrefix}/workflow/:workflowId`}
+                element={
+                    <RequireAuth user={user}>
+                        <WorkflowPage user={user} />{' '}
+                    </RequireAuth>
+                }
+            />
+            <Route
+                path={`/${pathPrefix}/workflow/:workflowId/dag/:dagId`}
+                element={
+                    <RequireAuth user={user}>
+                        <WorkflowPage user={user} />{' '}
+                    </RequireAuth>
+                }
+            />
+            <Route
+                path={`/${pathPrefix}/workflow/:workflowId/result/:dagResultId`}
+                element={
+                    <RequireAuth user={user}>
+                        <WorkflowPage user={user} />{' '}
+                    </RequireAuth>
+                }
+            />
+            <Route
+                path={`/${pathPrefix}/workflow/:workflowId/result/:dagResultId/operator/:nodeId`}
+                element={
+                    <RequireAuth user={user}>
+                        <OperatorDetailsPage user={user} />{' '}
+                    </RequireAuth>
+                }
+            />
+            <Route
+                path={`/${pathPrefix}/workflow/:workflowId/result/:dagResultId/artifact/:nodeId`}
+                element={
+                    <RequireAuth user={user}>
+                        <ArtifactDetailsPage user={user} />{' '}
+                    </RequireAuth>
+                }
+            />
+            <Route
+                path={`/${pathPrefix}/workflow/:workflowId/result/:dagResultId/metric/:nodeId`}
+                element={
+                    <RequireAuth user={user}>
+                        <MetricDetailsPage user={user} />{' '}
+                    </RequireAuth>
+                }
+            />
+            <Route
+                path={`/${pathPrefix}/workflow/:workflowId/result/:dagResultId/check/:nodeId`}
+                element={
+                    <RequireAuth user={user}>
+                        <CheckDetailsPage user={user} />{' '}
+                    </RequireAuth>
+                }
+            />
+            <Route
+                path={`/${pathPrefix}/workflow/:workflowId/dag/:dagId/operator/:nodeId`}
+                element={
+                    <RequireAuth user={user}>
+                        <OperatorDetailsPage user={user} />{' '}
+                    </RequireAuth>
+                }
+            />
+            <Route
+                path={`/${pathPrefix}/workflow/:workflowId/dag/:dagId/artifact/:nodeId`}
+                element={
+                    <RequireAuth user={user}>
+                        <ArtifactDetailsPage user={user} />{' '}
+                    </RequireAuth>
+                }
+            />
+            <Route
+                path={`/${pathPrefix}/workflow/:workflowId/dag/:dagId/metric/:nodeId`}
+                element={
+                    <RequireAuth user={user}>
+                        <MetricDetailsPage user={user} />{' '}
+                    </RequireAuth>
+                }
+            />
+            <Route
+                path={`/${pathPrefix}/workflow/:workflowId/dag/:dagId/check/:nodeId`}
+                element={
+                    <RequireAuth user={user}>
+                        <CheckDetailsPage user={user} />{' '}
+                    </RequireAuth>
+                }
+            />
+            <Route
+                path={`/${pathPrefix}/404`}
+                element={
+                    user && user.apiKey ? (
+                        <RequireAuth user={user}>
+                            <ErrorPage user={user} />{' '}
+                        </RequireAuth>
+                    ) : (
+                        <ErrorPage />
+                    )
+                }
+            />
+            <Route path="*" element={<Navigate replace to={`/404`} />} />
+        </Routes>
+    );
 
-  return (
-    <BrowserRouter>{routesContent}</BrowserRouter>
-  );
+    return <BrowserRouter>{routesContent}</BrowserRouter>;
 };
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 
 root.render(
-  <Provider store={store}>
-    <App />
-  </Provider>
+    <Provider store={store}>
+        <App />
+    </Provider>,
 );
